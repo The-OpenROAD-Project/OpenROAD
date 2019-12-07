8516aa4d7ff0dd04b46c3fefc8924da34513af3e
mgwoo <mgwoo@unist.ac.kr>
2019-12-07 01:43:43 -0800

    Update OpenDP unit testcases for top-level app
    Update OpenDP unit testcases for top-level app

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,2 +1,3 @@
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/test/regression fast && /OpenROAD/src/resizer/test/regression fast"
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "cd /OpenROAD/src/replace/test && python3 regression.py run openroad"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "cd /OpenROAD/src/opendp/test && python3 regression.py run openroad"
