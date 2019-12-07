6a2738c10cbdb6d4710d37e5aa026056812be4c6
mgwoo <mgwoo@unist.ac.kr>
2019-12-06 16:41:58 -0800

    replace regression updated

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,1 +1,2 @@
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/test/regression fast && /OpenROAD/src/resizer/test/regression fast"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "cd /OpenROAD/src/replace/test && python3 regression.py run openroad"
