d095a374315e0fb4bb3eaae366891a45e7a7ab9b
James Cherry <cherry@parallaxsw.com>
2019-12-09 16:08:10 -0700

    jenkins test.sh break lines

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,2 +1,4 @@
-docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/test/regression fast && /OpenROAD/src/resizer/test/regression fast && /OpenROAD/src/opendp/test/regression fast"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/test/regression fast"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/src/resizer/test/regression fast"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/src/opendp/test/regression fast"
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "cd /OpenROAD/src/replace/test && python3 regression.py run openroad"
