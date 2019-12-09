e3ff52c34672ae47893aea3d58bae833d4e7936c
mgwoo <mgwoo@unist.ac.kr>
2019-12-08 22:58:32 -0800

    Update opendp pointers to have simple regression testcases

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,3 +1,2 @@
-docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/test/regression fast && /OpenROAD/src/resizer/test/regression fast"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/test/regression fast && /OpenROAD/src/resizer/test/regression fast && /OpenROAD/src/opendp/test/regression fast"
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "cd /OpenROAD/src/replace/test && python3 regression.py run openroad"
-docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "cd /OpenROAD/src/opendp/test && python3 regression.py run openroad"
