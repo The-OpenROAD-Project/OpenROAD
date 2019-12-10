d214ae94780d0f3aecb1ca240232fcb860d6b344
James Cherry <cherry@parallaxsw.com>
2019-12-10 16:15:08 -0700

    jenkins exit if any test fails

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,3 +1,5 @@
+# exit on failure of any test line
+set -e
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/test/regression fast"
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/src/resizer/test/regression fast"
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/src/opendp/test/regression fast"
