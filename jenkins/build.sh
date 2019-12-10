60881cf6467baf6258dfa95de2e95d47a6d3ee11
Ahmed Agiza <ahmed.agiza22@gmail.com>
2019-12-09 19:19:42 -0500

    Fix Jenkins Build.sh

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,2 +1,2 @@
 docker build -t openroad/openroad --target base-dependencies .
-docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/jenkins/install.sh"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "./OpenROAD/jenkins/install.sh"
