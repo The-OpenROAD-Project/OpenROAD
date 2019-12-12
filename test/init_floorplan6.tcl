d3af24acaf7c8e10644e6e987a4a3750b7008490
James Cherry <cherry@parallaxsw.com>
2019-12-12 08:30:28 -0700

    init_floorplan diearea units, rows multiple of site size

--- test/init_floorplan6.tcl
+++ test/init_floorplan6.tcl
@@ -1,13 +1,14 @@
-# init_floorplan -tracks lef5.8
+# init_floorplan UNITS 2000
 source "helpers.tcl"
-read_lef init_floorplan5.lef
-read_def reg1.def
+read_lef init_floorplan6.lef
+read_verilog reg1.v
 read_liberty liberty1.lib
+link_design top
 initialize_floorplan -die_area "0 0 1000 1000" \
   -core_area "100 100 900 900" \
   -site site1
 auto_place_pins M1
 
-set def_file [make_result_file init_floorplan5.def]
+set def_file [make_result_file init_floorplan6.def]
 write_def $def_file
 report_file $def_file
