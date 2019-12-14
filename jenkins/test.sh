9df313869a403f1ea8815e79d67a790eadbd4fa7
James Cherry <cherry@parallaxsw.com>
2019-12-13 22:23:21 -0700

    woops; jenkins/test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,1 +1,8 @@
-docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "cd /OpenROAD; test/test_all.sh"
+# exit on failure of any test line
+set -e
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/test/regression fast"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/src/pdngen/test/regression fast"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/src/resizer/test/regression fast"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/src/opendp/test/regression fast"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "cd /OpenROAD/src/replace/test && python3 regression.py run openroad"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "cd /OpenROAD && cp etc/PO* . && tclsh ./src/FastRoute/tests/run_all.tcl"
