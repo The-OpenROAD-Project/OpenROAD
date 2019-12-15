ae4a8b6d6b4a4ce3152ec76f363faca7a528a69d
James Cherry <cherry@parallaxsw.com>
2019-12-15 15:32:53 -0700

    diff def file instead of printing it in log

--- src/pdngen/test/test_gcd.tcl
+++ src/pdngen/test/test_gcd.tcl
@@ -1,9 +1,7 @@
+source "helpers.tcl"
 read_lef nangate45/NangateOpenCellLibrary.mod.lef
 read_def gcd/floorplan.def
 pdngen gcd/PDN.cfg
 set def_file results/test_gcd.def
 write_def $def_file
-
-set f [open $def_file]
-fcopy $f stdout
-close $f
+diff_files $def_file test_gcd.defok
