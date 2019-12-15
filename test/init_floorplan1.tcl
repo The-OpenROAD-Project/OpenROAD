87fbc7bc6d0e7777aa03b3b2d5b1347fe96da55a
James Cherry <cherry@parallaxsw.com>
2019-12-14 17:08:27 -0700

    db def diff

--- test/init_floorplan1.tcl
+++ test/init_floorplan1.tcl
@@ -8,6 +8,5 @@ initialize_floorplan -die_area "0 0 1000 1000" \
   -site site1
 auto_place_pins M1
 
-set def_file [make_result_file init_floorplan1.def]
-write_def $def_file
-report_file $def_file
+write_def [make_result_file init_floorplan1.def]
+def_diff init_floorplan1.defok
