1ebba5c5be07db442531d7a1c3430a0793859741
eder_matheus <emrmonteiro@inf.ufrgs.br>
2019-12-24 17:48:28 -0300

    Add tapcell unit tests in Jenkins script

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -6,3 +6,4 @@ docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/src/resizer/
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/src/opendp/test/regression fast"
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "cd /OpenROAD/src/replace/test && python3 regression.py run openroad"
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "cd /OpenROAD && cp etc/PO* . && tclsh ./src/FastRoute/tests/run_all.tcl"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "cd /OpenROAD && tclsh ./src/tapcell/test/run_all.tcl"
