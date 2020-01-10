21b78f4d897536a01f55e236d50cb0382c4d90b4
James Cherry <cherry@parallaxsw.com>
2020-01-09 23:03:36 -0800

    test_all.sh run all but brain damaaged replace, fastroute, tritoncts

--- jenkins/test_all.sh
+++ jenkins/test_all.sh
@@ -1,12 +1,12 @@
-# Must be run from outside top level directory named OpenROAD due to
-# brain damage in replace and fastroute regressions.
-
 # exit if any regression script fails
 set -e
-#test/regression fast
-#src/pdngen/test/regression fast
-#src/resizer/test/regression fast
-#src/opendp/test/regression fast
-pushd src/replace/test && python3 regression.py run openroad && popd
-# fastroute: drop a few turds in the filesystem before testing
-#tclsh src/FastRoute/tests/run_all.tcl
+test/regression
+src/pdngen/test/regression
+src/resizer/test/regression
+src/opendp/test/regression
+src/TritonMacroPlace/test/regression
+
+# These tests are too brain dead to run outside of docker
+#pushd src/replace/test && python3 regression.py run openroad && popd
+#src/ioPlacer/tests/run_all.sh ./build/src/openroad ./src/ioPlacer/tests/
+#src/TritonCTS/tests/run_all.sh ./build/src/openroad ./src/TritonCTS/tests/
