d48b44efef89cf75608d5dee51253181fecf48d8
Mateus Fogaça <mateus.p.fogaca@gmail.com>
2020-01-10 15:16:21 -0300

    Fix typo

--- jenkins/test_all.sh
+++ jenkins/test_all.sh
@@ -5,7 +5,7 @@ src/pdngen/test/regression
 src/resizer/test/regression
 src/opendp/test/regression
 src/TritonMacroPlace/test/regression
-src/TritonCTS/tests/run_all.sh ./build/src/openroad ./src/TritonCTS/tests/
+src/TritonCTS/tests/run_all.sh
 
 # These tests are too brain dead to run outside of docker
 #pushd src/replace/test && python3 regression.py run openroad && popd
