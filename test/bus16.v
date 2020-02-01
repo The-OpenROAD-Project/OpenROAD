5b9094572dd1458b0736ec91c77c8a1f3676de3c
James Cherry <cherry@parallaxsw.com>
2020-02-01 10:57:21 -0700

    read_verilog lef/liberty when port order differs

--- test/bus16.v
+++
@@ -1,6 +0,0 @@
-module top (in, out);
-   input [15:0] in; 
-   output [15:0] out; 
-
-   bus16 bus1(.in(in), .out(out));
-endmodule
--- test/write_verilog3.tcl
+++ test/write_verilog3.tcl
@@ -3,6 +3,7 @@ source "helpers.tcl"
 read_lef -tech liberty1.lef
 read_lef bus1.lef
 read_def bus1.def
+read_liberty bus1.lib
 
 set verilog_file [make_result_file write_verilog3.v]
 write_verilog $verilog_file
