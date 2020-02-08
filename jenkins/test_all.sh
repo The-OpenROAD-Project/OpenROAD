31c572d7890fe273007e3bde43949609204c4c36
James Cherry <cherry@parallaxsw.com>
2020-02-08 10:50:36 -0700

    jenkins test

--- jenkins/test_all.sh
+++ jenkins/test_all.sh
@@ -1,16 +1,13 @@
 # exit if any regression script fails
 set -e
 test/regression
-src/pdngen/test/regression
-src/resizer/test/regression
-src/opendp/test/regression
-src/TritonMacroPlace/test/regression
-src/FastRoute/test/regression
+src/init_fp/test/regression
 src/ioPlacer/test/regression
+src/pdngen/test/regression
+src/TritonMacroPlace/test/regression
+src/replace/test/regression
+src/resizer/test/regression
 src/TritonCTS/test/regression
+src/opendp/test/regression
+src/FastRoute/test/regression
 src/tapcell/test/regression
-
-# hangs indefinitely
-#pushd src/replace/test && python3 regression.py run openroad && popd
-
-
