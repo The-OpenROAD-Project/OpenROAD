04f4120d2744d5a54b871b2719b3e97619226b07
James Cherry <cherry@parallaxsw.com>
2020-02-14 16:02:34 -0700

    sta_continue_on_error default 0

--- src/resizer/test/buffer_ports2.tcl
+++ src/resizer/test/buffer_ports2.tcl
@@ -10,6 +10,7 @@ catch { buffer_ports -buffer_cell liberty1/yyy } result
 puts $result
 catch { buffer_ports -buffer_cell [get_lib_cell xxx/yyy] } result
 puts $result
-buffer_ports -buffer_cell liberty1/snl_nor02x1
+catch { buffer_ports -buffer_cell liberty1/snl_nor02x1 } result
+puts $result
 buffer_ports -buffer_cell liberty1/snl_bufx2
 buffer_ports -buffer_cell [get_lib_cell liberty1/snl_bufx2]
