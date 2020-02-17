d371b733e4e15e7ae196b4c8cefd5a803a8ecb56
James Cherry <cherry@parallaxsw.com>
2020-02-17 16:00:59 -0700

    pdngen missing define_cmd_args

--- src/pdngen/src/PdnGen.tcl
+++ src/pdngen/src/PdnGen.tcl
@@ -28,7 +28,7 @@
 #OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 #OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#sta::define_cmd_args "pdngen" {[-verbose] config_file}
+sta::define_cmd_args "pdngen" {[-verbose] config_file}
 
 proc pdngen { args } {
   sta::parse_key_args "pdngen" args \
