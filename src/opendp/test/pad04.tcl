833ecef88b7177a8efa72b19f5b3c92a2514fd5b
James Cherry <cherry@parallaxsw.com>
2020-02-23 10:21:46 -0800

    opendp pad04

---
+++ src/opendp/test/pad04.tcl
@@ -0,0 +1,9 @@
+# 10 inst placement legal (should not move anything)
+source helpers.tcl
+read_lef NangateOpenCellLibrary.lef
+read_def pad04.def
+set_padding -global -left 2 -right 2
+legalize_placement
+set def_file [make_result_file pad04.def]
+write_def $def_file
+diff_file $def_file pad04.defok
