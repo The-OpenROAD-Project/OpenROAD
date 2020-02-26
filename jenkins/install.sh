0f5f1a236ac7fd763786b204f37be37a37509144
Matt Liberty <matt.liberty@gmail.com>
2020-02-26 10:52:22 -0800

    checkout the flow with test version of OpenROAD

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -5,3 +5,20 @@ mkdir -p /OpenROAD/build
 cd /OpenROAD/build
 cmake ..
 make -j 4
+
+# Capture the commit we are testing for use in flow testing
+commit=`git rev-parse --verify HEAD`
+
+# setup OpenROAD-flow
+cd /OpenROAD-flow
+if [[ ! -d .git ]]; then
+    git clone -b openroad https://github.com/The-OpenROAD-Project/OpenROAD-flow.git
+else
+    git fetch
+fi
+
+git checkout openroad
+git submodule update --init --recursive
+(cd tools/OpenROAD;
+ git checkout ${commit};
+ git submodule update --init --recursive)
