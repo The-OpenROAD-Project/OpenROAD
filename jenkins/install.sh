16a1ff1ed92285bb38f413380d49ea7ae11271d8
Matt Liberty <matt.liberty@gmail.com>
2020-02-26 11:42:46 -0800

    move flow tests to flow subdir for cleanup

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -2,25 +2,27 @@
 set -x
 set -e
 mkdir -p /OpenROAD/build
-cd /OpenROAD/build
-cmake ..
-make -j 4
-
-echo `whoami`
+cd /OpenROAD
+cmake -B build
+cmake --build build -j 4
 
 # Capture the commit we are testing for use in flow testing
 commit=`git rev-parse --verify HEAD`
 
-# setup OpenROAD-flow
-cd /OpenROAD-flow
-if [[ ! -d .git ]]; then
-    git clone -b openroad https://github.com/The-OpenROAD-Project/OpenROAD-flow.git /
-else
-    git fetch
+# Build a subdir 'flow' to run OpenROAD-flow tests in
+mkdir -p flow
+if [[ ! -d flow/OpenROAD-flow ]]; then
+    git clone -b openroad https://github.com/The-OpenROAD-Project/OpenROAD-flow.git flow
 fi
 
+cd flow/OpenROAD-flow
+
+# Get the head of the openroad branch of OpenROAD-flow
+git fetch
 git checkout openroad
 git submodule update --init --recursive
+
+# Swap to current commit of OpenROAD
 (cd tools/OpenROAD;
  git checkout ${commit};
  git submodule update --init --recursive)
