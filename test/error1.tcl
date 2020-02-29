6429a88fd86c2d71955dae4c6a55f0eb027c02f0
James Cherry <cherry@parallaxsw.com>
2020-02-29 14:12:38 -0800

    error1 force error

--- test/error1.tcl
+++ test/error1.tcl
@@ -1,7 +1,4 @@
 # error handling
-# cannot really test exit_on_error 1 because that will bounce us
-set exit_on_error 0
-
 if { [catch { ord::error "catch a luser" } result] } {
   puts "caught '$result'"
 }
@@ -13,3 +10,6 @@ if { [catch { sta::sta_error "sta lusing" } result] } {
 if { [catch { read_def xxx } result] } {
   puts "caught '$result'"
 }
+
+ord::error "last chance"
+puts "this message should not print because it follows an error"
