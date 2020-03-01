5d05d08a222dccbb9290d2a827c5c9ae84f13091
Ahmed Agiza <ahmed.agiza22@gmail.com>
2020-02-29 20:33:01 -0500

    Remove OpenPhySyn tests/tcl

--- src/OpenPhySyn/tests/tcl/read_lib.tcl
+++
@@ -1,4 +0,0 @@
-import_lib ../tests/data/libraries/gscl45nm/gscl45nm.lib
-print_lib
-puts "Read ok!"
-exit 0
--- src/OpenPhySyn/tests/tcl/sta_report_checks.tcl
+++
@@ -1,6 +0,0 @@
-import_lib ../tests/data/libraries/Nangate45/NangateOpenCellLibrary_typical.lib
-import_lef ../tests/data/libraries/Nangate45/NangateOpenCellLibrary.mod.lef
-import_def ../tests/data/designs/gcd/gcd.def
-sta create_clock [sta get_ports clk]  -name core_clock  -period 10
-sta report_checks -path_delay min
-exit 0
