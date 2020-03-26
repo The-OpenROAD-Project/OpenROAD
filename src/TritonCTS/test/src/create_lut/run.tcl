25554632b49a397ec37cd7fb2c6fe7e553f71948
Marcelo Freire Danigno <38264358+MarceloDanigno@users.noreply.github.com>
2020-03-26 15:53:56 -0300

    [TritonCTS] Fix create_lut test

--- src/TritonCTS/test/src/create_lut/run.tcl
+++ src/TritonCTS/test/src/create_lut/run.tcl
@@ -3,12 +3,5 @@ read_liberty "merged.lib"
 read_def "aes.def"
 read_verilog "aes.v"
 read_sdc "aes.sdc"
-
-clock_tree_synthesis -root_buf CLKBUF_X3 \
-                     -buf_list "BUF_X1" \
-                     -only_characterization 1 \
-                     -max_slew 10.0e-12 \
-                     -max_cap 30.0e-15 \
-                     -sqr_cap 7.7161e-5 \
-                     -sqr_res 3.8e-1 
+clock_tree_synthesis -buf_list "BUF_X1" -only_characterization 1 -max_slew 10.0e-12 -max_cap 30.0e-15 -sqr_cap 7.7161e-5 -sqr_res 3.8e-1
 
