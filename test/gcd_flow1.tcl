add5641b9ed09a1210165b1c9e28cb36e1b603f6
James Cherry <cherry@parallaxsw.com>
2020-03-27 12:06:56 -0700

    gcd_flow test disabled because cts prints time

--- test/gcd_flow1.tcl
+++ test/gcd_flow1.tcl
@@ -1,4 +1,5 @@
 # gcd flow pipe cleaner
+source "helpers.tcl"
 read_lef NangateOpenCellLibrary.lef
 read_liberty NangateOpenCellLibrary_typical.lib
 read_verilog gcd_nangate45.v
@@ -6,7 +7,7 @@ link_design gcd
 read_sdc gcd.sdc
 
 initialize_floorplan -site FreePDK45_38x28_10R_NP_162NW_34O \
-  -utilization 40 -tracks nangate45.tracks
+  -utilization 30
 auto_place_pins metal1
 
 pdngen -verbose gcd_pdn.cfg
@@ -14,12 +15,13 @@ pdngen -verbose gcd_pdn.cfg
 # Pre-sizing wireload timing
 report_checks
 
+set buffer_cell BUF_X4
 set_wire_rc -layer metal2
 resize -resize
-repair_max_cap -buffer_cell BUF_X4
-repair_max_slew -buffer_cell BUF_X4
-repair_max_fanout -max_fanout 100 -buffer_cell BUF_X4
-repair_hold_violations -buffer_cell BUF_X4
+repair_max_cap -buffer_cell $buffer_cell
+repair_max_slew -buffer_cell $buffer_cell
+repair_max_fanout -max_fanout 100 -buffer_cell $buffer_cell
+repair_hold_violations -buffer_cell $buffer_cell
 
 report_floating_nets -verbose
 report_design_area
@@ -29,8 +31,18 @@ report_checks
 
 global_placement -timing_driven
 
+clock_tree_synthesis -lut_file nangate45.lut \
+  -sol_list nangate45.sol_list \
+  -root_buf BUF_X4 \
+  -wire_unit 20
+
 set_placement_padding -global -right 8
 detailed_placement
 check_placement
 
+filler_placement FILL*
+
 report_checks
+
+set def_file [make_result_file gcd.def]
+write_def $def_file
