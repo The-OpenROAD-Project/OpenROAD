3ac79d3becd8d1a38bf6b822d98cd91a2bad45bf
Matt Liberty <matt.liberty@gmail.com>
2020-04-20 15:44:40 -0700

    Revert "temporarily make filler error into warning"
    This reverts commit e88d9ae45ceabeccd71e3b8011e9d76e2a8c441f.

--- src/opendp/src/FillerPlacement.cpp
+++ src/opendp/src/FillerPlacement.cpp
@@ -46,7 +46,6 @@ using std::min;
 using std::to_string;
 
 using ord::error;
-using ord::warn;
 
 using odb::dbLib;
 using odb::dbMaster;
@@ -126,13 +125,7 @@ void Opendp::placeRowFillers(const Grid* grid, int row)
       int gap = k - j;
       // printf("filling row %d gap %d %d:%d\n", row, gap, j, k - 1);
       dbMasterSeq& fillers = gapFillers(gap);
-      if (fillers.empty()) {
-        int x = core_.xMin() + k * site_width_;
-        int y = core_.yMin() + row * row_height_;
-        warn("No filler for row %d gap %d %d:%d (%d, %d)", row, gap, j, k - 1,
-             x, y);
-      }
-      k = j;
+      k                    = j;
       for (dbMaster* master : fillers) {
         string inst_name = "FILLER_" + to_string(row) + "_" + to_string(k);
         // printf(" filler %s %d\n", inst_name.c_str(), master->getWidth() /
@@ -175,7 +168,7 @@ dbMasterSeq& Opendp::gapFillers(int gap)
         }
       }
     }
-    // error("could not fill gap of size %d", gap);
+    error("could not fill gap of size %d", gap);
     return fillers;
   } else {
     return fillers;
