88f96f6c7ac5d10c47ab25b5e56f448fe2217b9c
James Cherry <cherry@parallaxsw.com>
2020-05-06 14:25:26 -0700

    gcd_resize ok

--- src/resizer/test/gcd_resize.tcl
+++ src/resizer/test/gcd_resize.tcl
@@ -14,8 +14,8 @@ repair_max_cap -buffer_cell $buffer_cell
 repair_max_slew -buffer_cell $buffer_cell
 repair_max_fanout -max_fanout 100 -buffer_cell $buffer_cell
 resize -dont_use {CLKBUF_* AOI211_X1 OAI211_X1}
-repair_tie_fanout -max_fanout 100 NangateOpenCellLibrary/LOGIC0_X1/Z
-repair_tie_fanout -max_fanout 100 NangateOpenCellLibrary/LOGIC1_X1/Z
+repair_tie_fanout -max_fanout 100 Nangate_typ/LOGIC0_X1/Z
+repair_tie_fanout -max_fanout 100 Nangate_typ/LOGIC1_X1/Z
 repair_hold_violations -buffer_cell $buffer_cell
 
 report_floating_nets -verbose
