6b45bed60601e7330fe2470c3f67c17f3cb347b1
Ahmed Ghazy <ax3ghazy@aucegypt.edu>
2020-05-11 18:32:59 +0200

    [init_fp] tracks orientation fixed when not using a tracks_file

--- src/init_fp/src/InitFloorplan.cc
+++ src/init_fp/src/InitFloorplan.cc
@@ -1,15 +1,15 @@
 // Copyright (c) 2019, Parallax Software, Inc.
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -452,7 +452,7 @@ InitFloorplan::makeTracks(Rect &die_area)
       uint width;
       int offset, pitch, track_count;
       switch (layer_dir) {
-      case dbTechLayerDir::HORIZONTAL:
+      case dbTechLayerDir::VERTICAL:
 	grid = dbTrackGrid::create(block_, layer);
 	width = die_area.dx();
 	pitch = layer->getPitchX();
@@ -466,7 +466,7 @@ InitFloorplan::makeTracks(Rect &die_area)
 	else
 	  printf("Error: layer %s has zero pitch.\n", layer->getConstName());
 	break;
-      case dbTechLayerDir::VERTICAL:
+      case dbTechLayerDir::HORIZONTAL:
 	grid = dbTrackGrid::create(block_, layer);
 	width = die_area.dy();
 	pitch = layer->getPitchY();
