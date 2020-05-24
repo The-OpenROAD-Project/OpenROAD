6c28416e02e9c14a0555d0b195006017518a57f0
James Cherry <cherry@parallaxsw.com>
2020-05-24 12:26:45 -0700

    init_fp use Nangate45

--- src/init_fp/test/init_floorplan4.tcl
+++ src/init_fp/test/init_floorplan4.tcl
@@ -1,12 +1,12 @@
 # init_floorplan -core_area non-multiples of site size
 source "helpers.tcl"
-read_lef liberty1.lef
+read_lef Nangate45/Nangate45.lef
+read_liberty Nangate45/Nangate45_typ.lib
 read_def reg1.def
-read_liberty liberty1.lib
 initialize_floorplan -die_area "0 0 1000 1000" \
   -core_area "110 110 900 900" \
-  -site site1
-auto_place_pins M1
+  -site FreePDK45_38x28_10R_NP_162NW_34O
+auto_place_pins metal1
 
 set def_file [make_result_file init_floorplan4.def]
 write_def $def_file
