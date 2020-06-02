f48b283ad28d668aefde0b4c805bcb9ed402ed47
Austin Rovinski <rovinski@umich.edu>
2020-06-01 22:38:31 -0400

    Revert "print user info for debug"
    This reverts commit 2ef2662cd2a9f58a2fbfca682f1aa573c6b70c24.

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -2,5 +2,4 @@
 set -x
 set -e
 docker build -t openroad/openroad --target base-dependencies .
-echo "User is ${USER} $(id -u ${USER})"
 docker run -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/OpenROAD openroad/openroad bash -c "./OpenROAD/jenkins/install.sh"
