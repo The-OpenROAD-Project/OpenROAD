ee16ae9acd42ad5034e6f6f0a6ed5d0a8df9a250
Matt Liberty <mattliberty@gmail.com>
2020-06-01 18:17:46 -0700

    try user name not uid

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -3,4 +3,4 @@ set -x
 set -e
 docker build -t openroad/openroad --target base-dependencies .
 echo "User is ${USER} $(id -u ${USER})"
-docker run -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/OpenROAD openroad/openroad bash -c "./OpenROAD/jenkins/install.sh"
+docker run -u ${USER} -v $(pwd):/OpenROAD openroad/openroad bash -c "./OpenROAD/jenkins/install.sh"
