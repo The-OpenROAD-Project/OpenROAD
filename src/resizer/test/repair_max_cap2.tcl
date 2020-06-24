a33b1617345c4d9cf4c683d5743a0251a8b89ae9
James Cherry <cherry@parallaxsw.com>
2020-06-24 09:24:50 -0700

    reorg repair_max_cap tests

--- src/resizer/test/repair_max_cap2.tcl
+++ src/resizer/test/repair_max_cap2.tcl
@@ -1,7 +1,6 @@
 # repair_max_cap unconstrained
 source "helpers.tcl"
 source "hi_fanout.tcl"
-source "report_max_cap.tcl"
 
 read_liberty Nangate45/Nangate45_typ.lib
 read_lef Nangate45/Nangate45.lef
@@ -9,6 +8,6 @@ set def_file [file join $result_dir "repair_max_cap2.def"]
 write_hi_fanout_def $def_file 40
 read_def $def_file
 
-report_max_cap 2
+report_check_types -max_capacitance
 repair_max_cap -buffer_cell BUF_X2
-report_max_cap 4
+report_check_types -max_capacitance
