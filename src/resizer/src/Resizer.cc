15605e3322957724029b45155724f1e86d70a698
James Cherry <cherry@parallaxsw.com>
2020-07-01 18:44:50 -0700

    repair_hold_violations try harder

--- src/resizer/src/Resizer.cc
+++ src/resizer/src/Resizer.cc
@@ -1114,18 +1114,21 @@ Resizer::repairHoldViolations(VertexSet &ends,
 			      LibertyCell *buffer_cell)
 {
   inserted_buffer_count_ = 0;
-  Slack worst_slack;
+  Slack worst_slack, prev_slack;
   Vertex *worst_vertex;
   sta_->worstSlack(MinMax::min(), worst_slack, worst_vertex);
+  prev_slack = worst_slack * 2;
 
   int pass = 1;
   while (worst_slack < 0.0
-	 && pass < 10) {
+	 // Make sure we are making progress.
+	 && abs(worst_slack - prev_slack) > abs(prev_slack) * .001) {
     debugPrint2(debug_, "repair_hold", 1, "pass %d worst_slack=%s\n",
 		pass,
 		units_->timeUnit()->asString(worst_slack, 3));
     repairHoldPass(ends, buffer_cell);
     sta_->findRequireds();
+    prev_slack = worst_slack;
     sta_->worstSlack(MinMax::min(), worst_slack, worst_vertex);
     pass++;
   }
