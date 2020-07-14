dd7c9f4c01ac212d6a0d7b8ac8d91514762da86a
Matt Liberty <matt.liberty@gmail.com>
2020-07-14 16:10:15 -0700

    remove extraneous tabs

--- jenkins/install_docker.sh
+++ jenkins/install_docker.sh
@@ -5,26 +5,26 @@ cd /OpenROAD
 cmake -B build
 time cmake --build build -j 8
 
-# Capture the commit we are testing for use in flow testing	
-commit=`git rev-parse --verify HEAD`	
+# Capture the commit we are testing for use in flow testing
+commit=`git rev-parse --verify HEAD`
 
-# Build a subdir 'flow' to run OpenROAD-flow tests in	
-if [[ ! -d flow ]]; then	
-    git clone -b openroad https://github.com/The-OpenROAD-Project/OpenROAD-flow.git flow	
-fi	
+# Build a subdir 'flow' to run OpenROAD-flow tests in
+if [[ ! -d flow ]]; then
+    git clone -b openroad https://github.com/The-OpenROAD-Project/OpenROAD-flow.git flow
+fi
 
-cd flow	
+cd flow
 
-# Get the head of the openroad branch of OpenROAD-flow	
-git pull --ff-only	
-git submodule update --init --recursive	
+# Get the head of the openroad branch of OpenROAD-flow
+git pull --ff-only
+git submodule update --init --recursive
 
-# Swap to current commit of OpenROAD	
-cd tools/OpenROAD	
-if ! git ls-remote local > /dev/null 2>&1; then	
-    git remote add local ../../..	
-fi	
-git fetch	
-git fetch local	
-git checkout ${commit}	
+# Swap to current commit of OpenROAD
+cd tools/OpenROAD
+if ! git ls-remote local > /dev/null 2>&1; then
+    git remote add local ../../..
+fi
+git fetch
+git fetch local
+git checkout ${commit}
 git submodule update --init --recursive
