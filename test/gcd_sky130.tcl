40b7490c1bed5c5fbe6ba11581d673592dd27b46
James Cherry <cherry@parallaxsw.com>
2020-07-21 17:11:11 -0700

    sky130

--- test/gcd_sky130.tcl
+++ test/gcd_sky130.tcl
@@ -8,6 +8,6 @@ set top_module "gcd"
 set sdc_file "gcd_sky130.sdc"
 set die_area {0 0 299.96 300.128}
 set core_area {9.996 10.08 289.964 290.048}
-set max_drv_count 10
+set max_drv_count 1
 
 source -echo "flow.tcl"
--- test/ibex_sky130.tcl
+++ test/ibex_sky130.tcl
@@ -8,6 +8,6 @@ set synth_verilog "ibex_sky130.v"
 set sdc_file "ibex_sky130.sdc"
 set die_area {0 0 3000.08 2999.8}
 set core_area {10.07 11.2 2990.01 2990}
-set max_drv_count 1
+set max_drv_count 60
 
 source -echo "flow.tcl"
