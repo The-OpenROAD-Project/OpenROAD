869669925e6e2e6b6383d4f59db0fda19b26d421
James Cherry <cherry@parallaxsw.com>
2020-07-22 19:03:04 -0700

    aes_sky130 floorplan

--- test/aes_sky130.tcl
+++ test/aes_sky130.tcl
@@ -6,8 +6,8 @@ set design "aes"
 set top_module "aes_cipher_top"
 set synth_verilog "aes_sky130.v"
 set sdc_file "aes_sky130.sdc"
-set die_area {0 0 620.15 620.6}
-set core_area {10.07 11.2 610.27 610.8}
+set die_area {0 0 1230.24 1228.77}
+set core_area {10.08 9.99 1220.16 1218.78}
 set max_drv_count 1
 
 source -echo "flow.tcl"
