a907c048a17be359cd1726a40cf9b16dcfe7ce7c
eder-matheus <emrmonteiro@inf.ufrgs.br>
2020-08-18 07:55:51 -0700

    [FastRoute] Remove unnecessary .sdc

--- src/FastRoute/test/clock_route.tcl
+++ src/FastRoute/test/clock_route.tcl
@@ -4,7 +4,9 @@ read_lef "sky130/sky130_std_cell.lef"
 
 read_def "gcd_sky130.def"
 
-read_sdc "clk.sdc"
+current_design gcd
+create_clock -name core_clock -period 2.0000 -waveform {0.0000 1.0000} [get_ports {clk}]
+set_propagated_clock [get_clocks {core_clock}]
 
 set guide_file [make_result_file clock_route.guide]
 
