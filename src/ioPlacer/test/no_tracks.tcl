669a1a2c1cb0c7216d7060851b1ff4e2d7a67da8
eder-matheus <emrmonteiro@inf.ufrgs.br>
2020-08-18 10:49:16 -0300

    [ioPlacer] Remove no_tracks.sdc

--- src/ioPlacer/test/no_tracks.tcl
+++ src/ioPlacer/test/no_tracks.tcl
@@ -1,8 +1,7 @@
 read_lef Nangate45/Nangate45.lef
 read_lib Nangate45/Nangate45_typ.lib
-read_verilog ./no_tracks.v
+read_verilog no_tracks.v
 link_design gcd
-read_sdc ./no_tracks.sdc
 
 initialize_floorplan -site FreePDK45_38x28_10R_NP_162NW_34O \
 -utilization 30
