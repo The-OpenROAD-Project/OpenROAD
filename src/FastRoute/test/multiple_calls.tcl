e7c2d78f1a9e7d3e4439d90b7662145aa124da63
eder-matheus <emrmonteiro@inf.ufrgs.br>
2020-08-18 14:01:19 -0700

    [FastRoute] Fix multiple_calls.tcl

--- src/FastRoute/test/multiple_calls.tcl
+++ src/FastRoute/test/multiple_calls.tcl
@@ -5,27 +5,12 @@ read_def "multiple_calls.def"
 set guide_file1 [make_result_file mc1_route.guide]
 set guide_file2 [make_result_file mc2_route.guide]
 
-FastRoute::set_output_file $guide_file1
-FastRoute::set_capacity_adjustment 0.0
-FastRoute::set_min_layer 1
-FastRoute::set_max_layer -1
-FastRoute::set_verbose 0
-FastRoute::set_grid_origin 0 0
-
-FastRoute::start_fastroute
-FastRoute::run_fastroute
-FastRoute::write_guides
+fastroute -output_file $guide_file1 \
+          -grid_origin {0 0}
 
 FastRoute::reset_fastroute
 
-FastRoute::set_output_file $guide_file2
-FastRoute::set_capacity_adjustment 0.8
-FastRoute::set_min_layer 1
-FastRoute::set_max_layer 10
-FastRoute::set_verbose 0
-
-FastRoute::start_fastroute
-FastRoute::run_fastroute
-FastRoute::write_guides
+fastroute -output_file $guide_file2 \
+          -capacity_adjustment 0.8 \
 
 diff_file $guide_file1 $guide_file2
