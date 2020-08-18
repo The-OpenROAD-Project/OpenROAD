82bd6a4e9965458120231bc572fe195acf99eafb
eder-matheus <emrmonteiro@inf.ufrgs.br>
2020-08-18 08:21:37 -0700

    [FastRoute] Update multiple_calls

--- src/FastRoute/test/multiple_calls.tcl
+++ src/FastRoute/test/multiple_calls.tcl
@@ -1,6 +1,6 @@
 source "helpers.tcl"
 read_lef "Nangate45/Nangate45.lef"
-read_def "gcd.def"
+read_def "multiple_calls.def"
 
 set guide_file1 [make_result_file mc1_route.guide]
 set guide_file2 [make_result_file mc2_route.guide]
@@ -19,7 +19,7 @@ FastRoute::write_guides
 FastRoute::reset_fastroute
 
 FastRoute::set_output_file $guide_file2
-FastRoute::set_capacity_adjustment 0.15
+FastRoute::set_capacity_adjustment 0.8
 FastRoute::set_min_layer 1
 FastRoute::set_max_layer 10
 FastRoute::set_verbose 0
@@ -27,3 +27,5 @@ FastRoute::set_verbose 0
 FastRoute::start_fastroute
 FastRoute::run_fastroute
 FastRoute::write_guides
+
+diff_file $guide_file1 $guide_file2
