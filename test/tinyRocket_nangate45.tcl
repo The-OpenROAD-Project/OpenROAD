5ee65e4b1f019602161f41f705310861459380d9
James Cherry <cherry@parallaxsw.com>
2020-08-19 18:09:01 -0700

    test/tinyRocket_gcd.tcl

--- test/tinyRocket_nangate45.tcl
+++ test/tinyRocket_nangate45.tcl
@@ -6,8 +6,8 @@ set design "tinyRocket"
 set top_module "RocketTile"
 set synth_verilog "tinyRocket_nangate45.v"
 set sdc_file "tinyRocket_nangate45.sdc"
-set extra_lef [glob Nangate45/fakeram45*.lef]
-set extra_liberty [glob Nangate45/fakeram45*.lib]
+set extra_lef [glob "Nangate45/fakeram45*.lef"]
+set extra_liberty [glob "Nangate45/fakeram45*.lib"]
 # These values must be multiples of placement site
 # x=0.19 y=1.4
 set die_area {0 0 924.92 799.4}
