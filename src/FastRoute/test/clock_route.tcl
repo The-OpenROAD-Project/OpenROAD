2bd58993519b6585489dd6b5a7cdd191128f3241
eder-matheus <emrmonteiro@inf.ufrgs.br>
2020-08-20 19:16:36 -0700

    [FastRoute] Update unit test scripts with new commands

--- src/FastRoute/test/clock_route.tcl
+++ src/FastRoute/test/clock_route.tcl
@@ -10,11 +10,17 @@ set_propagated_clock [get_clocks {core_clock}]
 
 set guide_file [make_result_file clock_route.guide]
 
+set_global_routing_layer_adjustment 2 0.8
+set_global_routing_layer_adjustment 3 0.7
+set_global_routing_layer_adjustment 4 0.5
+set_global_routing_layer_adjustment 5 0.5
+set_global_routing_layer_adjustment 6 0.5
+
+set_clock_route_flow 4 6
+
 fastroute -output_file $guide_file \
           -max_routing_layer 6 \
           -unidirectional_routing \
-          -layers_adjustments {{2 0.8} {3 0.7} {4 0.5} {5 0.5} {6 0.5}} \
-          -clock_nets_route_flow \
-          -min_layer_for_clock_net 4 \
 
 diff_file clock_route.guideok $guide_file
+
