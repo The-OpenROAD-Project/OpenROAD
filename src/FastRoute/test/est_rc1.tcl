2a0cae867c7b3d5da23924949bf5cbd9d599ab92
eder-matheus <emrmonteiro@inf.ufrgs.br>
2020-08-21 13:10:18 -0700

    [FastRoute] Update est_rc tests

--- src/FastRoute/test/est_rc1.tcl
+++ src/FastRoute/test/est_rc1.tcl
@@ -3,11 +3,8 @@ read_lef "Nangate45/Nangate45.lef"
 read_liberty Nangate45/Nangate45_typ.lib
 read_def "gcd.def"
 
-set guide_file [make_result_file gcd_route.guide]
-
-fastroute -output_file $guide_file \
-      -max_routing_layer 10 \
-      -unidirectional_routing
+fastroute -max_routing_layer 10 \
+          -unidirectional_routing
 estimate_parasitics -global_routing
 
 report_net -connections -verbose -digits 3 clk
