5579779d4c71926f49cb5b985a8246518f0b9b11
Vitor Bandeira <vitor.vbandeira@gmail.com>
2020-09-02 14:27:01 -0300

    Rename docker build tags

--- jenkins/build_docker.sh
+++ jenkins/build_docker.sh
@@ -2,8 +2,9 @@
 set -x
 set -e
 if [[ $# -eq 2 ]]; then
-    TARGET_OS=".$1.$2"
-    docker build -t "openroad/openroad$TARGET_OS" --target builder -f "./jenkins/docker/Dockerfile$TARGET_OS" .
+    TARGET_OS="$1"
+    TARGET_COMPILER="$2"
+    docker build -t "openroad/openroad_${TARGET_OS}_${TARGET_COMPILER}" --target builder -f "./jenkins/docker/Dockerfile.${TARGET_OS}.${TARGET_COMPILER}" .
 else
     docker build -t openroad/openroad --target base-dependencies .
     docker run -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/OpenROAD openroad/openroad bash -c "./OpenROAD/jenkins/install_docker.sh"
