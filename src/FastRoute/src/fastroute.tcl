2e2c3310fe0849fefffef969c73064353f2215d2
eder-matheus <emrmonteiro@inf.ufrgs.br>
2020-09-03 22:36:44 -0300

    [FastRoute] Add missing length check

--- src/FastRoute/src/fastroute.tcl
+++ src/FastRoute/src/fastroute.tcl
@@ -153,11 +153,15 @@ proc fastroute { args } {
 
   if { [info exists keys(-layers)] } {
     set layers $keys(-layers)
-    lassign $layers min_layer max_layer
-    sta::check_positive_integer "-layers" $min_layer
-    sta::check_positive_integer "-layers" $max_layer
-    FastRoute::set_min_layer $min_layer
-    FastRoute::set_max_layer $max_layer
+    if {[llength $layers] == 2} {
+      lassign $layers min_layer max_layer
+      sta::check_positive_integer "-layers" $min_layer
+      sta::check_positive_integer "-layers" $max_layer
+      FastRoute::set_min_layer $min_layer
+      FastRoute::set_max_layer $max_layer
+    } else {
+      ord::error "Wrong number of arguments for -layers"
+    }
   } else {
     FastRoute::set_min_layer 1
     FastRoute::set_max_layer -1
