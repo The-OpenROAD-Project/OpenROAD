f92b6a564e8df3354824bd4d95770c87f34130d3
eder-matheus <emrmonteiro@inf.ufrgs.br>
2020-09-10 14:22:44 -0300

    [FastRoute] Update repair_antennas1

--- src/FastRoute/test/repair_antennas1.tcl
+++ src/FastRoute/test/repair_antennas1.tcl
@@ -20,7 +20,7 @@ set_global_routing_layer_pitch 6 3.33
 fastroute -layers 2-6 \
           -unidirectional_routing
 
-repair_antennas sky130_fd_sc_hs__diode_2 DIODE
+repair_antennas sky130_fd_sc_hs__diode_2/DIODE
 
 set_placement_padding -global -left 0 -right 0
 check_placement
