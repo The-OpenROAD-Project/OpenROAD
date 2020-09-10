b44f288600100deed1b0ffc26b5926cb29fc8289
eder-matheus <emrmonteiro@inf.ufrgs.br>
2020-09-10 10:02:04 -0700

    [FastRoute] Rename test to repair_antennas1

--- src/FastRoute/test/repair_antennas1.tcl
+++ src/FastRoute/test/repair_antennas1.tcl
@@ -6,8 +6,8 @@ read_def "gcd_sky130.def"
 
 set_placement_padding -global -left 2 -right 2
 
-set guide_file [make_result_file diode_insert.guide]
-set def_file [make_result_file diode_insert.def]
+set guide_file [make_result_file repair_antennas1.guide]
+set def_file [make_result_file repair_antennas1.def]
 
 set_global_routing_layer_adjustment 3-6 0.15
 
@@ -28,5 +28,5 @@ check_placement
 write_guides $guide_file
 write_def $def_file
 
-diff_file diode_insert.guideok $guide_file
-diff_file diode_insert.defok $def_file
+diff_file repair_antennas1.guideok $guide_file
+diff_file repair_antennas1.defok $def_file
