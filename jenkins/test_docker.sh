2f41d7cded7f85a769219dcf24d50d3ffe14ab25
James Cherry <cherry@parallaxsw.com>
2020-09-12 11:15:02 -0700

    jenkins docker cleanup

--- jenkins/test_docker.sh
+++ jenkins/test_docker.sh
@@ -1,11 +1,4 @@
-# exit on failure of any test line	
-set -x	
 set -e	
-# Modify test/regression to add tests	
-if [[ $# -eq 2 ]]; then
-    TARGET_OS="$1"
-    TARGET_COMPILER="$2"
-    docker run "openroad/openroad_${TARGET_OS}_${TARGET_COMPILER}" bash -c "/OpenROAD/test/regression"
-else
-    docker run -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/test/regression"
-fi
+TARGET_OS="$1"
+TARGET_COMPILER="$2"
+docker run "openroad/openroad_${TARGET_OS}_${TARGET_COMPILER}" bash -c "/OpenROAD/test/regression"
