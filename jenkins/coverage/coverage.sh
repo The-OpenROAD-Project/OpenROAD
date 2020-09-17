6bfd1314c729f4f97a480b56f6f0648119f3c896
Vitor Bandeira <vitor.vbandeira@gmail.com>
2020-09-17 21:00:10 +0000

    Run regression tests for coverage

--- jenkins/coverage/coverage.sh
+++ jenkins/coverage/coverage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
 set -e
+./test/regression
 lcov --capture --directory ./build --exclude '/usr/include/*' --exclude '/opt/*' --exclude '/usr/lib/*' --exclude '/usr/local/*' --exclude '*build*' --output-file ./main_coverage.info
 genhtml ./main_coverage.info --output-directory ./out --ignore-errors source
