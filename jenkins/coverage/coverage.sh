0d4224731ca4d0dd33b4bbd9aba0cf0bea358d8b
Vitor Bandeira <vitor.vbandeira@gmail.com>
2020-09-17 20:50:40 +0000

    Use relative paths

--- jenkins/coverage/coverage.sh
+++ jenkins/coverage/coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 set -e
-lcov --capture --directory /OpenROAD/build --exclude '/usr/include/*' --exclude '/opt/*' --exclude '/usr/lib/*' --exclude '/usr/local/*' --exclude '*build*' --output-file /OpenROAD/main_coverage.info
-genhtml /OpenROAD/main_coverage.info --output-directory /OpenROAD/out --ignore-errors source
+lcov --capture --directory ./build --exclude '/usr/include/*' --exclude '/opt/*' --exclude '/usr/lib/*' --exclude '/usr/local/*' --exclude '*build*' --output-file ./main_coverage.info
+genhtml ./main_coverage.info --output-directory ./out --ignore-errors source
