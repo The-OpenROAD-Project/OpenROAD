25e537cce46721b17683c28824ad391bd64ad1d4
Vitor Bandeira <vitor.vbandeira@gmail.com>
2020-09-18 16:23:26 +0000

    Use coverage-output folder to hold results

--- jenkins/coverage/coverage.sh
+++ jenkins/coverage/coverage.sh
@@ -2,5 +2,6 @@
 set -x
 set -e
 ./test/regression
-lcov --capture --directory ./build --exclude '/usr/include/*' --exclude '/opt/*' --exclude '/usr/lib/*' --exclude '/usr/local/*' --exclude '*build*' --output-file ./main_coverage.info
-genhtml ./main_coverage.info --output-directory ./out --ignore-errors source
+mkdir -p coverage-output
+lcov --capture --directory ./build --exclude '/usr/include/*' --exclude '/opt/*' --exclude '/usr/lib/*' --exclude '/usr/local/*' --exclude '*build*' --output-file ./coverage-output/main_coverage.info
+genhtml ./coverage-output/main_coverage.info --output-directory ./coverage-output --ignore-errors source
