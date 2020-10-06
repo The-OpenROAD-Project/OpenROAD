f8661c4eb94ed7feb7d5009f83c023671a638bf3
James Cherry <cherry@parallaxsw.com>
2020-10-05 17:45:54 -0700

    repair_clk_nets seg fault if no prior delay calc

--- src/resizer/test/repair_clk_nets1.tcl
+++ src/resizer/test/repair_clk_nets1.tcl
@@ -6,7 +6,6 @@ create_clock in1 -period 10
 
 set_wire_rc -layer metal3
 estimate_parasitics -placement
-set_load 0 [get_net out1]
 
 foreach net_name {in1 n2 n2} {
   [sta::sta_to_db_net [get_net $net_name]] setSigType CLOCK
@@ -18,4 +17,4 @@ repair_clock_nets -max_wire_length 600 -buffer_cell BUF_X1
 puts [[sta::sta_to_db_net [get_net net2]] getSigType]
 
 report_long_wires 4
-report_checks -unconstrained -fields {input slew cap} -digits 3 -rise_to out1
+report_checks -unconstrained -fields {input slew cap} -rise_to out1
