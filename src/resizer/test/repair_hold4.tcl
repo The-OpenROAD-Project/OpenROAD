a83ccb814a0877b4ce395738db6d811d6a9cc148
James Cherry <cherry@parallaxsw.com>
2020-10-26 09:32:01 -0700

    repair_hold4 use sky130

--- src/resizer/test/repair_hold4.tcl
+++ src/resizer/test/repair_hold4.tcl
@@ -1,17 +1,18 @@
-# repair_hold_violations fanout regs with and without hold violations from data skew
+# repair_hold_violations repair_hold4 for sky130
 source helpers.tcl
-read_liberty Nangate45/Nangate45_typ.lib
-read_lef Nangate45/Nangate45.lef
+read_liberty sky130/sky130_tt.lib
+read_lef sky130/sky130_tech.lef
+read_lef sky130/sky130_std_cell.lef
 read_def repair_hold4.def
 
 create_clock -period 2 clk
 set_propagated_clock clk
 
-set_wire_rc -layer metal1
+set_wire_rc -layer met1
 estimate_parasitics -placement
 
 report_checks -path_delay min -format full_clock -digits 3 -group_count 3
 
-repair_hold_violations -buffer_cell BUF_X1
+repair_hold_violations -buffer_cell sky130_fd_sc_hs__dlygate4sd1_1
 
 report_checks -path_delay min -format full_clock -digits 3 -group_count 3
