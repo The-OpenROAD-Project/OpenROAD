ddca1dc162317d098c8a61e8d35d6e9256355845
James Cherry <cherry@parallaxsw.com>
2021-01-20 13:00:28 -0700

    opendp shift_move

--- src/opendp/src/Place.cpp
+++ src/opendp/src/Place.cpp
@@ -626,13 +626,14 @@ bool
 Opendp::shift_move(Cell *cell)
 {
   int x, y;
-  prePlaceLocation(cell, false, &x, &y);
+  prePlaceLocation(cell, true, &x, &y);
   int grid_x = gridX(x);
   int grid_y = gridY(y);
   // magic number alert
   int boundary_margin = 3;
+  int margin_width = paddedWidth(cell) * boundary_margin;
   set<Cell *> region_cells;
-  for (int x = grid_x - boundary_margin; x < grid_x + boundary_margin; x++) {
+  for (int x = grid_x - margin_width; x < grid_x + margin_width; x++) {
     for (int y = grid_y - boundary_margin; y < grid_y + boundary_margin; y++) {
       Pixel *pixel = gridPixel(x, y);
       if (pixel) {
