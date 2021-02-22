03ffda2d4b72d7a049a5ae7f6f24b668b81fbdc5
Stephanommg <stephanommg@yahoo.com.br>
2021-02-22 17:56:46 -0300

    Update FlexDR_graphics.h

--- src/TritonRoute/src/dr/FlexDR_graphics.h
+++ src/TritonRoute/src/dr/FlexDR_graphics.h
@@ -50,8 +50,6 @@ class frDesign;
 class FlexDRGraphics : public gui::Renderer
 {
  public:
-     
-  frDebugSettings*          settings_;
   
   // Debug InitialPlace
   FlexDRGraphics(frDebugSettings* settings, frDesign* design, odb::dbDatabase* db);
@@ -92,9 +90,7 @@ class FlexDRGraphics : public gui::Renderer
   // maps odb layerIdx -> tr layerIdx, with -1 for no equivalent
   std::vector<frLayerNum> layer_map_;
   std::vector<std::vector<frPoint>> points_by_layer_;
-  
-  
-//  void drawPath(gui::Painter& painter, std::vector<FlexMazeIdx>& path, frLayerNum ln);
+  frDebugSettings*          settings_;
   
 };
 
