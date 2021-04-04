3af1b89f5abc2735ffa4d6e42541f272f1ea07ab
osamahammad21 <osama21@aucegypt.edu>
2021-04-04 13:45:20 +0200

    edit create_sboxes.tcl

--- src/OpenDB/test/create_sboxes.tcl
+++ src/OpenDB/test/create_sboxes.tcl
@@ -11,13 +11,23 @@ set m1 [$tech findLayer metal1]
 set n1 [$block findNet VDD]
 set v1 [$block findVia VIA1]
 set swire [odb::dbSWire_create $n1 NONE ]
-set rect [odb::Rect]
-$rect init 0 0 100 100
-odb::createSBoxes $swire $m1 {$rect} NONE
-set point [odb::Point]
-$point setX 200
-$point setY 200
-odb::createSBoxes $swire $v1 {$point} NONE
+set rects {}
+for {set i 0} {$i < 10} {incr i} {
+    set start [expr {$i * 10}]
+    set end [expr {100 + $start}]
+    set rect [odb::Rect]
+    $rect init $start $start $end $end
+    lappend rects $rect
+}
+set points {}
+for {set i 200} {$i <= 1000} {set i [expr {$i + 100}]} {
+    set point [odb::Point]
+    $point setX $i
+    $point setY $i
+    lappend points $point
+}
+odb::createSBoxes $swire $m1 $rects NONE
+odb::createSBoxes $swire $v1 $points NONE
 set wires [$swire getWires]
 foreach box $wires {
     puts "[$box xMin] [$box yMin] [$box xMax] [$box yMax]"
