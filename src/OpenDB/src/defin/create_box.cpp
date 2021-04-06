580665e8952ce99f1f76c239bbe1b3fd66ff8a23
osamahammad21 <osama21@aucegypt.edu>
2021-04-06 13:51:44 +0200

    missing db logger

--- src/OpenDB/src/defin/create_box.cpp
+++ src/OpenDB/src/defin/create_box.cpp
@@ -56,7 +56,13 @@ void create_box(dbSWire* wire,
 
   if ((cur_x == prev_x) && (cur_y == prev_y))  // single point
   {
-    printf("(%d,%d) (%d,%d)\n", prev_x, prev_y, cur_x, cur_y);
+    logger->warn(utl::ODB,
+                 274,
+                 "Ambiguous path segment ({},{}) ({},{})",
+                 prev_x,
+                 prev_y,
+                 cur_x,
+                 cur_y);
     return;
     assert(0 && "ambiguous path segment");
   } else if (cur_x == prev_x)  // vert. path
