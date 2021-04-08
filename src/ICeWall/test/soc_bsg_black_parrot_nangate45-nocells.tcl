99d4bb7d939edeb0ca9773387ac4381009ae31c6
James Cherry <cherry@parallaxsw.com>
2021-04-08 12:20:17 -0700

    icewall tests use make-tracks

--- src/ICeWall/test/soc_bsg_black_parrot_nangate45-nocells.tcl
+++ src/ICeWall/test/soc_bsg_black_parrot_nangate45-nocells.tcl
@@ -18,8 +18,8 @@ if {[catch {ICeWall load_footprint soc_bsg_black_parrot_nangate45/bsg_black_parr
 initialize_floorplan \
   -die_area  {0 0 3000.000 3000.000} \
   -core_area {180.012 180.096 2819.964 2819.712} \
-  -tracks    [ICeWall get_tracks] \
   -site      FreePDK45_38x28_10R_NP_162NW_34O
+make_tracks
 
 if {[catch {ICeWall init_footprint soc_bsg_black_parrot_nangate45/soc_bsg_black_parrot.sigmap} msg]} {
   puts $errorInfo
