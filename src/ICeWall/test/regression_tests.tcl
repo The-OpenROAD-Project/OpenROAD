001b81614e3f7bb1746057d59129381225c28280
Colin Holehouse <colin.holehouse@arm.com>
2021-04-18 10:00:11 -0700

    New flipchip testcase with bump cell origin in the centre of the bump

--- src/ICeWall/test/regression_tests.tcl
+++ src/ICeWall/test/regression_tests.tcl
@@ -2,6 +2,7 @@ record_tests {
   soc_bsg_black_parrot_nangate45
   soc_bsg_black_parrot_nangate45.flipchip
   soc_bsg_black_parrot_nangate45-nocells
+  flipchip_test
   coyote_tc_sky130
   caravel_sky130
 }
