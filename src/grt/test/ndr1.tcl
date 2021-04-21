ebaa24e1033521b6aa6210e720d8b47473d1807d
eder-matheus <emrmonteiro@inf.ufrgs.br>
2021-04-21 20:51:21 -0300

    grt: test ndr1

--- src/grt/test/ndr1.tcl
+++ src/grt/test/ndr1.tcl
@@ -10,17 +10,31 @@ current_design gcd
 create_clock -name core_clock -period 2.0000 -waveform {0.0000 1.0000} [get_ports {clk}]
 set_propagated_clock [get_clocks {core_clock}]
 
-set guide_file [make_result_file clock_tracks_cost.guide]
+set db [ord::get_db]
+set block [[$db getChip] getBlock]
+set clk [$block findNet clk]
+
+create_ndr -name NDR \
+  	 	   	 -spacing { li1 0.51 met1 0.42 met2 0.42 met3 0.9 met4 0.9 met5 4.8 } \
+		   		 -width { li1 0.17 met1 0.14 met2 0.14 met3 0.3 met4 0.3 met5 1.6 }
+
+assign_ndr -ndr NDR -net clk
+assign_ndr -ndr NDR -net clknet_0_clk
+assign_ndr -ndr NDR -net clknet_2_0__leaf_clk
+assign_ndr -ndr NDR -net clknet_2_1__leaf_clk
+assign_ndr -ndr NDR -net clknet_2_2__leaf_clk
+assign_ndr -ndr NDR -net clknet_2_3__leaf_clk
+
+set guide_file [make_result_file ndr1.guide]
 
 set_global_routing_layer_adjustment met1 0.8
 set_global_routing_layer_adjustment met2 0.7
 set_global_routing_layer_adjustment * 0.5
 
 set_routing_layers -signal met1-met5 -clock met3-met5
-set_clock_routing -clock_tracks_cost 3
 
 global_route
 
 write_guides $guide_file
 
-diff_file clock_tracks_cost.guideok $guide_file
+diff_file ndr1.guideok $guide_file
--- src/grt/test/regression_tests.tcl
+++ src/grt/test/regression_tests.tcl
@@ -1,12 +1,12 @@
 record_tests {
   clock_route
-  clock_tracks_cost
   est_rc1
   est_rc2
   gcd
   invalid_routing_layer
   invalid_pin_placement
   multiple_calls
+  ndr1
   no_tracks
   region_adjustment
   repair_antennas1
