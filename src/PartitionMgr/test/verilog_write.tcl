4c0df5c16976fd1cc1b2df09e87d945d43d11f02
Peter Gadfort <peter.gadfort.civ@mail.mil>
2021-05-07 11:12:24 -0400

    par: verilog writing testcase, for when those are enabled

---
+++ src/PartitionMgr/test/verilog_write.tcl
@@ -0,0 +1,12 @@
+# Check if writing partition to verilog works
+source "helpers.tcl"
+read_lef "Nangate45/Nangate45.lef"
+read_liberty "Nangate45/Nangate45_typ.lib"
+read_verilog "gcd.v"
+link_design gcd
+
+set id [partition_netlist -tool chaco -num_partitions 2 -seeds {-289663928}]
+
+set v_file [make_result_file verilog_write.v]
+write_partitioning_to_verilog -partitioning_id $id $v_file
+diff_files verilog_write.vok $v_file
