9437f236f46806942f9d42bafa51e4fac2665516
Austin Rovinski <rovinski@umich.edu>
2021-05-25 00:44:04 -0400

    Update test files

--- src/TritonRoute/test/ispd18_sample.tcl
+++ src/TritonRoute/test/ispd18_sample.tcl
@@ -3,9 +3,9 @@ source "helpers.tcl"
 read_lef testcase/ispd18_sample/ispd18_sample.input.lef
 read_def testcase/ispd18_sample/ispd18_sample.input.def
 detailed_route -guide testcase/ispd18_sample/ispd18_sample.input.guide \
-               -outputguide results/ispd18_sample.output_guide.mod \
-               -outputDRC results/ispd18_sample.output_DRC.rpt \
-               -outputmaze results/ispd18_sample.output_Maze.log \
+               -output_guide results/ispd18_sample.output.guide.mod \
+               -output_drc results/ispd18_sample.output.drc.rpt \
+               -output_maze results/ispd18_sample.output.maze.log \
                -verbose 0
 
 set def_file [make_result_file ispd18_sample.def]
