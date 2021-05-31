881d3672a88c7af78628a23cdde934e7f47095b8
James Cherry <cherry@parallaxsw.com>
2021-05-30 18:00:22 -0700

    flow clk skew metrics

--- test/flow.tcl
+++ test/flow.tcl
@@ -1,4 +1,39 @@
-# assumes flow_helpers.tcl has been read
+############################################################################
+##
+## Copyright (c) 2019, OpenROAD
+## All rights reserved.
+##
+## BSD 3-Clause License
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met:
+##
+## * Redistributions of source code must retain the above copyright notice, this
+##   list of conditions and the following disclaimer.
+##
+## * Redistributions in binary form must reproduce the above copyright notice,
+##   this list of conditions and the following disclaimer in the documentation
+##   and/or other materials provided with the distribution.
+##
+## * Neither the name of the copyright holder nor the names of its
+##   contributors may be used to endorse or promote products derived from
+##   this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+## ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+## LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+## CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+## SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+## INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+## CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+## POSSIBILITY OF SUCH DAMAGE.
+##
+############################################################################
+
+# Assumes flow_helpers.tcl has been read.
 read_libraries
 read_verilog $synth_verilog
 link_design $top_module
@@ -140,8 +175,6 @@ write_verilog -remove_cells $filler_cells $verilog_file
 ################################################################
 # Detailed routing
 
-set detailed_routing 0
-if { $detailed_routing } {
 set_thread_count [exec getconf _NPROCESSORS_ONLN]
 detailed_route -guide $route_guide \
                -output_guide [make_result_file "${design}_${platform}_output_guide.mod"] \
@@ -154,16 +187,6 @@ utl::metric "DRT::drv" $drv_count
 
 set routed_def [make_result_file ${design}_${platform}_route.def]
 write_def $routed_def
-
-if { ![info exists drv_count] } {
-  fail "drv count not found."
-} elseif { $drv_count > $max_drv_count } {
-  fail "max drv count exceeded $drv_count > $max_drv_count."
-}
-} else {
-set rcx_rules_file ""
-utl::metric "DRT::drv" 0
-}
 
 ################################################################
 # Extraction
@@ -199,6 +222,8 @@ report_design_area
 utl::metric "worst_slack_min" [sta::worst_slack -min]
 utl::metric "worst_slack_max" [sta::worst_slack -max]
 utl::metric "tns_max" [sta::total_negative_slack -max]
+utl::metric "skew_setup" [sta::worst_clock_skew -setup]
+utl::metric "skew_hold" [sta::worst_clock_skew -hold]
 utl::metric "max_slew_violations" [sta::max_slew_violation_count]
 utl::metric "max_fanout_violations" [sta::max_fanout_violation_count]
 utl::metric "max_capacitance_violations" [sta::max_capacitance_violation_count]
