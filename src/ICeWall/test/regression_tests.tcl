6aab7cd70fc00c89eec30c2527e43507333f9f86
Colin Holehouse <colin.holehouse@arm.com>
2021-06-17 02:58:01 -0700

    Remove flipchip_test.api test, as this is the incorrect API, hte correct API covered in the tcl_interface.flipchip test

--- src/ICeWall/test/regression_tests.tcl
+++ src/ICeWall/test/regression_tests.tcl
@@ -3,7 +3,6 @@ record_tests {
   soc_bsg_black_parrot_nangate45.flipchip
   soc_bsg_black_parrot_nangate45-nocells
   flipchip_test
-  flipchip_test.api
   flipchip_test.m9
   coyote_tc_sky130
   caravel_sky130
