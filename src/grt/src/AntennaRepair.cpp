eda7ebbc9944ee4b4e0b5a6489d2b66402a24d4d
eder-matheus <eder.matheus.monteiro@gmail.com>
2021-06-23 16:00:14 -0300

    grt: allow dpl to move illegal placed diodes

--- src/grt/src/AntennaRepair.cpp
+++ src/grt/src/AntennaRepair.cpp
@@ -281,9 +281,11 @@ void AntennaRepair::insertDiode(odb::dbNet* net,
   odb::Rect instRect;
   antennaInst->getBBox()->getBox(instRect);
 
-  // allow detailed placement to move diodes with geometry out of the core area
-  // or near macro pins (can be placed out of row)
-  if (coreArea.contains(instRect) && !sinkInst->getMaster()->isBlock()) {
+  // allow detailed placement to move diodes with geometry out of the core area,
+  // or near macro pins (can be placed out of row), or illegal placed diodes
+  if (coreArea.contains(instRect) &&
+      !sinkInst->getMaster()->isBlock() &&
+      legallyPlaced) {
     antennaInst->setPlacementStatus(odb::dbPlacementStatus::FIRM);
   } else {
     antennaInst->setPlacementStatus(odb::dbPlacementStatus::PLACED);
