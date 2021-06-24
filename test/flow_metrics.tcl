dd8f441a401809577d9c30256547f00e9d1a695c
James Cherry <cherry@parallaxsw.com>
2021-06-23 22:17:03 -0700

    update flow metrics

--- test/flow_metrics.tcl
+++ test/flow_metrics.tcl
@@ -33,6 +33,8 @@
 ##
 ############################################################################
 
+source ~/tmp/json.tcl
+
 # Functions/variables common to metrics scripts.
 
 proc define_metric { name short_name fmt cmp_op margin_expr } {
@@ -110,9 +112,6 @@ define_metric "clock_period" "" "%d" "<=" {0}
 # Used by regression.tcl to check pass/fail metrics test.
 # Returns "pass" or failing metric comparison string.
 proc check_test_metrics { test } {
-  # Don't require json until it is really needed.
-  package require json
-
   set metrics_file [test_metrics_result_file $test]
   set metrics_limits_file [test_metrics_limits_file $test]
   if { ![file exists $metrics_file] } {
@@ -181,9 +180,6 @@ proc report_metrics_header {} {
 }
 
 proc report_test_metrics { test } {
-  # Don't require json until it is really needed.
-  package require json
-
   set metrics_result_file [test_metrics_result_file $test]
   if { [file exists $metrics_result_file] } {
     set stream [open $metrics_result_file r]
@@ -236,9 +232,6 @@ proc compare_flow_metrics_main {} {
 }
 
 proc compare_test_metrics { test relative } {
-  # Don't require json until it is really needed.
-  package require json
-
   set metrics_file [test_metrics_file $test]
   set result_file [test_metrics_result_file $test]
   if { [file exists $metrics_file] \
@@ -283,7 +276,7 @@ proc compare_test_metrics { test relative } {
 
 # Copy result metrics to test results saved in the repository.
 proc save_flow_metrics_main {} {
-  global argv argc
+  global argc argv
 
   if { $argv == "help" || $argv == "-help" } {
     puts {Usage: save_flow_metrics [test1]...}
@@ -313,29 +306,22 @@ proc save_metrics { test } {
 
 # Save result metrics + margins as metric limits.
 proc save_flow_metric_limits_main {} {
-  global argv
+  global argc argv
   if { $argv == "help" || $argv == "-help" } {
     puts {Usage: save_flow_metric_limits [failures] test1 [test2]...}
   } else {
-    if { $argv == "failures" } {
-      set tests [failed_tests]
+    if { $argc == 0 } {
+      set tests [expand_tests "flow"]
     } else {
-      set tests $argv
+      set tests [expand_tests $argv]
     }
     foreach test $tests {
-      if { [lsearch [group_tests "all"] $test] == -1 } {
-        puts "Error: test $test not found."
-      } else {
-        save_metric_limits $test
-      }
+      save_metric_limits $test
     }
   }
 }
 
 proc save_metric_limits { test } {
-  # Don't require json until it is really needed.
-  package require json
-  
   set metrics_file [test_metrics_result_file $test]
   set metrics_limits [test_metrics_limits_file $test]
   if { ! [file exists $metrics_file] } {
