14c56e0f419d6cd6c87d865de4f3e30a8dd48eed
Sanjiv Mathur <mathur.sanjiv@gmail.com>
2021-07-05 04:32:58 +0000

    Remove exit

--- src/rmp/test/const_cell_removal.tcl
+++ src/rmp/test/const_cell_removal.tcl
@@ -11,5 +11,3 @@ set tielo "LOGIC0_X1/Z"
 restructure -liberty_file Nangate45/Nangate45_typ.lib -target area -abc_logfile results/abc_rcon.log  -tielo_port $tielo -tiehi_port $tiehi
 
 report_design_area
-
-exit
