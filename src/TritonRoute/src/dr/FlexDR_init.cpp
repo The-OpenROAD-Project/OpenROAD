3aac04652940bcb4ce2af55054a785314302b919
stephano <stephano@stephano-VirtualBox>
2021-07-13 19:23:46 -0300

    remove on-the-fly ap temp fix

--- src/TritonRoute/src/dr/FlexDR_init.cpp
+++ src/TritonRoute/src/dr/FlexDR_init.cpp
@@ -978,8 +978,7 @@ void FlexDRWorker::initNet_termGenAp_new(const frDesign* design, drPin* dPin)
           if (!boost::polygon::intersect(pinRect, routeRect)) {
             continue;
           }
-          bool noJog = getTech()->getLayer(currLayerNum)->
-                        getLef58RightWayOnGridOnlyConstraint() != nullptr;
+          
           // pinRect now equals intersection of pinRect and routeRect
           auto currPrefRouteDir = getTech()->getLayer(currLayerNum)->getDir();
           // get intersecting tracks if any
@@ -1040,10 +1039,8 @@ void FlexDRWorker::initNet_termGenAp_new(const frDesign* design, drPin* dPin)
           } else {
             yLoc = (yl(pinRect) + yh(pinRect)) / 2;
           }
-          bool halfOnTrackFound = !yLocs.empty() || !xLocs.empty();
-          if (currLayerNum >= BOTTOM_ROUTING_LAYER && (!noJog || 
-                halfOnTrackFound) && (!isInitDR() || (xLoc != xh(routeRect) || 
-                yLoc != yh(routeRect)))) {
+          if (!isInitDR() || (xLoc != xh(routeRect) || 
+                yLoc != yh(routeRect))) {
             // TODO: update as drAccessPattern updated
             auto uap = std::make_unique<drAccessPattern>();
             frPoint pt(xLoc, yLoc);
