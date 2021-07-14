531726b51d3bd6809a8521e71d18b1cb4fea1f65
QuantamHD <emahintorabi@gmail.com>
2021-07-14 13:10:22 -0700

    changing test to use write_abstract_lef

--- src/OpenDB/test/gcd_abstract_lef.tcl
+++ src/OpenDB/test/gcd_abstract_lef.tcl
@@ -7,6 +7,6 @@ read_def "data/gcd/gcd_nangate45_route.def"
 
 set lef_file [make_result_file gcd_abstract_lef.lef]
 
-[ord::get_db_block] saveLef $lef_file
+write_abstract_lef $lef_file
 
 diff_file $lef_file "gcd_abstract_lef.lefok"
