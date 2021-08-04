36f51c03e9f85a6915550c553f2d95ec03d499a4
ibrahimkhairy <ibrahimkhairy@ymail.com>
2021-08-04 10:39:08 +0000

    adding regression tests
    Signed-off-by: ibrahimkhairy <ibrahimkhairy@ymail.com>

--- src/ICeWall/test/regression_tests.tcl
+++ src/ICeWall/test/regression_tests.tcl
@@ -5,6 +5,9 @@ record_tests {
   flipchip_test
   flipchip_test.m9
   coyote_tc_sky130
+  coyote_tc_sky130.some_pad_locations
+  coyote_tc_sky130.no_pad_locations
+  coyote_tc_sky130.not_enough_space
   caravel_sky130
   tcl_interface
   tcl_interface.flipchip
