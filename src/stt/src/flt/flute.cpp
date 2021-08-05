db49d502f324137115de7526352648cc0f5ab000
Eder Monteiro <eder.matheus.monteiro@gmail.com>
2021-08-05 15:17:51 -0300

    stt: restore pt and ptp sizes, fix stable_sort call
    Signed-off-by: Eder Monteiro <eder.matheus.monteiro@gmail.com>

--- src/stt/src/flt/flute.cpp
+++ src/stt/src/flt/flute.cpp
@@ -996,8 +996,8 @@ Tree flute(const std::vector<DTYPE>& x, const std::vector<DTYPE>& y, int acc) {
     xs.resize(d);
     ys.resize(d);
     s.resize(d);
-    pt = (struct point *)malloc(sizeof(struct point) * (d));
-    std::vector<point*> ptp(d);
+    pt = (struct point *)malloc(sizeof(struct point) * (d+1));
+    std::vector<point*> ptp(d+1);
 
     for (i = 0; i < d; i++) {
       pt[i].x = x[i];
@@ -1021,7 +1021,7 @@ Tree flute(const std::vector<DTYPE>& x, const std::vector<DTYPE>& y, int acc) {
         ptp[minidx] = tmpp;
       }
     } else {
-      std::stable_sort(ptp.begin(), ptp.end(), orderx);
+      std::stable_sort(ptp.begin(), ptp.end()-1, orderx);
     }
 
 #if FLUTE_REMOVE_DUPLICATE_PIN == 1
@@ -1061,7 +1061,7 @@ Tree flute(const std::vector<DTYPE>& x, const std::vector<DTYPE>& y, int acc) {
       ys[d - 1] = ptp[d - 1]->y;
       s[d - 1] = ptp[d - 1]->o;
     } else {
-      std::stable_sort(ptp.begin(), ptp.end(), ordery);
+      std::stable_sort(ptp.begin(), ptp.end()-1, ordery);
       for (i = 0; i < d; i++) {
         ys[i] = ptp[i]->y;
         s[i] = ptp[i]->o;
