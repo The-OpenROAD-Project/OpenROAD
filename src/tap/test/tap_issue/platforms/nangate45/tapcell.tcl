7d30cfac25dacdcf5bd4211631b15478851bb4ac
Salmaafifi98 <salmaafifi98@aucegypt.edu>
2021-08-09 11:53:27 -0600

    removed tap_issue test folder

--- src/tap/test/tap_issue/platforms/nangate45/tapcell.tcl
+++
@@ -1,5 +0,0 @@
-tapcell \
-  -endcap_cpp "2" \
-  -distance 120 \
-  -tapcell_master "TAPCELL_X1" \
-  -endcap_master "TAPCELL_X1"
--- src/tap/test/tap_issue/run-me-bp_fe-nangate45-base.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source vars-bp_fe-nangate45-base.sh
-./openroad -no_init ./scripts/tapcell.tcl
