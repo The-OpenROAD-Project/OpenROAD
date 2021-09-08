3533130131a62348d83ac6b0a75cb15787564d59
Peter Gadfort <peter.gadfort.civ@army.mil>
2021-09-07 20:18:27 -0400

    gui: fix copyright
    Signed-off-by: Peter Gadfort <peter.gadfort.civ@army.mil>

--- src/gui/src/ruler.h
+++ src/gui/src/ruler.h
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // BSD 3-Clause License
 //
-// Copyright (c) 2019, The Regents of the University of California
+// Copyright (c) 2021, The Regents of the University of California
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
