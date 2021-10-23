3f7872bf274da192b48d34c1e7afdeebd79dd3fb
James Cherry <cherry@parallaxsw.com>
2021-10-23 09:53:51 -0700

    rsz OR-496 Resizer does not take driving_cell max fanout into account
    Signed-off-by: James Cherry <cherry@parallaxsw.com>

--- src/rsz/test/repair_fanout5.tcl
+++ src/rsz/test/repair_fanout5.tcl
@@ -1,11 +1,13 @@
-# repair_design max_fanout non-liberty loads
+# repair_design max_fanout with set_max_fanout with input fanout > max_fanout
 source "helpers.tcl"
 read_liberty Nangate45/Nangate45_typ.lib
 read_lef Nangate45/Nangate45.lef
-read_lef repair_fanout3.lef
-read_def repair_fanout3.def
-
+read_def repair_fanout4.def
 set_max_fanout 10 [current_design]
+
+set_wire_rc -layer metal1
+estimate_parasitics -placement
+
 report_check_types -max_fanout
 repair_design
 report_check_types -max_fanout
