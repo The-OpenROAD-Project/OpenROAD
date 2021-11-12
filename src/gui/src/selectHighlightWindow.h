6b39391b107820f33b852a2bf1f836ed72e3f146
Peter Gadfort <peter.gadfort.civ@army.mil>
2021-11-12 15:02:56 -0500

    gui: correct selection behavior after sorting list
    Signed-off-by: Peter Gadfort <peter.gadfort.civ@army.mil>
    Signed-off-by: mgwoo <mwoo@eng.ucsd.edu>

--- src/gui/src/selectHighlightWindow.h
+++ src/gui/src/selectHighlightWindow.h
@@ -42,6 +42,7 @@
 #include <QModelIndex>
 #include <QPoint>
 #include <QShortcut>
+#include <QSortFilterProxyModel>
 #include <QStringList>
 #include <QStyledItemDelegate>
 #include <QToolBar>
@@ -168,7 +169,10 @@ class SelectHighlightWindow : public QDockWidget
  private:
   Ui::SelectHighlightWidget ui_;
   SelectionModel selection_model_;
+  QSortFilterProxyModel* sel_filter_proxy_;
+
   HighlightModel highlight_model_;
+  QSortFilterProxyModel* hlt_filter_proxy_;
 
   QMenu* select_context_menu_;
   QMenu* highlight_context_menu_;
