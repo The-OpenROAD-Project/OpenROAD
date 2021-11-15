8f78eab7e09de3b9132d9b6b511aa39e7f411a88
mgwoo <mwoo@eng.ucsd.edu>
2021-11-14 23:24:46 -0800

    update tcl based on PR comments
    Signed-off-by: mgwoo <mwoo@eng.ucsd.edu>

--- src/gpl/src/replace.tcl
+++ src/gpl/src/replace.tcl
@@ -270,15 +270,13 @@ proc global_placement { args } {
         catch {gpl::replace_nesterov_place_cmd} error
         set block [ord::get_db_block]
 
-        set diverge_codes [list "0304" "0305" "0306" "0307"]
+        set diverge_codes [list "GPL-0304" "GPL-0305" "GPL-0306" "GPL-0307"]
         set is_diverged 0
-        foreach code $diverge_codes {
-          if {[string first $code $error] != -1 } {
-            set is_diverged 1
-          }
+        if {[lsearch -exact $diverge_codes $error] >= 0} {
+          set is_diverged 1
         }
         if {$is_diverged && [llength [$block getInsts]] <= 500} {
-          utl::warn GPL 140 "RePlAce divergence detected. Rerun RePlAce up to BiCGSTAB."
+          utl::warn GPL 140 "RePlAce divergence detected. Rerunning RePlAce up to BiCGSTAB due to a design size <= 500 instances. This warning can be avoided by using the '-skip_nesterov_place' flag"
           gpl::replace_reset_cmd
           gpl::replace_initial_place_cmd
         }
