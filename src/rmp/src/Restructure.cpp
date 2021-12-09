4064432b6598bfb9d132ffff901d92909156c307
Tom Spyrou <46945466+tspyrou@users.noreply.github.com>
2021-12-09 12:56:16 -0800

    calling abc works read filetype error exists
    Signed-off-by: Tom Spyrou <46945466+tspyrou@users.noreply.github.com>

--- src/rmp/src/Restructure.cpp
+++ src/rmp/src/Restructure.cpp
@@ -36,6 +36,7 @@
 #include "rmp/Restructure.h"
 
 #include <stdio.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -69,6 +70,37 @@
 using utl::RMP;
 
 namespace rmp {
+
+////////////////////////////////////////////////////////////////////////
+///                        DECLARATIONS                              ///
+////////////////////////////////////////////////////////////////////////
+
+#if defined(ABC_NAMESPACE)
+namespace ABC_NAMESPACE
+{
+#elif defined(__cplusplus)
+extern "C"
+{
+#endif
+
+// procedures to start and stop the ABC framework
+// (should be called before and after the ABC procedures are called)
+void   Abc_Start();
+void   Abc_Stop();
+
+// procedures to get the ABC framework and execute commands in it
+typedef struct Abc_Frame_t_ Abc_Frame_t;
+
+Abc_Frame_t * Abc_FrameGetGlobalFrame();
+int    Cmd_CommandExecute( Abc_Frame_t * pAbc, const char * sCommand );
+
+#if defined(ABC_NAMESPACE)
+}
+using namespace ABC_NAMESPACE;
+#elif defined(__cplusplus)
+}
+#endif
+  
 
 void Restructure::init(ord::OpenRoad* openroad)
 {
@@ -218,7 +250,25 @@ void Restructure::runABC()
                  1,
                  "Writing ABC script file {}.",
                  abc_script_file);
+
       if (writeAbcScript(abc_script_file)) {
+	// Call linked abc
+	Abc_Frame_t * pAbc;
+	char Command[1000];
+
+	Abc_Start();
+	pAbc = Abc_FrameGetGlobalFrame();
+
+	sprintf( Command, "read %s", abc_script_file.c_str() );
+	if ( Cmd_CommandExecute( pAbc, Command ) )
+	  {
+	    fprintf( stdout, "Cannot execute command \"%s\".\n", Command );
+	    return;
+	  }
+      
+	Abc_Stop();
+	// Exit linked abc
+
         std::string abc_command = std::string("abc -F ") + abc_script_file;
         if (logfile_ != "")
           abc_command
