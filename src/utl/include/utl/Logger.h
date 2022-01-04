67a2d3d914589a6dbba8d237d2197a299a20a9b8
Davit Markarian <davit@udxs.me>
2022-01-04 18:28:53 +0100

    Fix metrics logging for non-string types. Update STA.
    Signed-off-by: Davit Markarian <davit@udxs.me>

--- src/utl/include/utl/Logger.h
+++ src/utl/include/utl/Logger.h
@@ -169,7 +169,7 @@ class Logger
   // API as we are writing JSON not user messages.
   // Note: these methods do no escaping so avoid special characters.
   template <typename T,
-            typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+            std::enable_if_t<std::is_arithmetic<T>>* = nullptr>
   inline void metric(const std::string_view metric,
                      T value)
   {
