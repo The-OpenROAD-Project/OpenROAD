8472dd45a21be9156605cfce68eb5bb055cb8919
Peter Gadfort <peter.gadfort.civ@army.mil>
2022-01-12 09:34:30 -0500

    gui: adding loupe icon as focus icon in inspector
    Signed-off-by: Peter Gadfort <peter.gadfort.civ@army.mil>

--- src/gui/src/inspector.cpp
+++ src/gui/src/inspector.cpp
@@ -808,7 +808,8 @@ void Inspector::makeAction(const Descriptor::Action& action)
     {"Delete", ":/delete.png"},
     {"Zoom to", ":/zoom_to.png"},
     {"Remove from highlight", ":/highlight_off.png"},
-    {"Add to highlight", ":/highlight_on.png"}
+    {"Add to highlight", ":/highlight_on.png"},
+    {"Focus", ":/focus.png"}
   };
   std::vector<std::pair<std::string, QString>> symbol_replacements{
     {"Fanin Cone", "\u25B7"},
