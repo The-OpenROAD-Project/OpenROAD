a811aaef498e5c93badfd4113e7b2450b053ce22
stephano <stephanommg@yahoo.com.br>
2022-02-23 17:35:46 -0300

    test clipsize 8-30
    Signed-off-by: stephano <stephanommg@yahoo.com.br>

--- src/drt/src/dr/FlexDR.cpp
+++ src/drt/src/dr/FlexDR.cpp
@@ -1714,8 +1714,8 @@ void FlexDR::searchRepair(int iter,
   auto& xgp = gCellPatterns.at(0);
   auto& ygp = gCellPatterns.at(1);
   int clipSize = size;
-  if (iter >= 10 && iter < 30)
-      clipSize += (iter - 9);
+  if (iter >= 8 && iter < 30)
+      clipSize += (iter - 7);
   int cnt = 0;
   int tot = (((int) xgp.getCount() - 1 - offset) / clipSize + 1)
             * (((int) ygp.getCount() - 1 - offset) / clipSize + 1);
