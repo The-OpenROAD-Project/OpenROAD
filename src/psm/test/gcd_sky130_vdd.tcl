51f85fb0191cb1aa8b8b70a00d0462e1db2935ce
VidyaChhabria <chhab011@umn.edu>
2022-02-22 23:55:22 -0800

    [psm] Remove platform files and create links
    Signed-off-by: VidyaChhabria <chhab011@umn.edu>

--- src/psm/test/gcd_sky130_vdd.tcl
+++ src/psm/test/gcd_sky130_vdd.tcl
@@ -1,9 +1,7 @@
-read_lef sky130_data/lef/sky130_fd_sc_hd.tlef  
-read_lef  sky130_data/lef/sky130_fd_sc_hd_merged.lef  
-read_lef sky130_data/lef/sky130io_fill.lef
+read_lef sky130_data/sky130hd.tlef
+read_lef  sky130_data/sky130hd_std_cell.lef 
 read_def sky130_data/gcd_sky130hd_floorplan.def
-read_liberty sky130_data/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-read_liberty sky130_data/lib/sky130_dummy_io.lib  
+read_liberty sky130_data/sky130hd_tt.lib
 read_sdc sky130_data/gcd_sky130hd_floorplan.sdc
 
 check_power_grid -net VDD 
