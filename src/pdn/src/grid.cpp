b09e09611f049b92c4e937d193097c8e810ad55d
Peter Gadfort <peter.gadfort.civ@army.mil>
2022-04-15 11:15:54 -0400

    pdn: remove partially overlapping vias
    Signed-off-by: Peter Gadfort <peter.gadfort.civ@army.mil>

--- src/pdn/src/grid.cpp
+++ src/pdn/src/grid.cpp
@@ -700,6 +700,30 @@ void Grid::makeVias(const ShapeTreeMap& global_shapes,
              remove_vias.size());
   remove_set_of_vias(remove_vias);
 
+  // remove vias are only partially overlapping
+  for (const auto& via : vias) {
+    const auto& via_area = via->getArea();
+    const auto& lower = via->getLowerShape()->getRect();
+    const auto& upper = via->getUpperShape()->getRect();
+    if (via_area.xMin() == lower.xMin() && via_area.xMax() == lower.xMax() &&
+        via_area.yMin() == upper.yMin() && via_area.yMax() == upper.yMax()) {
+      continue;
+    } else if (via_area.yMin() == lower.yMin() && via_area.yMax() == lower.yMax() &&
+               via_area.xMin() == upper.xMin() && via_area.xMax() == upper.xMax()) {
+      continue;
+    } else if (lower.contains(upper) || upper.contains(lower)) {
+      continue;
+    }
+    remove_vias.insert(via);
+  }
+  debugPrint(getLogger(),
+             utl::PDN,
+             "Via",
+             2,
+             "Removing {} vias due to partial overlap.",
+             remove_vias.size());
+  remove_set_of_vias(remove_vias);
+
   // build via tree
   vias_.clear();
   for (auto& via : vias) {
