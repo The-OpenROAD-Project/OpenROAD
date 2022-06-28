fc4e333cfdc44f47211d5e62c8f7044e626a5f34
habibayassin <habibayassin@aucegypt.edu>
2022-06-28 16:23:57 +0000

    cleaning up
    Signed-off-by: habibayassin <habibayassin@aucegypt.edu>

--- src/cts/test/check_buffers.tcl
+++ src/cts/test/check_buffers.tcl
@@ -7,7 +7,6 @@ read_def check_buffers.def
 create_clock -period 5 clk
 set_wire_rc -clock -layer metal5
 
-cts::clock_tree_synthesis_debug
 clock_tree_synthesis -root_buf CLKBUF_X3 \
                      -buf_list CLKBUF_X3 \
                      -wire_unit 20 \
