6ee9d95aac303ba2a1c226a40ab3a91d26bd34fb
Eder Monteiro <eder.matheus.monteiro@gmail.com>
2022-07-06 17:30:27 -0300

    tap: update n45 unit tests to use proper cell
    Signed-off-by: Eder Monteiro <eder.matheus.monteiro@gmail.com>

--- src/tap/test/gcd_nangate45.tcl
+++ src/tap/test/gcd_nangate45.tcl
@@ -5,7 +5,7 @@ read_def gcd_nangate45.def
 
 set def_file [make_result_file gcd_nangate45.def]
 
-tapcell -distance "20" -tapcell_master "FILLCELL_X1" -endcap_master "FILLCELL_X1"
+tapcell -distance "20" -tapcell_master "TAPCELL_X1" -endcap_master "TAPCELL_X1"
 
 write_def $def_file
 
--- src/tap/test/multiple_calls.tcl
+++ src/tap/test/multiple_calls.tcl
@@ -3,12 +3,12 @@ read_lef Nangate45/Nangate45.lef
 read_lef Nangate45/fakeram45_64x7.lef
 read_def gcd_ripup.def
 
-tapcell -distance "20" -tapcell_master "FILLCELL_X1" -endcap_master "FILLCELL_X1"
+tapcell -distance "20" -tapcell_master "TAPCELL_X1" -endcap_master "TAPCELL_X1"
 
 set def_file1 [make_result_file mc1.def]
 write_def $def_file1
 
-tapcell -distance "10" -tapcell_master "FILLCELL_X1" -endcap_master "FILLCELL_X1"
+tapcell -distance "10" -tapcell_master "TAPCELL_X1" -endcap_master "TAPCELL_X1"
 
 set def_file2 [make_result_file mc2.def]
 write_def $def_file2
