99ecb401c46b047a5f569218d4bcc33059e7dcbe
Eder Monteiro <eder.matheus.monteiro@gmail.com>
2022-07-21 18:21:33 -0300

    odb: simplify cutRows
    Signed-off-by: Eder Monteiro <eder.matheus.monteiro@gmail.com>

--- src/odb/src/zutil/util.cpp
+++ src/odb/src/zutil/util.cpp
@@ -182,15 +182,15 @@ void cutRows(dbBlock* block,
 
   // Gather rows needing to be cut up front
   for (dbRow* row : block->getRows()) {
-    std::map<dbRow*, vector<dbBox*>> row_blockages;
+    std::vector<dbBox*> row_blockages;
     for (dbBox* blockage : blockages) {
       if (overlaps(blockage, row, halo_x, halo_y)) {
-        row_blockages[row].push_back(blockage);
+        row_blockages.push_back(blockage);
       }
     }
-    // Cut rows around macros
-    for (auto& [k, ignored] : row_blockages) {
-      cutRow(block, k, row_blockages[k], min_row_width, halo_x, halo_y);
+    // Cut row around macros
+    if (!row_blockages.empty()) {
+      cutRow(block, row, row_blockages, min_row_width, halo_x, halo_y);
     }
   }
 
