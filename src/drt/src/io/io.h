fe08b019f351ba8d57bc410da16f24d5f1de53ce
habibayassin <habibayassin@aucegypt.edu>
2022-09-09 23:35:29 +0000

    fixing coverity test failures
    Signed-off-by: habibayassin <habibayassin@aucegypt.edu>

--- src/drt/src/io/io.h
+++ src/drt/src/io/io.h
@@ -66,6 +66,7 @@ class Parser
         logger(loggerIn),
         tmpBlock(nullptr),
         readLayerCnt(0),
+        masterSliceLayer(nullptr),
         tmpGuides(),
         tmpGRPins(),
         trackOffsetMap(),
