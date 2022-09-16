7de1a461b8d7e00f8128e9331af6e9b17b139730
Don MacMillen <don@macmillen.net>
2022-09-16 10:39:25 -0700

    Finished Python tests for ifp

---
+++ src/ifp/test/make_tracks6.py
@@ -0,0 +1,23 @@
+import openroad as ord
+from openroad import Tech, Design
+import odb
+import helpers
+
+tech = Tech()
+tech.readLEF("Nangate45/Nangate45.lef")
+
+design = Design(tech)
+design.readVerilog("reg1.v")
+design.link("top")
+
+floorplan = design.getFloorplan()
+floorplan.initFloorplan(helpers.make_rect(design, 0, 0, 1000, 1000),
+                        helpers.make_rect(design, 100, 100, 900, 900),
+                        "FreePDK45_38x28_10R_NP_162NW_34O")
+
+
+floorplan.makeTracks()
+
+def_file = helpers.make_result_file("make_tracks6.def")
+design.writeDef(def_file)
+helpers.diff_files('make_tracks6.defok', def_file)
