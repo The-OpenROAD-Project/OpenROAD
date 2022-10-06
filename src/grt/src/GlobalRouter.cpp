d39b422de21f91f70976065650cbbb1e2812db86
Eder Monteiro <emrmonteiro@inf.ufrgs.br>
2022-10-06 19:57:42 -0300

    Revert "grt: sort nets by clock status"
    This reverts commit caa1825833d26fee97bbb86a11635d43fa00066f.
    
    Signed-off-by: Eder Monteiro <emrmonteiro@inf.ufrgs.br>

--- src/grt/src/GlobalRouter.cpp
+++ src/grt/src/GlobalRouter.cpp
@@ -2537,16 +2537,7 @@ std::vector<Net*> GlobalRouter::initNetlist()
       nets.push_back(net);
     }
   }
-
-  auto compareIsNonLeafClock = [this](const Net* a, const Net* b) {
-    short is_non_leaf_clock_a = isNonLeafClock(a->getDbNet()) ? 1 : 0;
-    short is_non_leaf_clock_b = isNonLeafClock(b->getDbNet()) ? 1 : 0;
-
-    return is_non_leaf_clock_a > is_non_leaf_clock_b;
-  };
-
-  std::stable_sort(nets.begin(), nets.end(), nameLess);
-  std::stable_sort(nets.begin(), nets.end(), compareIsNonLeafClock);
+  std::sort(nets.begin(), nets.end(), nameLess);
   return nets;
 }
 
