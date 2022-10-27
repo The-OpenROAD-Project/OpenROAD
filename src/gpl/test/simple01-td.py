1eaae78c14b26378a606221eb61f97ec15134baa
Don MacMillen <don@macmillen.net>
2022-10-27 14:46:34 -0700

    Remove dead code from test
    Signed-off-by: Don MacMillen <don@macmillen.net>

--- src/gpl/test/simple01-td.py
+++ src/gpl/test/simple01-td.py
@@ -21,26 +21,3 @@ design.evalTclString("report_worst_slack")
 def_file = helpers.make_result_file("simple01-td.def")
 design.writeDef(def_file)
 helpers.diff_files(def_file, "simple01-td.defok")
-
-
-# source helpers.tcl
-# set test_name simple01-td
-# read_liberty ./library/nangate45/NangateOpenCellLibrary_typical.lib
-
-# read_lef ./nangate45.lef
-# read_def ./$test_name.def
-
-# create_clock -name core_clock -period 2 clk
-
-# set_wire_rc -signal -layer metal3
-# set_wire_rc -clock  -layer metal5
-
-# global_placement -timing_driven
-
-# # check reported wns
-# estimate_parasitics -placement
-# report_worst_slack
-
-# set def_file [make_result_file $test_name.def]
-# write_def $def_file
-# diff_file $def_file $test_name.defok
