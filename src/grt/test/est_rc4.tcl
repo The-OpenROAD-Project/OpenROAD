96266d3d7d3657fb420f1e586ca04c6ef34fd866
Eder Monteiro <emrmonteiro@inf.ufrgs.br>
2022-11-22 18:38:12 -0300

    grt: remove unit test
    Signed-off-by: Eder Monteiro <emrmonteiro@inf.ufrgs.br>

--- src/grt/test/est_rc4.tcl
+++
@@ -1,9 +0,0 @@
-# estimate parasitics based on gr results
-source "helpers.tcl"
-read_liberty Nangate45/Nangate45_typ.lib
-read_db "est_rc4.odb"
-
-set_routing_layers -signal metal2-metal10
-estimate_parasitics -global_routing
-
-report_net -connections -verbose -digits 3 clk
