27deb42938ee35389f5ddbf4486a97906e1a66ae
Matt Liberty <mliberty@eng.ucsd.edu>
2022-12-06 15:27:43 -0800

    gui: set background brush to white in the CTS viewer
    Signed-off-by: Matt Liberty <mliberty@eng.ucsd.edu>

--- src/gui/src/clockWidget.cpp
+++ src/gui/src/clockWidget.cpp
@@ -604,6 +604,7 @@ ClockTreeView::ClockTreeView(ClockTree* tree,
 
   setRenderHint(QPainter::Antialiasing);
   setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+  setBackgroundBrush(Qt::white);
 
   scene_ = static_cast<ClockTreeScene*>(scene());
 
