b0d073dcf08399ddb8e7b8308c2db30660ff2c8d
osamahammad21 <osama21@aucegypt.edu>
2022-12-12 14:16:34 +0200

    drt: fix
    Signed-off-by: osamahammad21 <osama21@aucegypt.edu>

--- src/drt/src/gc/FlexGC_cut.cpp
+++ src/drt/src/gc/FlexGC_cut.cpp
@@ -366,6 +366,11 @@ void FlexGCWorker::Impl::checkLef58CutSpacingTbl(
     cutClass = layer1->getCutClass(cutClassIdx)->getName();
 
   auto dbRule = con->getODBRule();
+  if (dbRule->isLayerValid() && GC_IGNORE_PDN && viaRect->isFixed()
+      && viaRect->hasNet() && viaRect->getNet()->getFrNet()
+      && viaRect->getNet()->getFrNet()->getType().isSupply())
+    return;
+
   bool isUpperVia = true;
   frLayerNum queryLayerNum;
   if (dbRule->isLayerValid()) {
@@ -400,8 +405,8 @@ void FlexGCWorker::Impl::checkLef58CutSpacingTbl(
       continue;
     if (ptr->getPin() == viaRect->getPin())
       continue;
-    if (GC_IGNORE_PDN && ptr->isFixed() && ptr->hasNet()
-        && ptr->getNet()->getFrNet()
+    if (dbRule->isLayerValid() && GC_IGNORE_PDN && ptr->isFixed()
+        && ptr->hasNet() && ptr->getNet()->getFrNet()
         && ptr->getNet()->getFrNet()->getType().isSupply())
       continue;
     if (isUpperVia)
