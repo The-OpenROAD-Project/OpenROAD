a64ed2215482a6a0f517e1221c1ace229b2aa00f
habibayassin <habibayassin@aucegypt.edu>
2022-12-17 00:44:15 +0000

    configure library path
    Signed-off-by: habibayassin <habibayassin@aucegypt.edu>

--- etc/DependencyInstaller.sh
+++ etc/DependencyInstaller.sh
@@ -480,6 +480,8 @@ case "${os}" in
         fi
         _installOrTools "centos" "7" "amd64"
         _installCentosCleanUp
+        echo "/usr/local/clang/lib" | sudo tee /etc/ld.so.conf
+        ldconfig
         cat <<EOF
 To enable GCC-8 or Clang-7 you need to run:
     source /opt/rh/devtoolset-8/enable
