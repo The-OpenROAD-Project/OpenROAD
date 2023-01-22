941028ab5c02eea064b127dd81c30dae55b10971
habibayassin <habibayassin@aucegypt.edu>
2023-01-22 07:41:50 +0000

    change warning tags
    Signed-off-by: habibayassin <habibayassin@aucegypt.edu>

--- etc/DockerHelper.sh
+++ etc/DockerHelper.sh
@@ -278,7 +278,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   numThreads=$(sysctl -n hw.ncpu)
 else
   cat << EOF
-[WARNING FLW-0025] Unsupported OSTYPE: cannot determine number of host CPUs"
+WARNING: Unsupported OSTYPE: cannot determine number of host CPUs"
   Defaulting to 2 threads. Use --threads N to use N threads"
 EOF
   numThreads=2
