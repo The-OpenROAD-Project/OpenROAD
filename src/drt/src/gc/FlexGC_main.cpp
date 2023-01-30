9859279ec2dd4bb0769072827fe73d11dd537156
Eder Monteiro <emrmonteiro@inf.ufrgs.br>
2023-01-30 15:26:40 -0300

    drt: detect drNet for drPatchWire during FlexGC
    Signed-off-by: Eder Monteiro <emrmonteiro@inf.ufrgs.br>

--- src/drt/src/gc/FlexGC_main.cpp
+++ src/drt/src/gc/FlexGC_main.cpp
@@ -1849,9 +1849,57 @@ void FlexGCWorker::Impl::checkMetalShape_addPatch(gcPin* pin, int min_area)
   patch->setLayerNum(layer_idx);
   patch->setOrigin(offset);
   patch->setOffsetBox(patchBx);
+
+  // get drNet for patch
+  gcNet* gc_net = pin->getNet();
+  frNet* fr_net = gc_net->getFrNet();
+  if (fr_net != nullptr) {
+    const std::vector<drNet*>* dr_nets = drWorker_->getDRNets(fr_net);
+    if (dr_nets->size() == 1) {
+      patch->addToNet((*dr_nets)[0]);
+    } else {
+      // detect what drNet has objetcs overlapping with the patch
+      checkMetalShape_patchOwner_helper(patch.get(), dr_nets);
+    }
+  }
+
   Rect shiftedPatch = patchBx;
   shiftedPatch.moveTo(offset.x(), offset.y());
   pwires_.push_back(std::move(patch));
+}
+
+void FlexGCWorker::Impl::checkMetalShape_patchOwner_helper(drPatchWire* patch,
+                                                           const std::vector<drNet*>* dr_nets)
+{
+  Rect patch_box = patch->getOffsetBox();
+  for (drNet* dr_net : *dr_nets) {
+    // check if patch overlaps with some rect of dr_net
+    if (patch_box.overlaps(dr_net->getPinBox())) {
+      patch->addToNet(dr_net);
+      return;
+    }
+
+    for (auto&& conn_fig : dr_net->getExtConnFigs()) {
+      if (patch_box.overlaps(conn_fig->getBBox())) {
+        patch->addToNet(dr_net);
+        return;
+      }
+    }
+
+    for (auto&& conn_fig : dr_net->getRouteConnFigs()) {
+      if (patch_box.overlaps(conn_fig->getBBox())) {
+        patch->addToNet(dr_net);
+        return;
+      }
+    }
+
+    for (auto&& conn_fig : dr_net->getBestRouteConnFigs()) {
+      if (patch_box.overlaps(conn_fig->getBBox())) {
+        patch->addToNet(dr_net);
+        return;
+      }
+    }
+  }
 }
 
 void FlexGCWorker::Impl::checkMetalShape_main(gcPin* pin)
