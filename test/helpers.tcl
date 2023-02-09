cb5c79d5c5c82af53cd44120da04d88f2648e0b7
Christopher D. Leary <cdleary@gmail.com>
2023-02-09 05:21:26 +0000

    Add back lmap polyfill for CentOS 7.
    Signed-off-by: Christopher D. Leary <cdleary@gmail.com>

--- test/helpers.tcl
+++ test/helpers.tcl
@@ -86,6 +86,19 @@ proc run_unit_test_and_exit { relative_path } {
   exit 0
 }
 
+# Note: required e.g. in CentOS 7 environment.
+if {[package vcompare [package present Tcl] 8.6] == -1} {
+    proc lmap {args} {
+        set result {}
+        set var [lindex $args 0]
+        foreach item [lindex $args 1] {
+            uplevel 1 "set $var $item"
+            lappend result [uplevel 1 [lindex $args end]]
+        }
+        return $result
+    }
+}
+
 set ::failing_checks 0
 set ::passing_checks 0
 
