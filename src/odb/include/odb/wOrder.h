c6f95de4224af37baa8f376e7539d90a49cd545e
Matt Liberty <mliberty@precisioninno.com>
2023-02-26 20:55:19 -0800

    odb: rm dead code
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/odb/include/odb/wOrder.h
+++ src/odb/include/odb/wOrder.h
@@ -32,8 +32,6 @@
 
 #pragma once
 
-#include "odb.h"
-
 namespace utl {
 class Logger;
 }
--- src/odb/src/db/wOrder.cpp
+++ src/odb/src/db/wOrder.cpp
@@ -32,116 +32,37 @@
 
 #include "wOrder.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "db.h"
-#include "dbMap.h"
-#include "dbShape.h"
-#include "dbWireCodec.h"
 #include "tmg_conn.h"
 
 namespace odb {
 
-//
-// not including tmg_db.h, only need tmg_findNet
-//
-
-static tmg_conn* _conn = NULL;
+static tmg_conn* conn = NULL;
 
 void orderWires(utl::Logger* logger, dbBlock* block)
 {
-  if (_conn == NULL) {
-    _conn = new tmg_conn(logger);
+  if (conn == NULL) {
+    conn = new tmg_conn(logger);
   }
-  _conn->resetSplitCnt();
+  conn->resetSplitCnt();
   for (auto net : block->getNets()) {
     if (net->getSigType().isSupply() || net->isWireOrdered()) {
       continue;
     }
-    _conn->analyzeNet(net);
+    conn->analyzeNet(net);
   }
 }
 
 void orderWires(utl::Logger* logger, dbNet* net)
 {
-  if (_conn == NULL) {
-    _conn = new tmg_conn(logger);
+  if (conn == NULL) {
+    conn = new tmg_conn(logger);
   }
   if (net->getSigType().isSupply()) {
     return;
   }
-  _conn->resetSplitCnt();
-  _conn->analyzeNet(net);
-}
-
-/////////////////////////////////////////////////////////////////
-
-typedef struct tmg_wire_link
-{
-  tmg_wire_link* next;
-  dbWire* wire;
-} tmg_wire_link;
-
-class tmg_wire_link_pool
-{
- private:
-  int blkSize;
-  int blkN;
-  int curblk;
-  tmg_wire_link* curp;
-  tmg_wire_link* endp;
-  tmg_wire_link** V;
-
- public:
-  tmg_wire_link_pool();
-  ~tmg_wire_link_pool();
-  void init();
-  tmg_wire_link* get();
-};
-
-tmg_wire_link_pool::tmg_wire_link_pool()
-{
-  V = (tmg_wire_link**) malloc(4096 * sizeof(tmg_wire_link*));
-  blkSize = 4096;
-  V[0] = (tmg_wire_link*) malloc(blkSize * sizeof(tmg_wire_link));
-  blkN = 1;
-  curblk = 0;
-  curp = V[0];
-  endp = V[0] + blkSize;
-}
-
-tmg_wire_link_pool::~tmg_wire_link_pool()
-{
-  int j;
-  for (j = 0; j < blkN; j++)
-    free(V[j]);
-  free(V);
-}
-
-void tmg_wire_link_pool::init()
-{
-  curblk = 0;
-  curp = V[0];
-  endp = V[0] + blkSize;
-}
-
-tmg_wire_link* tmg_wire_link_pool::get()
-{
-  tmg_wire_link* x = curp++;
-  if (curp == endp) {
-    curblk++;
-    if (curblk == blkN) {
-      if (blkN == 4096) {
-        return NULL;
-      }
-      V[blkN++] = (tmg_wire_link*) malloc(blkSize * sizeof(tmg_wire_link));
-    }
-    curp = V[curblk];
-    endp = V[curblk] + blkSize;
-  }
-  x->next = NULL;
-  return x;
+  conn->resetSplitCnt();
+  conn->analyzeNet(net);
 }
 
 }  // namespace odb
