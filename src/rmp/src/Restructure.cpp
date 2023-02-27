141cb179825cb7c9183ba82b67979fc8282bb565
Osama <osama21@aucegypt.edu>
2023-02-27 15:33:58 +0200

    rmp: clang-format Restructure.cpp
    Signed-off-by: Osama <osama21@aucegypt.edu>

--- src/rmp/src/Restructure.cpp
+++ src/rmp/src/Restructure.cpp
@@ -35,6 +35,7 @@
 
 #include "rmp/Restructure.h"
 
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <time.h>
@@ -63,8 +64,6 @@
 #include "sta/Search.hh"
 #include "sta/Sta.hh"
 #include "utl/Logger.h"
-#include <unistd.h> 
-#include <fcntl.h>
 
 using utl::RMP;
 using namespace abc;
@@ -195,7 +194,7 @@ void Restructure::runABC()
 
   debugPrint(
       logger_, RMP, "remap", 1, "Running ABC with {} modes.", modes.size());
-  
+
   for (size_t curr_mode_idx = 0; curr_mode_idx < modes.size();
        curr_mode_idx++) {
     output_blif_file_name_
@@ -215,17 +214,17 @@ void Restructure::runABC()
                1,
                "Writing ABC script file {}.",
                abc_script_file);
-    
+
     if (writeAbcScript(abc_script_file)) {
       // call linked abc
       Abc_Start();
       Abc_Frame_t* abc_frame = Abc_FrameGetGlobalFrame();
       const std::string command = "source " + abc_script_file;
-      
+
       fflush(stdout);
       int stdout_fd = dup(STDOUT_FILENO);
       const std::string abc_log_name = logfile_ + std::to_string(curr_mode_idx);
-      std::ofstream { abc_log_name.c_str() };
+      std::ofstream{abc_log_name.c_str()};
       int redir_fd = open(abc_log_name.c_str(), O_WRONLY);
       dup2(redir_fd, STDOUT_FILENO);
       close(redir_fd);
@@ -245,7 +244,7 @@ void Restructure::runABC()
       files_to_remove.emplace_back(abc_log_name);
     }
   }  // end modes
-  
+
   // Inspect ABC results to choose blif with least instance count
   for (int curr_mode_idx = 0; curr_mode_idx < modes.size(); curr_mode_idx++) {
     // Skip failed ABC runs
