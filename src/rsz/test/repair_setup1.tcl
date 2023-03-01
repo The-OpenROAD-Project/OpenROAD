b98e6006255920f7f7adbb3853b84c2edaba8c5f
Harsh Vardhan <openroad@chez-vardhan.com>
2023-02-28 22:09:09 -0800

    fix: Initial comment with logging/fix 1 test for GR parasitics

--- src/rsz/test/repair_setup1.tcl
+++ src/rsz/test/repair_setup1.tcl
@@ -5,10 +5,20 @@ read_lef Nangate45/Nangate45.lef
 read_def repair_setup1.def
 create_clock -period 0.3 clk
 
+source Nangate45/Nangate45.vars
 source Nangate45/Nangate45.rc
-set_wire_rc -layer metal3
-estimate_parasitics -placement
-report_checks -fields input -digits 3
+source $tracks_file
 
+####################################################
+# Older way 
+#set_wire_rc -layer metal3
+#estimate_parasitics -placement
+####################################################
+# Newer way 
+set_routing_layers -signal $global_routing_layers
+global_route
+estimate_parasitics -global_routing
+####################################################
+report_checks -fields input -digits 3
 repair_timing -setup
 report_checks -fields input -digits 3
