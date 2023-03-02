e3a87ce4a6df303a2584df870b69de9c2dda7b13
Ethan Mahintorabi <ethanmoon@google.com>
2023-03-02 01:13:31 +0000

    remove displacement flag
    Signed-off-by: Ethan Mahintorabi <ethanmoon@google.com>

--- src/dpl/src/Opendp.tcl
+++ src/dpl/src/Opendp.tcl
@@ -158,10 +158,8 @@ namespace eval dpl {
 # measured as a multiple of row_height.
 proc detailed_placement_debug { args } {
   sta::parse_key_args "detailed_placement_debug" args \
-      keys {-instance -min_displacement} \
-      flags {-displacement}
+      keys {-instance -min_displacement}
 
-  set displacement [info exists flags(-displacement)]
 
   if { [info exists keys(-min_displacement)] } {
     set min_displacement $keys(-min_displacement)
