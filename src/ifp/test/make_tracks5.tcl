609b4d6bda85756024db1c2e9b346e23a4d106e0
Mina Ashraf <mina146@aucegypt.edu>
2023-03-06 21:03:48 +0000

    removing site name from most tests
    Signed-off-by: Mina Ashraf <mina146@aucegypt.edu>

--- src/ifp/test/make_tracks5.tcl
+++ src/ifp/test/make_tracks5.tcl
@@ -5,8 +5,7 @@ read_liberty Nangate45/Nangate45_typ.lib
 read_verilog reg1.v
 link_design top
 initialize_floorplan -die_area "0 0 1000 1000" \
-  -core_area "100 100 900 900" \
-  -site FreePDK45_38x28_10R_NP_162NW_34O
+  -core_area "100 100 900 900" 
 
 make_tracks metal1 -x_offset 0.1 -x_pitch 0.2 -y_offset 0.1 -y_pitch 0.2
 make_tracks metal1 -x_offset 0.15 -x_pitch 0.2 -y_offset 0.15 -y_pitch 0.2
