e43d0f9a0eaeedcdd42718ebd1b1f7dacdf519c9
luis201420 <tauro_luisito_20@hotmail.com>
2023-03-06 18:22:17 +0000

    grt: removing unused function
    Signed-off-by: luis201420 <tauro_luisito_20@hotmail.com>

--- src/grt/include/grt/GlobalRouter.h
+++ src/grt/include/grt/GlobalRouter.h
@@ -357,11 +357,6 @@ class GlobalRouter
       int layer,
       const std::unordered_map<int, std::vector<odb::Rect>>& macro_obs_per_layer,
       std::vector<odb::Rect>& extended_obs);
-  bool layerIsBlocked(
-      int layer,
-      odb::dbTechLayerDir& direction,
-      const std::unordered_map<int, odb::Rect>& macro_obs_per_layer,
-      odb::Rect& extended_obs);
   void extendObstructions(
       std::unordered_map<int, std::vector<odb::Rect>>& macro_obs_per_layer,
       int bottom_layer,
