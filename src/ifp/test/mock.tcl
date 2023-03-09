1c97ad68ed06eecca978b1ea61ccebc1cb74924a
Mina Ashraf <mina146@aucegypt.edu>
2023-03-09 22:42:06 +0200

    unit tests defok and ok
    Signed-off-by: Mina Ashraf <mina146@aucegypt.edu>

--- src/ifp/test/mock.tcl
+++ src/ifp/test/mock.tcl
@@ -2,6 +2,8 @@ source "helpers.tcl"
 read_lef Nangate45/Nangate45.lef
 read_lef Nangate45/fake_macros.lef
 read_def mock.def
-initialize_floorplan -utilization 30 \
-  -aspect_ratio 0.5 \
-  -core_space 1 
+initialize_floorplan -utilization 30 -aspect_ratio 0.5
+
+set def_file [make_result_file mock.def]
+write_def $def_file
+diff_files mock.defok $def_file
