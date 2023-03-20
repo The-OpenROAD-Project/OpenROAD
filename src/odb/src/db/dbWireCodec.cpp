507484f265453854e58ed48298078035b22551e7
Ethan Mahintorabi <ethanmoon@google.com>
2023-03-20 22:24:48 +0000

    clang-format
    Signed-off-by: Ethan Mahintorabi <ethanmoon@google.com>

--- src/odb/src/db/dbWireCodec.cpp
+++ src/odb/src/db/dbWireCodec.cpp
@@ -618,7 +618,6 @@ void dbWireEncoder::end()
 
 void dbWireEncoder::setColor(uint8_t mask_color)
 {
-
   // LEF/DEF says 3 is the max number of supported masks per layer.
   // 0 is also not a valid mask.
   if (mask_color < 1 || mask_color > 3) {
