48eaa8a865ad827f8c1cc80ecf28f2d5d12b79e8
Eder Monteiro <emrmonteiro@inf.ufrgs.br>
2023-03-21 19:53:04 -0300

    drt: use single path for stacked vias
    Signed-off-by: Eder Monteiro <emrmonteiro@inf.ufrgs.br>

--- src/drt/src/TritonRoute.cpp
+++ src/drt/src/TritonRoute.cpp
@@ -1002,10 +1002,11 @@ void TritonRoute::stackVias(odb::dbBTerm* bterm,
   }
   odb::dbWireEncoder wire_encoder;
   wire_encoder.begin(wire);
+  odb::dbTechLayer* top_tech_layer = tech->findRoutingLayer(top_layer_idx);
+  wire_encoder.newPath(top_tech_layer, odb::dbWireType::ROUTED);
   for (int layer_idx = top_layer_idx; layer_idx < bterm_bottom_layer_idx;
        layer_idx++) {
     odb::dbTechLayer* tech_layer = tech->findRoutingLayer(layer_idx);
-    wire_encoder.newPath(tech_layer, odb::dbWireType::ROUTED);
     wire_encoder.addPoint(via_position.getX(), via_position.getY());
     wire_encoder.addTechVia(default_vias[layer_idx]);
   }
