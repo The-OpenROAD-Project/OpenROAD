15df989a5d7a2126e34afb714512b2c78893f3dd
Mina Ashraf <mina146@aucegypt.edu>
2023-04-14 15:11:22 +0200

    tests changing
    Signed-off-by: Mina Ashraf <mina146@aucegypt.edu>

--- src/dpl/test/fragmented_row03.tcl
+++ src/dpl/test/fragmented_row03.tcl
@@ -2,6 +2,9 @@
 source "helpers.tcl"
 read_lef Nangate45/Nangate45.lef
 read_def fragmented_row03.def
+set_debug_level DPL "place" 1
+set_debug_level DPL "group" 1
+set_debug_level DPL "grid" 1
 catch {detailed_placement} msg
 puts $msg
 catch {check_placement -verbose} error
