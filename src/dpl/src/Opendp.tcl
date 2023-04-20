5f78c4b0b20ec6d470026cf75293ec75f690af98
Peter Gadfort <gadfort@zeroasic.com>
2023-04-20 08:59:49 -0400

    dpl: add error for no found masters and link to PR that changed the behavior
    Signed-off-by: Peter Gadfort <gadfort@zeroasic.com>

--- src/dpl/src/Opendp.tcl
+++ src/dpl/src/Opendp.tcl
@@ -184,11 +184,11 @@ proc detailed_placement_debug { args } {
 }
 
 proc get_masters_arg { arg_name arg } {
-  set matched 0
   set masters {}
   # Expand master name regexps
   set db [ord::get_db]
   foreach name $arg {
+    set matched 0
     foreach lib [$db getLibs] {
       foreach master [$lib getMasters] {
         set master_name [$master getConstName]
@@ -198,9 +198,13 @@ proc get_masters_arg { arg_name arg } {
         }
       }
     }
+    if { !$matched } {
+      utl::warn "DPL" 28 "$name did not match any masters."
+    }
   }
-  if { !$matched } {
-    utl::warn "DPL" 28 "$name did not match any masters."
+  if { [llength $arg] > 0 && [llength $masters] == 0 } {
+    utl::error "DPL" 39 "\"$arg\" did not match any masters.
+This could be due to a change from using regex to glob to search for cell masters. https://github.com/The-OpenROAD-Project/OpenROAD/pull/3210"
   }
   return $masters
 }
