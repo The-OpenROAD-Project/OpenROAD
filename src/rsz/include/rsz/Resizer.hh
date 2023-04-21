8226b3d5cd8b9cfaf391b063cb00fbfcd278b769
Harsh Vardhan <openroad@chez-vardhan.com>
2023-04-21 00:07:53 -0700

    fix: clangtidy
    Signed-off-by: Harsh Vardhan <openroad@chez-vardhan.com>

--- src/rsz/include/rsz/Resizer.hh
+++ src/rsz/include/rsz/Resizer.hh
@@ -394,8 +394,8 @@ protected:
   float portFanoutLoad(LibertyPort *port) const;
   float portCapacitance(LibertyPort *input,
                         const Corner *corner) const;
-  void swapPins(Instance *inst, LibertyPort *pin1,
-                LibertyPort *pin2, bool journal);
+  void swapPins(Instance *inst, LibertyPort *port1,
+                LibertyPort *port2, bool journal);
   void findSwapPinCandidate(LibertyPort *input_port, LibertyPort *drvr_port,
                             float load_cap, const DcalcAnalysisPt *dcalc_ap,
                             // Return value
