770348561e960c13f1069c66bce63da6415a6c7e
Felipe Garay <fgaray@google.com>
2023-04-20 19:44:34 -0700

    For debug strings, passing the string name instead of the raw value
    Signed-off-by: Felipe Garay <fgaray@google.com>

--- src/odb/src/db/dbBTerm.cpp
+++ src/odb/src/db/dbBTerm.cpp
@@ -311,7 +311,7 @@ void dbBTerm::setSigType(dbSigType type)
                "DB_ECO",
                1,
                "ECO: setSigType {}",
-               type.getValue());
+               type.getString());
     block->_journal->updateField(
         this, _dbBTerm::FLAGS, prev_flags, flagsToUInt(bterm));
   }
@@ -337,7 +337,7 @@ void dbBTerm::setIoType(dbIoType type)
                "DB_ECO",
                1,
                "ECO: setIoType {}",
-               type.getValue());
+               type.getString());
     block->_journal->updateField(
         this, _dbBTerm::FLAGS, prev_flags, flagsToUInt(bterm));
   }
