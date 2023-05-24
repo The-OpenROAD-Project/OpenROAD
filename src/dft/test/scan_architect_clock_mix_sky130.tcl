df423e5dc3efa9a19686a116270416d9f3f12ea0
Felipe Garay <fgaray@google.com>
2023-05-24 16:03:11 -0700

    Fixing unit test for clock mixing .
    Signed-off-by: Felipe Garay <fgaray@google.com>

--- src/dft/test/scan_architect_clock_mix_sky130.tcl
+++ src/dft/test/scan_architect_clock_mix_sky130.tcl
@@ -12,14 +12,13 @@ create_clock -name clock2 -period 2.0000 -waveform {0.0000 1.0000} [get_ports {c
 
 set_dft_config -max_length 3 -clock_mixing clock_mix
 
-
-set verilog_file_before_preview [make_result_file scan_architect_no_mix_sky130_before_preview.v]
+set verilog_file_before_preview [make_result_file scan_architect_clock_mix_sky130_before_preview.v]
 write_verilog -sort $verilog_file_before_preview
 
 preview_dft -verbose
 
 # Preview should not modify the net
-set verilog_file_after_preview [make_result_file scan_architect_no_mix_sky130_after_preview.v]
+set verilog_file_after_preview [make_result_file scan_architect_clock_mix_sky130_after_preview.v]
 write_verilog -sort $verilog_file_after_preview
 
 # Before and after preview, the netlist should be the same
@@ -27,6 +26,6 @@ diff_files $verilog_file_after_preview $verilog_file_before_preview
 
 insert_dft
 
-set verilog_file [make_result_file scan_architect_no_mix_sky130.v]
+set verilog_file [make_result_file scan_architect_clock_mix_sky130.v]
 write_verilog $verilog_file
-diff_files $verilog_file scan_architect_no_mix_sky130.vok
+diff_files $verilog_file scan_architect_clock_mix_sky130.vok
