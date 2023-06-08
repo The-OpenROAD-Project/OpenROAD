3772e4acc29e7dfab034847973931ae9927fe59c
osamahammad21 <osama21@aucegypt.edu>
2023-06-08 18:21:19 +0300

    drt: minarea patch center if better
    Signed-off-by: osamahammad21 <osama21@aucegypt.edu>

--- src/drt/src/dr/FlexDR_maze.cpp
+++ src/drt/src/dr/FlexDR_maze.cpp
@@ -3367,41 +3367,25 @@ void FlexDRWorker::routeNet_postAstarAddPatchMetal(drNet* net,
       bpIdx, isPatchHorz, bpPatchLeft, patchLength);
   auto costR = routeNet_postAstarAddPathMetal_isClean(
       epIdx, isPatchHorz, epPatchLeft, patchLength);
-  if (std::min(costL, costR) != 0 && !layer->isUnidirectional()
-      && layer->getMinStepConstraint() == nullptr
-      && layer->getLef58MinStepConstraints().empty()) {
-    // patch wrong direction
-    auto costLU = routeNet_postAstarAddPathMetal_isClean(
-        bpIdx, !isPatchHorz, true, patchLength);
-    auto costLD = routeNet_postAstarAddPathMetal_isClean(
-        bpIdx, !isPatchHorz, false, patchLength);
-    auto costRU = routeNet_postAstarAddPathMetal_isClean(
-        epIdx, !isPatchHorz, true, patchLength);
-    auto costRD = routeNet_postAstarAddPathMetal_isClean(
-        epIdx, !isPatchHorz, false, patchLength);
-    auto minCost = std::min(costLU, std::min(costLD, std::min(costRU, costRD)));
-    if (minCost < std::min(costL, costR)) {
-      if (minCost == costLU) {
-        routeNet_postAstarAddPatchMetal_addPWire(
-            net, bpIdx, !isPatchHorz, true, patchLength, patchWidth);
-      } else if (minCost == costLD) {
-        routeNet_postAstarAddPatchMetal_addPWire(
-            net, bpIdx, !isPatchHorz, false, patchLength, patchWidth);
-      } else if (minCost == costRU) {
-        routeNet_postAstarAddPatchMetal_addPWire(
-            net, epIdx, !isPatchHorz, true, patchLength, patchWidth);
-      } else if (minCost == costRD) {
-        routeNet_postAstarAddPatchMetal_addPWire(
-            net, epIdx, !isPatchHorz, false, patchLength, patchWidth);
-      }
-      return;
-    }
-  }
-  if (costL <= costR) {
+  frCoord halfPatchLength
+      = frCoord(ceil(1.0 * patchLength / 2 / getTech()->getManufacturingGrid()))
+        * getTech()->getManufacturingGrid();
+  int costCenter = routeNet_postAstarAddPathMetal_isClean(
+                       bpIdx, isPatchHorz, bpPatchLeft, halfPatchLength)
+                   + routeNet_postAstarAddPathMetal_isClean(
+                       epIdx, isPatchHorz, epPatchLeft, halfPatchLength);
+  if (costCenter < costL && costCenter < costR) {
     routeNet_postAstarAddPatchMetal_addPWire(
-        net, bpIdx, isPatchHorz, bpPatchLeft, patchLength, patchWidth);
+        net, bpIdx, isPatchHorz, bpPatchLeft, halfPatchLength, patchWidth);
+    routeNet_postAstarAddPatchMetal_addPWire(
+        net, epIdx, isPatchHorz, epPatchLeft, halfPatchLength, patchWidth);
   } else {
-    routeNet_postAstarAddPatchMetal_addPWire(
-        net, epIdx, isPatchHorz, epPatchLeft, patchLength, patchWidth);
+    if (costL <= costR) {
+      routeNet_postAstarAddPatchMetal_addPWire(
+          net, bpIdx, isPatchHorz, bpPatchLeft, patchLength, patchWidth);
+    } else {
+      routeNet_postAstarAddPatchMetal_addPWire(
+          net, epIdx, isPatchHorz, epPatchLeft, patchLength, patchWidth);
+    }
   }
 }
