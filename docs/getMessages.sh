2168bb23e1cd69dad724af86d74454ae26b834ab
luarss <espsluar@gmail.com>
2023-06-23 02:10:23 +0800

    Added OR messages glossary
    Signed-off-by: luarss <espsluar@gmail.com>

---
+++ docs/getMessages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+python ../etc/find_messages.py -d ../src > user/Messages.md
+
+input_file="user/Messages.md"
+output_file="user/MessagesFinal.md"
+
+# Create file header
+echo "# OpenROAD Messages Glossary" > "$output_file"
+echo "Listed below are the OpenROAD warning/error codes you may encounter during running." >> "$output_file"
+echo "" >> "$output_file"
+
+# Create the Markdown table header
+echo "| Tool | Code | Message                                             |" >> "$output_file"
+echo "| ---- | ---- | --------------------------------------------------- |" >> "$output_file"
+
+awk '{
+  ant=$1;
+  num=$2;
+  message=substr($0, index($0,$4));
+  printf("| %s | %s | %s |\n", ant, num, message);
+}' "$input_file" >> "$output_file"
