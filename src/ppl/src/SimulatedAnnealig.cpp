f5d4e2c92ba346df99054dde5c21071bec297f28
Eder Monteiro <emrmonteiro@inf.ufrgs.br>
2023-06-26 19:27:10 -0300

    ppl: start simulated annealing implementation for the pin placement problem
    Signed-off-by: Eder Monteiro <emrmonteiro@inf.ufrgs.br>

---
+++ src/ppl/src/SimulatedAnnealig.cpp
@@ -0,0 +1,116 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// BSD 3-Clause License
+//
+// Copyright (c) 2019, The Regents of the University of California
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <algorithm>
+#include <random>
+
+#include "SimulatedAnnealing.h"
+#include "utl/Logger.h"
+#include "utl/algorithms.h"
+
+namespace ppl {
+
+SimulatedAnnealing::SimulatedAnnealing(Netlist* netlist,
+                                       std::vector<Slot>& slots,
+                                       Logger* logger,
+                                       odb::dbDatabase* db)
+    : netlist_(netlist), slots_(slots), logger_(logger), db_(db)
+{
+  num_slots_ = slots.size();
+  num_pins_ = netlist->numIOPins();
+}
+
+void SimulatedAnnealing::init()
+{
+  pin_assignment_.resize(num_pins_);
+  slot_indices_.resize(num_slots_);
+  std::iota(slot_indices_.begin(), slot_indices_.end(), 0);
+  logger_->report("Num pins: {};\nNum slots: {}", num_pins_, num_slots_);
+}
+
+void SimulatedAnnealing::randomAssignment()
+{
+  std::mt19937 g;
+  g.seed(seed_);
+  utl::shuffle(slot_indices_.begin(), slot_indices_.end(), g);
+  for (int i = 0; i < pin_assignment_.size(); i++) {
+    pin_assignment_[i] = slot_indices_[i];
+  }
+}
+
+int SimulatedAnnealing::getAssignmentCost()
+{
+  int cost = 0;
+
+  for (int i = 0; i < pin_assignment_.size(); i++) {
+    int slot_idx = pin_assignment_[i];
+    const odb::Point& position = slots_[slot_idx].pos;
+    cost += netlist_->computeIONetHPWL(i, position);
+  }
+
+  debugPrint(logger_, utl::PPL, "annealing", 1, "assignment cost: {}", cost);
+
+  return cost;
+}
+
+void SimulatedAnnealing::getAssignment(std::vector<IOPin>& assignment)
+{
+  for (int i = 0; i < pin_assignment_.size(); i++) {
+    IOPin& io_pin = netlist_->getIoPin(i);
+    Slot& slot = slots_[pin_assignment_[i]];
+
+    io_pin.setPos(slot.pos);
+    io_pin.setLayer(slot.layer);
+    io_pin.setPlaced();
+    assignment.push_back(io_pin);
+    slot.used = true;
+  }
+}
+
+void SimulatedAnnealing::run()
+{
+  init();
+  randomAssignment();
+  int cost = getAssignmentCost();
+
+  logger_->report("Random assignment cost: {}", dbuToMicrons(cost));
+}
+
+double SimulatedAnnealing::dbuToMicrons(int64_t dbu)
+{
+  return (double) dbu / (db_->getChip()->getBlock()->getDbUnitsPerMicron());
+}
+
+}  // namespace ppl
---
+++ src/ppl/src/SimulatedAnnealing.h
@@ -0,0 +1,83 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// BSD 3-Clause License
+//
+// Copyright (c) 2019, The Regents of the University of California
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "Netlist.h"
+#include "Slots.h"
+#include "odb/geom.h"
+#include "ppl/IOPlacer.h"
+
+namespace utl {
+class Logger;
+}  // namespace utl
+
+namespace ppl {
+using odb::Point;
+using odb::Rect;
+
+using utl::Logger;
+
+class SimulatedAnnealing
+{
+ public:
+  SimulatedAnnealing(Netlist* netlist,
+                     std::vector<Slot>& slots,
+                     Logger* logger,
+                     odb::dbDatabase* db);
+  virtual ~SimulatedAnnealing() = default;
+  void init();
+  void randomAssignment();
+  int getAssignmentCost();
+  void getAssignment(std::vector<IOPin>& assignment);
+  void run();
+
+ private:
+  double dbuToMicrons(int64_t dbu);
+
+  // [pin] -> slot
+  std::vector<int> pin_assignment_;
+  std::vector<int> slot_indices_;
+  Netlist* netlist_;
+  std::vector<Slot>& slots_;
+  int num_slots_;
+  int num_pins_;
+  Logger* logger_ = nullptr;
+  odb::dbDatabase* db_;
+  const int fail_cost_ = std::numeric_limits<int>::max();
+  const int seed_ = 42;
+};
+
+}  // namespace ppl
