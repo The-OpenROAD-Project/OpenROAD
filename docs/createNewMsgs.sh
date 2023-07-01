5a83a34fcc3d63b4f7f3b74a0c869dde07f0c142
luarss <espsluar@gmail.com>
2023-07-02 00:46:25 +0800

    update to bash scripts+proper indentation
    Signed-off-by: luarss <espsluar@gmail.com>

--- docs/createNewMsgs.sh
+++ docs/createNewMsgs.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # First step is to call find_messages.py to generate the messages.txt file
 # Next is to parse it to get the error <NUM> -> create <NUM>.md
@@ -14,22 +14,22 @@
 # 010723: For now, we are just trying prototype so limited to first two tool (ant)
 cwd=$(pwd)
 for i in $(ls -d ../src/*/); do
- #rm -rf  "$i"doc/messages
- if [[ ! $i =~ "ant" ]] && [[ ! $i =~ "cts" ]]; then
-    continue
- fi
- cd $cwd && echo $i
- path=$(readlink -f ${i})
- echo $path && cd $path
- python ../../etc/find_messages.py > messages.txt
- file_nums=$(awk '{print $2}' messages.txt)
- mkdir -p doc/messages && cd doc/messages
- for num in $file_nums; do
-   if [ ! -f "$num".md ]; then
-     touch "$num".md 
-   else
-     echo "Skipping $num.md"
-   fi
- done
+    #rm -rf  "$i"doc/messages
+    if [[ ! $i =~ "ant" ]] && [[ ! $i =~ "cts" ]]; then
+        continue
+    fi
+    cd $cwd && echo $i
+    path=$(readlink -f ${i})
+    echo $path && cd $path
+    python ../../etc/find_messages.py > messages.txt
+    file_nums=$(awk '{print $2}' messages.txt)
+    mkdir -p doc/messages && cd doc/messages
+    for num in $file_nums; do
+        if [ ! -f "$num".md ]; then
+            touch "$num".md 
+        else
+            echo "Skipping $num.md"
+        fi
+    done
 done
 
