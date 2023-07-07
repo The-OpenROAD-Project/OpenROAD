ac981a5182c6d112d29d3440fc2cd5f9922ebb29
luarss <espsluar@gmail.com>
2023-07-08 00:32:22 +0800

    added message type
    Signed-off-by: luarss <espsluar@gmail.com>

--- docs/getMessages.py
+++ docs/getMessages.py
@@ -9,8 +9,8 @@ with open('user/MessagesFinal.md', 'w') as f:
     f.write("# OpenROAD Messages Glossary\n")
     f.write("Listed below are the OpenROAD warning/errors you may encounter while using the application.\n")
     f.write("\n")
-    f.write("| Tool | Code | Filename:Line Number | Information                  |\n")
-    f.write("| ---- | ---- | -------------------- | ---------------------------- |\n")
+    f.write("| Tool | Code | Filename:Line Number | Type | Information             |\n")
+    f.write("| ---- | ---- | -------------------- | ---- | ----------------------- |\n")
 
     lines = output.split('\n')
     for line in lines:
@@ -19,10 +19,11 @@ with open('user/MessagesFinal.md', 'w') as f:
         ant = columns[0] 
         num = columns[1]
         fileLineNum = f"[{columns[2]}]({columns[-1]})"
+        msgType = columns[-2]
         tool = columns[0].lower()
         try:
             message = open(f"../src/{tool}/doc/messages/{num}.md").read().strip()
         except OSError as e:
             message = "-"
         if not message: message = "-"
-        f.write(f"| {ant} | {num} | {fileLineNum} |{message} |\n")
+        f.write(f"| {ant} | {num} | {fileLineNum} | {msgType} |{message} |\n")
