c8fe6cb74e6ed61b16e06932dde3c758317831d4
Arthur Koucher <arthurckoucher@gmail.com>
2023-07-27 12:05:05 -0300

    ppl: fix typo
    Signed-off-by: Arthur Koucher <arthurckoucher@gmail.com>

--- src/ppl/test/annealing3.tcl
+++ src/ppl/test/annealing3.tcl
@@ -3,7 +3,7 @@ source "helpers.tcl"
 read_lef Nangate45/Nangate45.lef
 read_def gcd.def
 
-set_simulated_annealing -temperature 10.0 -max_iterations 10000 -perturb_per_iter 500 -alpha 0.95zz
+set_simulated_annealing -temperature 10.0 -max_iterations 10000 -perturb_per_iter 500 -alpha 0.95
 place_pins -hor_layers metal3 -ver_layers metal4 -annealing
 
 set def_file [make_result_file annealing3.def]
