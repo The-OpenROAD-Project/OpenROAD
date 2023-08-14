c73223be00064fc8208b5cd144a07855df75b09d
Wen-Tian-Pineapple <74345454+Wen-Tian-Pineapple@users.noreply.github.com>
2023-08-14 10:58:55 -0500

    Update conf.py
    Signed-off-by: Wen-Tian-Pineapple <74345454+Wen-Tian-Pineapple@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -49,8 +49,10 @@ myst_enable_extensions = [
 ]
 
 external_toc_path = 'toc.yml'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
