cdc94c46f490fcf5a67ffe20839caf1a23d3961a
Wen-Tian-Pineapple <74345454+Wen-Tian-Pineapple@users.noreply.github.com>
2023-08-14 10:55:46 -0500

    Update conf.py
    Signed-off-by: Wen-Tian-Pineapple <74345454+Wen-Tian-Pineapple@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -3,24 +3,17 @@
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Path setup --------------------------------------------------------------
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-
 # -- Project information -----------------------------------------------------
-
 project = 'OpenROAD'
 copyright = 'The Regents of the University of California, 2021'
 author = 'OpenROAD Team'
-
-
 # -- General configuration ---------------------------------------------------
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -31,9 +24,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
     'sphinx_external_toc',
-    'sphinx_copybutton',
     'myst_parser',
-    'sphinxcontrib.mermaid'
 ]
 
 myst_enable_extensions = [
@@ -49,21 +40,16 @@ myst_enable_extensions = [
     'tasklist',
     'html_image',
 ]
-
 external_toc_path = 'toc.yml'
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
 source_suffix = ['.md']
-
 # The master toctree document.
 master_doc = 'index'
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
@@ -81,82 +67,75 @@ exclude_patterns = [
     'main/src/odb/src/lef/README.md',
     'main/docs',
 ]
-
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
-
-
 # -- Options for HTML output -------------------------------------------------
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_book_theme"
+html_theme = "sphinx_symbiflow_theme"
 
 html_theme_options = {
-    "repository_url": "https://github.com/The-OpenROAD-Project/OpenROAD",
-    "repository_branch": "master",
-    "show_navbar_depth": 2,
-    "use_edit_page_button": True,
-    "use_source_button": True,
-    "use_issues_button": True,
-    # "use_repository_button": True,
-    "use_download_button": True,
+    # Repository integration
+    # Set the repo url for the link to appear
+    'github_url': 'https://github.com/The-OpenROAD-Project/OpenROAD',
+    # The name of the repo. If must be set if github_url is set
+    'repo_name': 'OpenROAD',
+    # Must be one of github, gitlab or bitbucket
+    'repo_type': 'github',
 
-    # list for more fine-grained ordering of icons
-    "icon_links": [
-        {
-            "name": "The OpenROAD Project",
-            "url": "https://theopenroadproject.org/",
-            "icon": "fa-solid fa-globe",
-        },
-        {
-            "name": "Twitter",
-            "url": "https://twitter.com/OpenROAD_EDA",
-            "icon": "fa-brands fa-twitter",
-        },
-        {
-            "name": "Email",
-            "url": "mailto:openroad@eng.ucsd.edu",
-            "icon": "fa-solid fa-envelope",
-        },
-        {
-            "name": "GitHub",
-            "url": "https://github.com/The-OpenROAD-Project/OpenROAD",
-            "icon": "fa-brands fa-github",
-        },
-        {
-            "name": "Stars",
-            "url": "https://github.com/The-OpenROAD-Project/OpenROAD/stargazers",
-            "icon": "https://img.shields.io/github/stars/The-OpenROAD-Project/OpenROAD",
-            "type": "url",
-        },
-   ],
+    # Set the name to appear in the left sidebar/header. If not provided, uses
+    # html_short_title if defined, or html_title
+    'nav_title': "OpenROAD",
+
+    # A list of dictionaries where each has three keys:
+    #   href: The URL or pagename (str)
+    #   title: The title to appear (str)
+    #   internal: Flag indicating to use pathto (bool)
+    'nav_links': [
+        {"title": "Home", "href": "index", "internal": True},
+        {"title": "The OpenROAD Project",
+            "href": "https://theopenroadproject.org", "internal": False},
+    ],
+
+    # Customize css colors.
+    # For details see link.
+    # https://getmdl.io/customize/index.html
+    #
+    # Primary colors:
+    # red, pink, purple, deep-purple, indigo, blue, light-blue, cyan,
+    # teal, green, light-green, lime, yellow, amber, orange, deep-orange,
+    # brown, grey, blue-grey, white
+    # (Default: deep-purple)
+    'color_primary': 'indigo',
+    # Values: Same as color_primary.
+    # (Default: indigo)
+    'color_accent': 'blue',
+
+    # Hide the symbiflow links
+    'hide_symbiflow_links': True,
+
+    "html_minify": False,
+    "html_prettify": True,
+    "css_minify": True,
+    "globaltoc_depth": 2,
+    "table_classes": ["plain"],
+    "master_doc": False,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
-
-
 def swap_prefix(file, old, new):
     with open(file, 'r') as f:
         lines = f.read()
     lines = lines.replace(old, new)
     with open(file, 'wt') as f:
         f.write(lines)
-
-
 def setup(app):
     import os
 
     if not os.path.exists('./main'):
         os.symlink('..', './main')
-    # these prefix swaps will be reverted and is needed for sphinx compilation.
     swap_prefix('../README.md', '(docs/', '(../')
-    swap_prefix('../README.md', '```mermaid', '```{mermaid}\n:align: center\n')
-
-    # for populating OR Messages page.
-    command = "python getMessages.py"
-    _ = os.popen(command).read()
