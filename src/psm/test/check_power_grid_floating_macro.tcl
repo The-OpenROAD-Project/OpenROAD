da9f91d5a259aa010b18974c69a56b49055b6615
Peter Gadfort <gadfort@zeroasic.com>
2023-08-16 11:53:56 -0400

    psm: add unit test for error report
    Signed-off-by: Peter Gadfort <gadfort@zeroasic.com>
    Signed-off-by: Ahmad ElRouby <a.refaat@aucegypt.edu>

--- src/psm/test/check_power_grid_floating_macro.tcl
+++ src/psm/test/check_power_grid_floating_macro.tcl
@@ -5,5 +5,10 @@ read_lef asap7/asap7_tech_1x_201209.lef
 read_lef asap7/asap7sc7p5t_28_R_1x_220121a.lef
 read_lef asap7_data/fakeram7_256x32.lef
 read_def asap7_data/riscv.def
-catch {check_power_grid -net VDD} err
+
+
+set error_report [make_result_file check_power_grid_floating_macro.rpt]
+catch {check_power_grid -net VDD -error_file $error_report} err
 puts $err
+
+diff_files $error_report check_power_grid_floating_macro.rptok
