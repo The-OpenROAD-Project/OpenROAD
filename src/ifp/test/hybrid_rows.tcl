b24297f15f44a82c2406e99c68dba3985daa6746
mina1460 <mina146@aucegypt.edu>
2023-08-20 19:54:32 +0300

    making test smaller
    Signed-off-by: mina1460 <mina146@aucegypt.edu>

--- src/ifp/test/hybrid_rows.tcl
+++ src/ifp/test/hybrid_rows.tcl
@@ -5,8 +5,8 @@ read_lef Nangate45/fake_macros.lef
 read_verilog hybrid_rows.v
 link_design top
 set_debug_level IFP hybrid 1
-initialize_floorplan -die_area "0 0 1000 10000" \
-  -core_area "1000 1000 9000 9000" 
+initialize_floorplan -die_area "0 0 3000 3000" \
+  -core_area "100 100 2800 2800" 
 
 set def_file [make_result_file hybrid_rows.def]
 write_def $def_file
