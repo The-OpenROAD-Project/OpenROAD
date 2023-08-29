3521462f29799ca06c8ded8f8c58391845ecff15
Wen-Tian-Pineapple <74345454+Wen-Tian-Pineapple@users.noreply.github.com>
2023-08-29 06:42:52 +0000

    Add files via upload
    Signed-off-by: Wen-Tian-Pineapple <74345454+Wen-Tian-Pineapple@users.noreply.github.com>

--- src/pdn/test/sroute_test.tcl
+++ src/pdn/test/sroute_test.tcl
@@ -3,48 +3,15 @@ source "helpers.tcl"
 
 read_lef sky130hd/sky130hd.tlef 
 read_lef sky130hd/sky130_fd_sc_hd_merged.lef 
-read_lef sky130_temp_sensor/HEADER.lef
-read_lef sky130_temp_sensor/SLC.lef
+read_lef sky130_temp_sensor/srouteHEADER.lef
+read_lef sky130_temp_sensor/srouteSLC.lef
 
-read_def sky130_temp_sensor/floorplan.def
+read_db sky130_temp_sensor/4_cts.odb
+read_sdc sky130_temp_sensor/4_cts.sdc
 
-add_global_connection -net VDD -inst_pattern {.*} -pin_pattern VPWR -power
-add_global_connection -net VSS -inst_pattern {.*} -pin_pattern VGND -ground
-add_global_connection -net VSS -inst_pattern {.*} -pin_pattern VNB
+add_sroute_connect  -net "VIN" -outerNet "VDD" -layers {met1 met4} -cut_pitch {200 200} -fixed_vias {M3M4_PR_M} -metalwidths {1000 1000} -metalspaces {800} -ongrid {met3 met4} -stripDY 490 \
+                    -insts "temp_analog_1.a_header_0 temp_analog_1.a_header_1 temp_analog_1.a_header_2"
 
-add_global_connection -net VDD -inst_pattern {temp_analog_1.*} -pin_pattern VPWR
-add_global_connection -net VDD -inst_pattern {temp_analog_1.*} -pin_pattern VPB
-add_global_connection -net VIN -inst_pattern {temp_analog_0.*} -pin_pattern VPWR -power
-add_global_connection -net VIN -inst_pattern {temp_analog_0.*} -pin_pattern VPB
-
-set_voltage_domain -power VDD -ground VSS
-
-define_pdn_grid -name "Core"
-add_pdn_stripe -followpins -layer met1 -width 0.48 -extend_to_core_ring
-add_pdn_stripe -layer met4 -width 1.6 -pitch 50.0 -offset 13.570 -extend_to_core_ring
-
-add_pdn_ring -grid "Core" -layers {met4 met3} -widths 5.0 -spacings 2.0 -core_offsets 4.5
-
-add_pdn_connect -grid "Core" -layers {met1 met3}
-add_pdn_connect -grid "Core" -layers {met4 met3}
-
-set_voltage_domain -power VIN -ground VSS -region "TEMP_ANALOG"
-define_pdn_grid -name "TempSensor" -voltage_domains "TEMP_ANALOG"
-add_pdn_stripe -followpins -layer met1 -width 0.48 -extend_to_core_ring
-add_pdn_stripe -layer met4 -width 1.6 -spacing 1.6 -pitch 50.0 -offset 13.570 -extend_to_core_ring
-add_pdn_stripe -layer met5 -width 1.6 -spacing 1.6 -pitch 27.2 -offset 13.600 -extend_to_core_ring
-
-add_pdn_ring -grid "TempSensor" -layers {met4 met5} -widths 5.0 -spacings 2.0 -core_offsets 3.5
-
-add_pdn_connect -grid "TempSensor" -layers {met1 met4}
-add_pdn_connect -grid "TempSensor" -layers {met4 met5}
-
-pdngen
-
-pdn::add_sroute_inst "VIN" "PHY_34" "VPWR" 57220 5000 5000 58200
-pdn::add_sroute_inst "VIN" "PHY_23" "VPWR" 57220 5000 5000 58200
-
-add_sroute_connect -net "VIN" -outerNet "VDD" -layers {met1 met4} -cut_pitch {200 200} -fixed_vias {M4M5_PR_M} -hDX 57220 -hDY 5000 -vDX 5000 -vDY 58200 -metalwidths {1500 1500} -metalspaces {500} -ongrid {met5 met4} -stripDY 480
 
 set def_file [make_result_file sroute_test.def]
 write_def $def_file
