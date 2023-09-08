8eda6424fd636bb87411fb0d596a9eb79747bf63
Eder Monteiro <emrmonteiro@inf.ufrgs.br>
2023-09-07 23:57:23 -0300

    grt: fix report and congestion map for designs that are regular in X or Y
    Signed-off-by: Eder Monteiro <emrmonteiro@inf.ufrgs.br>
    Signed-off-by: Ahmad ElRouby <a.refaat@aucegypt.edu>

--- src/grt/src/fastroute/src/FastRoute.cpp
+++ src/grt/src/fastroute/src/FastRoute.cpp
@@ -804,16 +804,25 @@ void FastRouteCore::updateDbCongestion()
     const unsigned short capV = v_capacity_3D_[k];
     const unsigned short last_row_capH = last_row_h_capacity_3D_[k];
     const unsigned short last_col_capV = last_col_v_capacity_3D_[k];
+    bool is_horizontal = ((k % 2) - layer_orientation_) == 0;
     for (int y = 0; y < y_grid_; y++) {
       for (int x = 0; x < x_grid_; x++) {
         const unsigned short blockageH = h_edges_3D_[k][y][x].red;
         const unsigned short blockageV = v_edges_3D_[k][y][x].red;
         const unsigned short usageH = h_edges_3D_[k][y][x].usage + blockageH;
         const unsigned short usageV = v_edges_3D_[k][y][x].usage + blockageV;
-        if (x == x_grid_ - 1 || y == y_grid_ - 1) {
-          db_gcell->setCapacity(layer, x, y, last_row_capH, last_col_capV, 0);
+        if (is_horizontal) {
+          if (!regular_y_ && y == y_grid_ - 1) {
+            db_gcell->setCapacity(layer, x, y, last_row_capH, capV, 0);
+          } else {
+            db_gcell->setCapacity(layer, x, y, capH, capV, 0);
+          }
         } else {
-          db_gcell->setCapacity(layer, x, y, capH, capV, 0);
+          if (!regular_x_ && x == x_grid_ - 1) {
+            db_gcell->setCapacity(layer, x, y, capH, last_col_capV, 0);
+          } else {
+            db_gcell->setCapacity(layer, x, y, capH, capV, 0);
+          }
         }
         db_gcell->setUsage(layer, x, y, usageH, usageV, 0);
         db_gcell->setBlockage(layer, x, y, blockageH, blockageV, 0);
@@ -1314,8 +1323,28 @@ std::vector<int> FastRouteCore::getOriginalResources()
 {
   std::vector<int> original_resources(num_layers_);
   for (int l = 0; l < num_layers_; l++) {
-    original_resources[l]
-        += (v_capacity_3D_[l] + h_capacity_3D_[l]) * y_grid_ * x_grid_;
+    bool is_horizontal = ((l % 2) - layer_orientation_) == 0;
+    if (is_horizontal) {
+      if (!regular_y_) {
+        original_resources[l] += (v_capacity_3D_[l] + h_capacity_3D_[l])
+                                 * (y_grid_) * (x_grid_ - 1);
+        original_resources[l] += (last_col_v_capacity_3D_[l] * y_grid_)
+                                 + (last_row_h_capacity_3D_[l] * x_grid_);
+      } else {
+        original_resources[l]
+            += (v_capacity_3D_[l] + h_capacity_3D_[l]) * (y_grid_) * (x_grid_);
+      }
+    } else {
+      if (!regular_x_) {
+        original_resources[l] += (v_capacity_3D_[l] + h_capacity_3D_[l])
+                                 * (y_grid_ - 1) * (x_grid_);
+        original_resources[l] += (last_col_v_capacity_3D_[l] * y_grid_)
+                                 + (last_row_h_capacity_3D_[l] * x_grid_);
+      } else {
+        original_resources[l]
+            += (v_capacity_3D_[l] + h_capacity_3D_[l]) * (y_grid_) * (x_grid_);
+      }
+    }
   }
 
   return original_resources;
