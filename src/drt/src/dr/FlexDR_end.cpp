25f5c9026314749c1c1dd8baf65fec42153c2c4f
Osama <osama21@aucegypt.edu>
2023-09-29 20:33:32 +0300

    drt: limit degradation in number of violations in ripup mode 2
    Signed-off-by: Osama <osama21@aucegypt.edu>

--- src/drt/src/dr/FlexDR_end.cpp
+++ src/drt/src/dr/FlexDR_end.cpp
@@ -616,7 +616,7 @@ bool FlexDRWorker::end(frDesign* design)
   if (getDRIter() && getInitNumMarkers() == 0 && !needRecheck_) {
     return false;
     // do not write back if current clip is worse than input
-  } else if (getRipupMode() == 0 && getBestNumMarkers() > getInitNumMarkers()) {
+  } else if (getRipupMode() != 1 && getBestNumMarkers() > getInitNumMarkers()) {
     // cout <<"skip clip with #init/final = " <<getInitNumMarkers() <<"/"
     // <<getNumMarkers() <<endl;
     return false;
