419f8e97345113d9bb1d09ec6596e63ddd6df8dd
Matt Liberty <mliberty@precisioninno.com>
2023-10-04 15:53:57 -0700

    gui: avoid a race when creating widgets
    The commandAboutToExecute signal needs to reach the layoutViewer before
    it kicks off a render to ensure it is synchronous.
    
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/gui/src/layoutTabs.cpp
+++ src/gui/src/layoutTabs.cpp
@@ -82,6 +82,9 @@ void LayoutTabs::blockLoaded(odb::dbBlock* block)
                                  this);
   viewer->setLogger(logger_);
   viewers_.push_back(viewer);
+  if (command_executing_) {
+    viewer->commandAboutToExecute();
+  }
   auto scroll = new LayoutScroll(viewer, this);
   viewer->blockLoaded(block);
 
@@ -112,6 +115,10 @@ void LayoutTabs::blockLoaded(odb::dbBlock* block)
 void LayoutTabs::tabChange(int index)
 {
   current_viewer_ = viewers_[index];
+  if (command_executing_) {
+    current_viewer_->commandAboutToExecute();
+  }
+
   emit setCurrentBlock(current_viewer_->getBlock());
 }
 
@@ -316,6 +323,7 @@ void LayoutTabs::commandAboutToExecute()
   if (current_viewer_) {
     current_viewer_->commandAboutToExecute();
   }
+  command_executing_ = true;
 }
 
 void LayoutTabs::commandFinishedExecuting()
@@ -323,6 +331,7 @@ void LayoutTabs::commandFinishedExecuting()
   if (current_viewer_) {
     current_viewer_->commandFinishedExecuting();
   }
+  command_executing_ = false;
 }
 
 void LayoutTabs::restoreTclCommands(std::vector<std::string>& cmds)
