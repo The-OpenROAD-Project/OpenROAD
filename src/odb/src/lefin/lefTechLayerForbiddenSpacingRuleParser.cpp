958a064457fe51c66ed2878ca37aeac2f6c8d8a0
Eder Monteiro <emrmonteiro@inf.ufrgs.br>
2023-10-10 11:58:17 -0300

    odb: clang-tidy
    Signed-off-by: Eder Monteiro <emrmonteiro@inf.ufrgs.br>

--- src/odb/src/lefin/lefTechLayerForbiddenSpacingRuleParser.cpp
+++ src/odb/src/lefin/lefTechLayerForbiddenSpacingRuleParser.cpp
@@ -53,16 +53,18 @@ void lefTechLayerForbiddenSpacingRuleParser::parse(std::string s,
   boost::split(rules, s, boost::is_any_of(";"));
   for (auto rule : rules) {
     boost::algorithm::trim(rule);
-    if (rule.empty())
+    if (rule.empty()) {
       continue;
+    }
     rule += " ; ";
-    if (!parseSubRule(rule, layer))
+    if (!parseSubRule(rule, layer)) {
       lefin_->warning(
           438,
           "parse mismatch in layer propery LEF58_FORBIDDENSPACING for "
           "layer {} :\"{}\"",
           layer->getName(),
           rule);
+    }
   }
 }
 
