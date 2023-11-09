7c586879e4fb3298bf5e75ea4eb67e9adad0bd46
Matt Liberty <mliberty@precisioninno.com>
2023-11-08 21:49:40 -0800

    ifp: update die area in hybrid row tests
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/ifp/test/hybrid_rows.tcl
+++ src/ifp/test/hybrid_rows.tcl
@@ -5,7 +5,7 @@ read_lef Nangate45/fake_macros.lef
 read_verilog hybrid_rows.v
 link_design top
 set_debug_level IFP hybrid 1
-initialize_floorplan -die_area "0 0 300 300" \
+initialize_floorplan -die_area "0 0 1100 1100" \
   -core_area "100 100 1000 1000" 
 
 set def_file [make_result_file hybrid_rows.def]
