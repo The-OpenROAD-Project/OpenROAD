56a6c64a45d49598f3161b6011dd4538ff023fc0
Matt Liberty <mliberty@precisioninno.com>
2023-11-21 21:22:10 -0800

    odb: clang-format
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/odb/src/db/dbDatabase.h
+++ src/odb/src/db/dbDatabase.h
@@ -71,7 +71,6 @@ const uint db_schema_major = 0;  // Not used...
 const uint db_schema_initial = 57;
 const uint db_schema_minor = 73;  // Current revision number
 
-
 // Revision where _dbSite::_row_pattern/_parent_lib/_parent_site were added
 const uint db_schema_site_row_pattern = 73;
 
