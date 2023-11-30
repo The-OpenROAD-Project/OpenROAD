f4c8e93adf728c8e97bc8822da28991bfa3938fc
Jack Luar <39641663+luarss@users.noreply.github.com>
2023-11-30 17:47:42 +0000

    added tclshell interpreter
    Signed-off-by: Jack Luar <39641663+luarss@users.noreply.github.com>
    Signed-off-by: luarss <39641663+luarss@users.noreply.github.com>

---
+++ docs/manpages/tclInterp/main.cpp
@@ -0,0 +1,18 @@
+#include <tcl.h>
+#include <tclreadline.h>
+#include <tclExtend.h>
+#include <iostream>
+
+// Minimal example of Tcl shell
+// Note that man is enabled only on Tclx. So we need to enable it
+int tclAppInit(Tcl_Interp* interp)
+{
+  return 0;
+}
+
+int main(int argc, char* argv[]){
+    // Setup the app with tcl
+    auto* interp = Tcl_CreateInterp();
+    Tclx_Init(interp);
+    Tcl_Main(1, argv, tclAppInit);
+}
