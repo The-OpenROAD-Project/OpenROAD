1011493a6b1d16d5f144dbc4f2421b31a6cd0d7c
Arthur Koucher <arthurckoucher@gmail.com>
2023-12-05 17:28:30 -0300

    mpl2: add macro only test case & clean tests and files that were not being used
    Signed-off-by: Arthur Koucher <arthurckoucher@gmail.com>

---
+++ src/mpl2/test/macro_only.tcl
@@ -0,0 +1,31 @@
+source "helpers.tcl"
+set LIB_DIR "./Nangate45"
+#
+set tech_lef "$LIB_DIR/Nangate45_tech.lef"
+set std_cell_lef "$LIB_DIR/Nangate45.lef"
+set fake_macro_lef "$LIB_DIR/fake_macros.lef"
+set liberty_file "$LIB_DIR/Nangate45_fast.lib"
+set fake_macro_lib "$LIB_DIR/fake_macros.lib"
+ 
+set synth_verilog "./testcases/macro_only.v"
+set floorplan_def "./testcases/macro_only.def"
+set top_module "macro_only"
+ 
+read_lef $tech_lef
+read_lef $std_cell_lef
+read_lef $fake_macro_lef
+read_liberty $liberty_file
+read_liberty $fake_macro_lib
+ 
+read_verilog $synth_verilog
+link_design $top_module
+#
+read_def $floorplan_def -floorplan_initialize
+
+set_thread_count 0
+rtl_macro_placer -report_directory results/macro_only -halo_width 4.0
+
+set def_file [make_result_file macro_only.def]
+write_def $def_file
+
+diff_files macro_only.defok $def_file
