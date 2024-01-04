df155d5b27edaa7dd1cbc6001cc82d4609326b45
Matt Liberty <mliberty@precisioninno.com>
2024-01-03 21:01:36 -0800

    Exclude .local from code coverage to avoid reporting on dependencies
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- etc/CodeCoverage.sh
+++ etc/CodeCoverage.sh
@@ -24,6 +24,7 @@ _lcov() {
         --exclude "/opt/*" \
         --exclude "/usr/lib/*" \
         --exclude "/usr/local/*" \
+        --exclude "*/.local/*" \
         --exclude "*build*" \
         --output-file ./coverage-output/main_coverage.info
 
