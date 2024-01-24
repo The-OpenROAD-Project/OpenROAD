d6a7b8fa293d3466c781534e844c8a9f08ff1e3f
Eder Monteiro <emrmonteiro@inf.ufrgs.br>
2024-01-24 20:07:54 -0300

    tap: avoid placing tapcells out of the rows
    Signed-off-by: Eder Monteiro <emrmonteiro@inf.ufrgs.br>

--- src/tap/src/tapcell.cpp
+++ src/tap/src/tapcell.cpp
@@ -231,9 +231,10 @@ int Tapcell::placeTapcells(odb::dbMaster* tapcell_master,
     x_loc = partially_overlap.right
                 ? partially_overlap.x_limit_right - tap_width
                 : x_loc;
-    if (!overlap
-        || ((partially_overlap.left || partially_overlap.right)
-            && !isOverlapping(x_loc, tap_width, ori, row_insts))) {
+    if ((!overlap
+         || ((partially_overlap.left || partially_overlap.right)
+             && !isOverlapping(x_loc, tap_width, ori, row_insts)))
+        && x_loc + tap_width <= urx) {
       const int lly = row_bb.yMin();
       auto* inst = makeInstance(
           db_->getChip()->getBlock(),
