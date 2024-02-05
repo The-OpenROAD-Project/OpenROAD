ba967f946923a4e57b8251e40acb5e7bf059e079
luarss <39641663+luarss@users.noreply.github.com>
2024-02-05 01:48:51 +0000

    change pandoc to nroff for cat file conversion
    Signed-off-by: luarss <39641663+luarss@users.noreply.github.com>

--- docs/src/test/man_func.tcl
+++ docs/src/test/man_func.tcl
@@ -3,9 +3,7 @@ source "helpers.tcl"
 
 # Test 1) Running man path with set manpath variable.
 set test_dir [file dirname [file normalize [info script]]]
-puts $test_dir
 set manpath_dir [file normalize [file dirname [file dirname [file normalize $test_dir]]]]
-puts $manpath_dir
 set manpath_dir "$manpath_dir/cat"
 set output [man openroad -manpath $manpath_dir -no_query]
 
