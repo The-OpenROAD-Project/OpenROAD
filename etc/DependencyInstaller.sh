8ffc100b44dc7cb567178dbb296d0e900800218e
habibayassin <habibayassin@aucegypt.edu>
2024-02-28 15:41:21 +0000

    update docker install
    Signed-off-by: habibayassin <habibayassin@aucegypt.edu>

--- etc/DependencyInstaller.sh
+++ etc/DependencyInstaller.sh
@@ -564,16 +564,27 @@ _installCI() {
     apt-get -y update
 
     #docker
-    apt-get -y install ca-certificates curl
-    install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    chmod a+r /etc/apt/keyrings/docker.asc
+    apt install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        software-properties-common
+    # apt-get -y install ca-certificates curl
+    # install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+    # curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    # chmod a+r /etc/apt/keyrings/docker.asc
+    # echo \
+    # "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    # $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    # tee /etc/apt/sources.list.d/docker.list > /dev/null
     echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    tee /etc/apt/sources.list.d/docker.list > /dev/null
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get -y update
-    apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    apt install -y docker-ce docker-ce-cli containerd.io
+    # apt-get -y install docker-ce docker-ce-cli containerd.io
 }
 
 _checkIsLocal() {
