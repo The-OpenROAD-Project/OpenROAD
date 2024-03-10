4f04f8d7ac71507d37450dfdbe2f4bc566b8fae9
Peter Gadfort <peter.gadfort@gmail.com>
2024-03-09 20:03:36 -0500

    pdn: add support for UPF powerswitches in PDN
    Signed-off-by: Peter Gadfort <peter.gadfort@gmail.com>

---
+++ src/pdn/test/power_switch_upf_error.tcl
@@ -0,0 +1,26 @@
+# test if UPF is loaded, information must come from UPF
+source "helpers.tcl"
+
+read_lef sky130hd/sky130hd.tlef
+read_lef sky130hd/sky130_fd_sc_hd_merged.lef
+read_lef sky130_power_switch/power_switch.lef
+
+read_verilog sky130_power_switch/netlist.v
+link_design gcd
+
+create_power_domain PD_TOP \
+  -elements {.}
+
+initialize_floorplan -utilization 10 -site unithd
+tapcell \
+  -distance 14 \
+  -tapcell_master "sky130_fd_sc_hd__tapvpwrvgnd_1"
+
+add_global_connection -net VDD -power -pin_pattern "^VDDG$"
+add_global_connection -net VDD_SW -power -pin_pattern "^VPB$"
+add_global_connection -net VDD_SW -pin_pattern "^VPWR$"
+add_global_connection -net VSS -power -pin_pattern "^VGND$"
+add_global_connection -net VSS -power -pin_pattern "^VNB$"
+
+catch {set_voltage_domain -power VDD -ground VSS -switched_power VDD_SW} err
+puts $err
