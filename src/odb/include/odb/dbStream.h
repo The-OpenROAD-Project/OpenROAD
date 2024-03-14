01ba5f849ecfa07a2dbebc6a6cc602ba24edb93d
andyfox-rushc <andy@rushc.com>
2024-03-14 14:50:28 -0700

    formatting change to dbStream.h

--- src/odb/include/odb/dbStream.h
+++ src/odb/include/odb/dbStream.h
@@ -192,7 +192,7 @@ class dbOStream
       return *this;
     } else {
       *this << std::get<I>(tup);
-      return ((*this).operator << <I + 1>(tup));
+      return ((*this).operator<< <I + 1>(tup));
     }
   }
 
