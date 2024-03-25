3eddbb0892f57eb36a588340f4cab043ce0d1ad2
Matt Liberty <mliberty@precisioninno.com>
2024-03-25 13:41:20 -0700

    rsz: check for exceeding max_buffer_percent after each endpoint
    Previously we only check after each pass.  For really bad designs this
    could be too infrequent.
    
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/rsz/src/RepairHold.cc
+++ src/rsz/src/RepairHold.cc
@@ -360,7 +360,10 @@ RepairHold::repairHoldPass(VertexSeq &hold_failures,
   for (Vertex *end_vertex : hold_failures) {
     resizer_->updateParasitics();
     repairEndHold(end_vertex, buffer_cell, setup_margin, hold_margin,
-                  allow_setup_violations, max_buffer_count);
+                  allow_setup_violations);
+    if (inserted_buffer_count_ > max_buffer_count) {
+      break;
+    }
   }
 }
 
@@ -369,8 +372,7 @@ RepairHold::repairEndHold(Vertex *end_vertex,
                           LibertyCell *buffer_cell,
                           const double setup_margin,
                           const double hold_margin,
-                          const bool allow_setup_violations,
-                          const int max_buffer_count)
+                          const bool allow_setup_violations)
 {
   PathRef end_path = sta_->vertexWorstSlackPath(end_vertex, min_);
   if (!end_path.isNull()) {
