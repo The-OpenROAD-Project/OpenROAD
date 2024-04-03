2a90ff4cdd4d361c4f063a8caf87bdb646bd22ea
Matt Liberty <mliberty@precisioninno.com>
2024-04-02 21:49:55 -0700

    gui: use a monospace font for multi-line properties in the inspector
    It makes it easier to read tables that align columns with whitespace.
    
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/gui/src/inspector.cpp
+++ src/gui/src/inspector.cpp
@@ -154,6 +154,11 @@ QStandardItem* SelectedItemModel::makeItem(const QString& name)
   item->setEditable(false);
   item->setSelectable(false);
   item->setData(Qt::AlignTop, Qt::TextAlignmentRole);
+  if (name.contains('\n')) {
+    QFont font("Monospace");
+    font.setStyleHint(QFont::Monospace);
+    item->setData(font, Qt::FontRole);
+  }
   return item;
 }
 
