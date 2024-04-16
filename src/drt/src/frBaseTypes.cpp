9f033ae806d94178f2cde75ae62698e6ecc6cbd5
osamahammad21 <osama21@aucegypt.edu>
2024-04-16 20:10:59 +0200

    drt: fix missing << frcLef58EnclosureConstraint
    Signed-off-by: osamahammad21 <osama21@aucegypt.edu>

--- src/drt/src/frBaseTypes.cpp
+++ src/drt/src/frBaseTypes.cpp
@@ -297,6 +297,8 @@ std::ostream& operator<<(std::ostream& os, frConstraintTypeEnum type)
       return os << "frcLef58TwoWiresForbiddenSpcConstraint";
     case frConstraintTypeEnum::frcLef58ForbiddenSpcConstraint:
       return os << "frcLef58ForbiddenSpcConstraint";
+    case frConstraintTypeEnum::frcLef58EnclosureConstraint:
+      return os << "frcLef58EnclosureConstraint";
   }
   return os << "Bad frConstraintTypeEnum";
 }
