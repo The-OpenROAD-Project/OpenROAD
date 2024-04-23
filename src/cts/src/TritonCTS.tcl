317aa4d83f25f568003d9fd12861afe75b38babc
arthur <arthurjolo@gmail.com>
2024-04-23 09:38:28 -0300

    fix the cts tcl command to disable dummy load
    Signed-off-by: arthur <arthurjolo@gmail.com>

--- src/cts/src/TritonCTS.tcl
+++ src/cts/src/TritonCTS.tcl
@@ -89,7 +89,7 @@ sta::define_cmd_args "clock_tree_synthesis" {[-wire_unit unit]
                                              [-insertion_delay] \
                                              [-no_insertion_delay] \
                                              [-sink_buffer_max_cap_derate] \
-                                             [-use_dummy_load] \
+                                             [-dont_use_dummy_load] \
                                              [-delay_buffer_derate] \
 };# checker off
 
@@ -103,7 +103,8 @@ proc clock_tree_synthesis { args } {
           -sink_clustering_levels -tree_buf \
           -sink_buffer_max_cap_derate -delay_buffer_derate} \
     flags {-post_cts_disable -sink_clustering_enable -balance_levels \
-           -obstruction_aware -apply_ndr -insertion_delay -no_insertion_delay -use_dummy_load
+           -obstruction_aware -apply_ndr -insertion_delay -no_insertion_delay \
+           -dont_use_dummy_load
   };# checker off
 
   sta::check_argc_eq0 "clock_tree_synthesis" $args
@@ -212,6 +213,12 @@ proc clock_tree_synthesis { args } {
   }
 
   cts::set_obstruction_aware [info exists flags(-obstruction_aware)]
+
+  if { [info exists flags(-dont_use_dummy_load)] } {
+    cts::set_dummy_load false
+  } else {
+    cts::set_dummy_load true
+  }
 
   cts::set_apply_ndr [info exists flags(-apply_ndr)]
 
