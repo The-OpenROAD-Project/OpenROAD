fe250e5de946fa817a37b3ac69f8ef0340112cf6
arthur <arthurjolo@gmail.com>
2024-05-02 15:25:56 -0300

    fix crash for commands when no design was loaded
    Signed-off-by: arthur <arthurjolo@gmail.com>

--- src/ant/src/AntennaChecker.tcl
+++ src/ant/src/AntennaChecker.tcl
@@ -34,9 +34,14 @@ sta::define_cmd_args "check_antennas" { [-verbose]\
 };# checker off
 
 proc check_antennas { args } {
+
   sta::parse_key_args "check_antennas" args \
     keys {-report_file -net} \
     flags {-verbose -report_violating_nets};# checker off
+
+  if { [ord::get_db_block] == "NULL" } {
+    utl::error ANT 3 "No design block found."
+  }
 
   sta::check_argc_eq0 "check_antennas" $args
 
