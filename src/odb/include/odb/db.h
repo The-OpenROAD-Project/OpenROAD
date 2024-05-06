d8f3f3107a3ca8d4f9b3f2925f37f0d9e8dbfc82
Augusto Berndt <gudehh@gmail.com>
2024-05-06 20:16:22 -0300

    odb: conversion functions between micron and DBUs Added bidirectional conversion functions for both linear and area measurements.
    Signed-off-by: Augusto Berndt <gudehh@gmail.com>

--- src/odb/include/odb/db.h
+++ src/odb/include/odb/db.h
@@ -1178,6 +1178,26 @@ class dbBlock : public dbObject
   int getDbUnitsPerMicron();
 
   ///
+  /// Convert a length from database units (DBUs) to microns.
+  ///
+  double dbuToMicrons(const int64_t dbu);
+
+  ///
+  /// Convert an area from database units squared (DBU^2) to square microns.
+  ///
+  double dbuAreaToMicrons(const int64_t dbu_area);
+
+  ///
+  /// Convert a length from microns to database units (DBUs).
+  ///
+  int64_t micronsToDbu(const double microns);
+
+  ///
+  /// Convert an area from square microns to database units squared (DBU^2).
+  ///
+  int64_t micronsAreaToDbu(const double micronsArea);
+  
+  ///
   /// Get the hierarchy delimeter.
   /// Returns (0) if the delimeter was not set.
   /// A hierarchy delimeter can only be set at the time
