ee70dfb2429aac470f4cd3a5de269c841442bec4
Arthur Koucher <arthurckoucher@gmail.com>
2024-05-07 16:32:16 -0300

    gui: format
    Signed-off-by: Arthur Koucher <arthurckoucher@gmail.com>

--- src/gui/src/timingWidget.cpp
+++ src/gui/src/timingWidget.cpp
@@ -140,7 +140,8 @@ void TimingWidget::setColumnDisplayMenu()
   int column_index = 0;
 
   // Populate with all the available columns' actions.
-  for (const auto& [column, name] : setup_timing_paths_model_->getColumnNames()) {
+  for (const auto& [column, name] :
+       setup_timing_paths_model_->getColumnNames()) {
     QAction* action = new QAction(name, this);
     action->setCheckable(true);
     action->setChecked(true);
