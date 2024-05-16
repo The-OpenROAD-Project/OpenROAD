e73aee44eda7d277f3b999a8633600f54aae615c
andyfox-rushc <andy@rushc.com>
2024-05-15 18:19:57 -0700

    Added -hier option to linker for hier2 test. Signed-off-by: Andy Fox <andy@rushc.com>
    Signed-off-by: andyfox-rushc <andy@rushc.com>

--- src/dbSta/test/hier2.tcl
+++ src/dbSta/test/hier2.tcl
@@ -3,7 +3,7 @@ source "helpers.tcl"
 read_lef example1.lef
 read_liberty example1_typ.lib
 read_verilog hier2.v
-link_design top
+link_design top -hier
 write_verilog hier2_out.v
 diff_files hier2_out.v hier2_out.vok
 
