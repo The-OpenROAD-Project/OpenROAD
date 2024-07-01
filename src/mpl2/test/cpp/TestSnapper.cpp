a25a4da5549f2eb803d67fa6464d64f34727b0b9
Emmanuella Rumanti <amuritna@gmail.com>
2024-07-01 16:31:33 +0000

    mpl2: apply clang formatting to proposed snapper test
    Signed-off-by: Emmanuella Rumanti <amuritna@gmail.com>

--- src/mpl2/test/cpp/TestSnapper.cpp
+++ src/mpl2/test/cpp/TestSnapper.cpp
@@ -1,28 +1,25 @@
+#include "../../src/hier_rtlmp.h"
 #include "gtest/gtest.h"
 #include "mpl2/rtl_mp.h"
-#include "../../src/hier_rtlmp.h"
 #include "odb/db.h"
 #include "odb/util.h"
 #include "utl/Logger.h"
 
 namespace mpl2 {
-class Mpl2SnapperTest : public ::testing::Test
-{
+class Mpl2SnapperTest : public ::testing::Test {
  protected:
   template <class T>
   using OdbUniquePtr = std::unique_ptr<T, void (*)(T*)>;
 
-  void SetUp() override
-  {
+  void SetUp() override {
     utl::Logger* logger = new utl::Logger();
 
     db_ = OdbUniquePtr<odb::dbDatabase>(odb::dbDatabase::create(),
                                         &odb::dbDatabase::destroy);
     chip_ = OdbUniquePtr<odb::dbChip>(odb::dbChip::create(db_.get()),
                                       &odb::dbChip::destroy);
-    block_ = OdbUniquePtr<odb::dbBlock>(
-        chip_->getBlock(), &odb::dbBlock::destroy);
-    
+    block_ =
+        OdbUniquePtr<odb::dbBlock>(chip_->getBlock(), &odb::dbBlock::destroy);
   }
 
   utl::Logger logger_;
@@ -32,16 +29,15 @@ class Mpl2SnapperTest : public ::testing::Test
   OdbUniquePtr<odb::dbBlock> block_{nullptr, &odb::dbBlock::destroy};
 };
 
-TEST_F(Mpl2SnapperTest, CanSetMacroForEmptyInstances)
-{
+TEST_F(Mpl2SnapperTest, CanSetMacroForEmptyInstances) {
   // create a simple block and then add 3 instances to that block
   // without any further configuration to each instance,
   // and then run setMacro(inst) on each instance
-  
+
   utl::Logger* logger = new utl::Logger();
   odb::dbDatabase* db_ = odb::dbDatabase::create();
   db_->setLogger(logger);
-  
+
   odb::dbTech* tech_ = odb::dbTech::create(db_, "tech");
   odb::dbLib* lib_ = odb::dbLib::create(db_, "lib", tech_, ',');
   odb::dbTechLayer::create(tech_, "L1", odb::dbTechLayerType::MASTERSLICE);
@@ -51,13 +47,15 @@ TEST_F(Mpl2SnapperTest, CanSetMacroForEmptyInstances)
   master_->setWidth(1000);
   master_->setHeight(1000);
   master_->setType(odb::dbMasterType::CORE);
-  odb::dbMTerm::create(master_, "in", odb::dbIoType::INPUT, odb::dbSigType::SIGNAL);
-  odb::dbMTerm::create(master_, "out", odb::dbIoType::OUTPUT, odb::dbSigType::SIGNAL);
+  odb::dbMTerm::create(master_, "in", odb::dbIoType::INPUT,
+                       odb::dbSigType::SIGNAL);
+  odb::dbMTerm::create(master_, "out", odb::dbIoType::OUTPUT,
+                       odb::dbSigType::SIGNAL);
   master_->setFrozen();
-  
+
   odb::dbBlock* block_ = odb::dbBlock::create(chip_, "simple_block");
   block_->setDieArea(odb::Rect(0, 0, 1000, 1000));
-  
+
   odb::dbDatabase::beginEco(block_);
   odb::dbInst* inst1 = odb::dbInst::create(block_, master_, "cells_1");
   odb::dbInst* inst2 = odb::dbInst::create(block_, master_, "cells_2");
