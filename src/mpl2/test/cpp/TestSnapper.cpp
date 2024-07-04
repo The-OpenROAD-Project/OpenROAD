e9b1102a13ac8798067ec3b83db54a21c4e79b19
Emmanuella Rumanti <amuritna@gmail.com>
2024-07-04 16:11:34 +0000

    mpl2: refactor TestSnapper
    Signed-off-by: Emmanuella Rumanti <amuritna@gmail.com>

--- src/mpl2/test/cpp/TestSnapper.cpp
+++ src/mpl2/test/cpp/TestSnapper.cpp
@@ -89,19 +89,6 @@ TEST_F(Mpl2SnapperTest, CanSnapMacros)
   // - returns computation results
   // inst->setOrigin
   // - can be checked using inst->getOrigin
-  
-  utl::Logger* logger = new utl::Logger();
-  odb::dbDatabase* db_ = odb::dbDatabase::create();
-  db_->setLogger(logger);
-  
-  odb::dbTech* tech_ = odb::dbTech::create(db_, "tech");
-  tech_->setManufacturingGrid(2);
-
-  odb::dbLib* lib_ = odb::dbLib::create(db_, "lib", tech_, ',');
-  odb::dbChip* chip_ = odb::dbChip::create(db_);
-
-  odb::dbTechLayer* layer1_ = odb::dbTechLayer::create(tech_, "layer1", odb::dbTechLayerType::CUT);
-  odb::dbTechLayer* layer2_ = odb::dbTechLayer::create(tech_, "layer2", odb::dbTechLayerType::CUT);
 
   // During the construction of a new HardMacro object
   // with input dbInst* inst, the following values are retrieved:
@@ -109,45 +96,71 @@ TEST_F(Mpl2SnapperTest, CanSnapMacros)
   // master->getWidth, master->getHeight, master->getMTerms,
   // mterm->getSigType, mterm->getMPins,
   // mpin->getGeometry, box->getBox (see: object.cpp)
-
-  odb::dbMaster* master_ = odb::dbMaster::create(lib_, "simple_master");
-  master_->setWidth(1000);
-  master_->setHeight(1000);
-  master_->setType(odb::dbMasterType::BLOCK); // snapper expects a block type
   
-  // component with 1 input, 1 output -> 2 terminals
-  odb::dbMTerm* mterm_i = odb::dbMTerm::create(master_, "in", odb::dbIoType::INPUT, odb::dbSigType::SIGNAL);
-  odb::dbMPin* mpin_i = odb::dbMPin::create(mterm_i);
-  odb::dbBox* box_i = odb::dbBox::create(mpin_i, layer1_, 0, 0, 500, 500);
+  utl::Logger* logger = new utl::Logger();
+  odb::dbDatabase* db_ = odb::dbDatabase::create();
+  db_->setLogger(logger);
+  
+  odb::dbTech* tech_ = odb::dbTech::create(db_, "tech");
+  odb::dbLib* lib_ = odb::dbLib::create(db_, "lib", tech_, ',');
+  odb::dbChip* chip_ = odb::dbChip::create(db_);
 
-  odb::dbMTerm* mterm_o = odb::dbMTerm::create(master_, "out", odb::dbIoType::OUTPUT, odb::dbSigType::SIGNAL);
-  odb::dbMPin* mpin_o = odb::dbMPin::create(mterm_o);
-  odb::dbBox* box_o = odb::dbBox::create(mpin_o, layer2_, 0, 0, 500, 500);
-
-  master_->setFrozen();
-
+  // create simple block of size 1000x1000
   odb::dbBlock* block_ = odb::dbBlock::create(chip_, "simple_block");
   block_->setDieArea(odb::Rect(0, 0, 1000, 1000));
   odb::dbGCellGrid* grid_ = odb::dbGCellGrid::create(block_);
 
+  // create 2 layers and configure with corresponding grid patterns
+  // grid pattern parameters: origin, line count, step
+  // (0, 50, 20) -> 0 20 40 60 80 ... 980
+  odb::dbTechLayer* layer1_ = odb::dbTechLayer::create(tech_, "layer1", odb::dbTechLayerType::CUT);
   odb::dbTrackGrid* track1 = odb::dbTrackGrid::create(block_, layer1_);
-  track1->addGridPatternX(0, 20, 10);
-  track1->addGridPatternY(0, 20, 10);
+  track1->addGridPatternX(0, 50, 20);
+  track1->addGridPatternY(0, 50, 20);
 
+  odb::dbTechLayer* layer2_ = odb::dbTechLayer::create(tech_, "layer2", odb::dbTechLayerType::CUT);
   odb::dbTrackGrid* track2 = odb::dbTrackGrid::create(block_, layer2_);
-  track2->addGridPatternX(0, 20, 10);
-  track2->addGridPatternY(0, 20, 10);
+  track2->addGridPatternX(0, 50, 20);
+  track2->addGridPatternY(0, 50, 20);
 
+  // set manufacturing grid size
+  tech_->setManufacturingGrid(5); 
+
+  // snapper expects a block type master
+  odb::dbMaster* master_ = odb::dbMaster::create(lib_, "simple_master");
+  master_->setWidth(1000);
+  master_->setHeight(1000);
+  master_->setType(odb::dbMasterType::BLOCK);
+  
+  // component with 1 input (layer1), 1 output (layer2) -> 2 terminals 
+  // each pin occupies (0, 0) to (50, 50)
+  odb::dbMTerm* mterm_i = odb::dbMTerm::create(master_, "in", odb::dbIoType::INPUT, odb::dbSigType::SIGNAL);
+  odb::dbMPin* mpin_i = odb::dbMPin::create(mterm_i);
+  odb::dbBox* box_i = odb::dbBox::create(mpin_i, layer1_, 0, 0, 50, 50);
+
+  odb::dbMTerm* mterm_o = odb::dbMTerm::create(master_, "out", odb::dbIoType::OUTPUT, odb::dbSigType::SIGNAL);
+  odb::dbMPin* mpin_o = odb::dbMPin::create(mterm_o);
+  odb::dbBox* box_o = odb::dbBox::create(mpin_o, layer2_, 0, 0, 50, 50);
+
+  master_->setFrozen();
+
+  // create 3 instances on block_
   odb::dbDatabase::beginEco(block_);
   odb::dbInst* inst1 = odb::dbInst::create(block_, master_, "cells_1");
-  odb::dbInst* inst2 = odb::dbInst::create(block_, master_, "cells_2");
-  odb::dbInst* inst3 = odb::dbInst::create(block_, master_, "cells_3");
+  //odb::dbInst* inst2 = odb::dbInst::create(block_, master_, "cells_2");
+  //odb::dbInst* inst3 = odb::dbInst::create(block_, master_, "cells_3");
   odb::dbDatabase::endEco(block_);
+
+  // inst setup: (all quantities in internal DB units)
+  // 1000 x 1000 master and block 
+  // with 2 pins, one on each of the 2 layers, at (0, 0, 50, 50)
+  // grid pattern is (0, 50, 20) -> 0 20 40 60 80 ... 980
+  // manufacturing grid size is 5
 
   Snapper snapper;
   snapper.setMacro(inst1);
-  
-  inst1->setOrigin(-250, 0);
+
+  inst1->setOrigin(435, 500);
 
   logger->report(
     "initial (x, y) of inst1: ({}, {})",
@@ -163,6 +176,7 @@ TEST_F(Mpl2SnapperTest, CanSnapMacros)
     inst1->getOrigin().y()
   );
 
+  /*
   snapper.setMacro(inst2);
   
   inst2->setOrigin(0, 0);
@@ -198,6 +212,7 @@ TEST_F(Mpl2SnapperTest, CanSnapMacros)
     inst3->getOrigin().x(),
     inst3->getOrigin().y()
   );
+  */
 }
 
 }  // namespace mpl2
