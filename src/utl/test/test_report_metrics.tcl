da39697193385bf4a84b2841c0afc3683c05488b
dav77227 <davitg@vorak-solutions.com>
2024-07-04 16:02:34 +0400

    Added results dir for metrics files dump
    Signed-off-by: dav77227 <davitg@vorak-solutions.com>

--- src/utl/test/test_report_metrics.tcl
+++ src/utl/test/test_report_metrics.tcl
@@ -3,4 +3,4 @@ read_lef ./../../../test/Nangate45/Nangate45.lef
 read_verilog ./../../../test/gcd_nangate45.v
 link_design gcd
 read_sdc ./../../../test/timing_api_2.sdc
-report_metrics 2 final
+report_metrics 2 final false false ./results/
