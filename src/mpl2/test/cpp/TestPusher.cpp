67570e4294c11d640d4fdd2dd9f97b62d78b012e
Emmanuella Rumanti <amuritna@gmail.com>
2024-07-09 05:17:18 +0000

    mpl2: Remove typos
    Signed-off-by: Emmanuella Rumanti <amuritna@gmail.com>

--- src/mpl2/test/cpp/TestPusher.cpp
+++ src/mpl2/test/cpp/TestPusher.cpp
@@ -59,8 +59,8 @@ TEST_F(Mpl2PusherTest, CanConstructPusher)
   odb::dbMTerm::create(
       master_, "out", odb::dbIoType::OUTPUT, odb::dbSigType::SIGNAL);
   master_->setFrozen();
-=
-  odb::dbInst* inst1 = odb::dbInst::create(block_, master_, "cells_1");=
+
+  odb::dbInst* inst1 = odb::dbInst::create(block_, master_, "cells_1");
 
   // Case 1:
   // cluster1 is HardMacroCluster
