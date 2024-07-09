97e02954a81163464a98c0554bb4a874f98cb12c
Emmanuella Rumanti <amuritna@gmail.com>
2024-07-09 14:04:50 +0000

    mpl2: Fix padding issue
    Signed-off-by: Emmanuella Rumanti <amuritna@gmail.com>

--- src/mpl2/test/cpp/helper/helper.h
+++ src/mpl2/test/cpp/helper/helper.h
@@ -5,9 +5,9 @@ namespace mpl2 {
 odb::dbDatabase* createSimpleDB();
 
 odb::dbMaster* createSimpleMaster(odb::dbLib* lib,
-                               const char* name,
-                               uint width,
-                               uint height,
-                               const odb::dbMasterType& type);
+                                  const char* name,
+                                  uint width,
+                                  uint height,
+                                  const odb::dbMasterType& type);
 
 }  // namespace mpl2
