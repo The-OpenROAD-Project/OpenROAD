321d1a6714540a94764af662fde0858a68f7341a
Vitor Bandeira <vvbandeira@precisioninno.com>
2024-07-17 21:11:46 +0000

    etc: install buildx
    Signed-off-by: Vitor Bandeira <vvbandeira@precisioninno.com>

--- etc/DependencyInstaller.sh
+++ etc/DependencyInstaller.sh
@@ -580,15 +580,24 @@ _installCI() {
         parallel \
         software-properties-common \
         unzip
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo \
-    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    # Add Docker's official GPG key:
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+        -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        tee /etc/apt/sources.list.d/docker.list > /dev/null
+
     apt-get -y update
     apt-get -y install --no-install-recommends \
-        containerd.io \
         docker-ce \
-        docker-ce-cli
+        docker-ce-cli \
+        containerd.io \
+        docker-buildx-plugin
 }
 
 _checkIsLocal() {
