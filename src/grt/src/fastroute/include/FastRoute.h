650657d72e38bed1d6fed145682ef1b5a7099a7c
Eder Monteiro <eder.matheus.monteiro@gmail.com>
2024-07-23 13:11:50 -0300

    grt: address comments
    Signed-off-by: Eder Monteiro <eder.matheus.monteiro@gmail.com>

--- src/grt/src/fastroute/include/FastRoute.h
+++ src/grt/src/fastroute/include/FastRoute.h
@@ -125,7 +125,7 @@ class FastRouteCore
                      int x2,
                      int y2,
                      int layer,
-                     int reducedCap,
+                     uint16_t reducedCap,
                      bool isReduce);
   void applyVerticalAdjustments(const odb::Point& first_tile,
                                 const odb::Point& last_tile,
--- src/grt/src/fastroute/src/FastRoute.cpp
+++ src/grt/src/fastroute/src/FastRoute.cpp
@@ -380,7 +380,7 @@ void FastRouteCore::addAdjustment(int x1,
                                   int x2,
                                   int y2,
                                   int layer,
-                                  int reducedCap,
+                                  uint16_t reducedCap,
                                   bool isReduce)
 {
   const int k = layer - 1;
@@ -409,10 +409,10 @@ void FastRouteCore::addAdjustment(int x1,
     if (!isReduce) {
       const int increase = reducedCap - cap;
       h_edges_[y1][x1].cap += increase;
-      int new_red = static_cast<int>(h_edges_[y1][x1].red) - increase;
-      int new_red_3D = static_cast<int>(h_edges_3D_[k][y1][x1].red) - increase;
-      h_edges_[y1][x1].red = new_red >= 0 ? new_red : 0;
-      h_edges_3D_[k][y1][x1].red = new_red_3D >= 0 ? new_red_3D : 0;
+      int new_red = h_edges_[y1][x1].red - increase;
+      int new_red_3D = h_edges_3D_[k][y1][x1].red - increase;
+      h_edges_[y1][x1].red = std::max(new_red, 0);
+      h_edges_3D_[k][y1][x1].red = std::max(new_red_3D, 0);
     } else {
       h_edges_3D_[k][y1][x1].red += reduce;
     }
@@ -443,10 +443,10 @@ void FastRouteCore::addAdjustment(int x1,
     if (!isReduce) {
       int increase = reducedCap - cap;
       v_edges_[y1][x1].cap += increase;
-      int new_red = static_cast<int>(v_edges_[y1][x1].red) - increase;
-      int new_red_3D = static_cast<int>(v_edges_3D_[k][y1][x1].red) - increase;
-      v_edges_[y1][x1].red = new_red >= 0 ? new_red : 0;
-      v_edges_3D_[k][y1][x1].red = new_red_3D >= 0 ? new_red_3D : 0;
+      int new_red = v_edges_[y1][x1].red - increase;
+      int new_red_3D = v_edges_3D_[k][y1][x1].red - increase;
+      v_edges_[y1][x1].red = std::max(new_red, 0);
+      v_edges_3D_[k][y1][x1].red = std::max(new_red_3D, 0);
     } else {
       v_edges_3D_[k][y1][x1].red += reduce;
     }
