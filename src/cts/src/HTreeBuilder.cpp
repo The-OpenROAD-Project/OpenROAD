96dd5032d5e079e4fb2cf9c6137c2b283a33554e
arthur <arthurjolo@gmail.com>
2024-08-07 16:51:59 -0300

    add debug prints again
    Signed-off-by: arthur <arthurjolo@gmail.com>

--- src/cts/src/HTreeBuilder.cpp
+++ src/cts/src/HTreeBuilder.cpp
@@ -1175,8 +1175,9 @@ void HTreeBuilder::run()
       logger_->info(CTS,
                     32,
                     " Stop criterion found. Max number of sinks is {}.",
-                    options_->getMaxFanout() ? options_->getMaxFanout()
-                                             : numMaxLeafSinks_);
+                    options_->getMaxFanout()
+                    ? options_->getMaxFanout()
+                    : numMaxLeafSinks_);
       break;
     }
   }
@@ -1213,6 +1214,7 @@ void HTreeBuilder::run()
 bool HTreeBuilder::isNumberOfSinksTooSmall(unsigned numSinksPerSubRegion) const
 {
   if (options_->getMaxFanout()) {
+    logger_->report("Ta verificando por fanout");
     return numSinksPerSubRegion < options_->getMaxFanout();
   }
   return numSinksPerSubRegion < numMaxLeafSinks_;
