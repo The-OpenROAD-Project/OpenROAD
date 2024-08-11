197a7c57b05d62f65e0d1be29867b7f8a6496652
andyfox-rushc <andy@rushc.com>
2024-08-11 12:42:04 -0700

    definReader order for hierarchical iterator use. Signed-off-by: Andy Fox andy@rushc.com
    Signed-off-by: andyfox-rushc <andy@rushc.com>

--- src/odb/src/defin/definReader.cpp
+++ src/odb/src/defin/definReader.cpp
@@ -2137,11 +2137,15 @@ bool definReader::createBlock(const char* file)
     }
   }
   defrClear();
-  /*
-   * dbModule* top_module = _block->getTopModule();
-  top_module->getChildren().reverse();
-  top_module->getInsts().reverse();
-  */
+
+  dbModule* top_module = _block->getTopModule();
+
+  if (top_module->getChildren().size() > 0) {
+    top_module->getChildren().reverse();
+  }
+  if (top_module->getInsts().size() > 0) {
+    top_module->getInsts().reverse();
+  }
   return true;
   // 1220 return errors() == 0;
 }
