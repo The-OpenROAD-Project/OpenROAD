475c5afb67a50fe64a5fd0e609cc5b84d6421c3a
fangzhong-lyu <soxehli@gmail.com>
2024-08-11 22:58:19 -0400

    fix formatting
    Signed-off-by: fangzhong-lyu <soxehli@gmail.com>

--- src/odb/include/odb/gdsUtil.h
+++ src/odb/include/odb/gdsUtil.h
@@ -1,30 +1,76 @@
-#pragma once 
+#pragma once
+
+#include <endian.h>
 
 #include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <endian.h>
 
 namespace odb {
 
 enum RecordType
 {
-  HEADER = 0,     BGNLIB = 1,     LIBNAME = 2,      UNITS = 3, 
-  ENDLIB = 4,     BGNSTR = 5,     STRNAME = 6 ,     ENDSTR = 7,
-  BOUNDARY = 8,   PATH = 9,       SREF = 10,        AREF = 11,
-  TEXT = 12,      LAYER = 13,     DATATYPE = 14,    WIDTH = 15,
-  XY = 16,        ENDEL = 17,     SNAME = 18,       COLROW = 19,
-  TEXTNODE = 20,  NODE = 21,      TEXTTYPE = 22,    PRESENTATION = 23,
-  SPACING = 24,   STRING = 25,    STRANS = 26,      MAG = 27,
-  ANGLE = 28,     UINTEGER = 29,  USTRING = 30,     REFLIBS = 31,
-  FONTS = 32,     PATHTYPE = 33,  GENERATIONS = 34, ATTRTABLE = 35,
-  STYPTABLE = 36, STRTYPE = 37,   ELFLAGS = 38,     ELKEY = 39,
-  LINKTYPE = 40,  LINKKEYS = 41,  NODETYPE = 42,    PROPATTR = 43,
-  PROPVALUE = 44, BOX = 45,       BOXTYPE = 46,     PLEX = 47,
-  BGNEXTN = 48,   ENDEXTN = 49,   TAPENUM = 50,     TAPECODE = 51,
-  STRCLASS = 52,  RESERVED = 53,  FORMAT = 54,      MASK = 55,
-  ENDMASKS = 56,  LIBDIRSIZE = 57, SRFNAME = 58,    LIBSECUR = 59,
+  HEADER = 0,
+  BGNLIB = 1,
+  LIBNAME = 2,
+  UNITS = 3,
+  ENDLIB = 4,
+  BGNSTR = 5,
+  STRNAME = 6,
+  ENDSTR = 7,
+  BOUNDARY = 8,
+  PATH = 9,
+  SREF = 10,
+  AREF = 11,
+  TEXT = 12,
+  LAYER = 13,
+  DATATYPE = 14,
+  WIDTH = 15,
+  XY = 16,
+  ENDEL = 17,
+  SNAME = 18,
+  COLROW = 19,
+  TEXTNODE = 20,
+  NODE = 21,
+  TEXTTYPE = 22,
+  PRESENTATION = 23,
+  SPACING = 24,
+  STRING = 25,
+  STRANS = 26,
+  MAG = 27,
+  ANGLE = 28,
+  UINTEGER = 29,
+  USTRING = 30,
+  REFLIBS = 31,
+  FONTS = 32,
+  PATHTYPE = 33,
+  GENERATIONS = 34,
+  ATTRTABLE = 35,
+  STYPTABLE = 36,
+  STRTYPE = 37,
+  ELFLAGS = 38,
+  ELKEY = 39,
+  LINKTYPE = 40,
+  LINKKEYS = 41,
+  NODETYPE = 42,
+  PROPATTR = 43,
+  PROPVALUE = 44,
+  BOX = 45,
+  BOXTYPE = 46,
+  PLEX = 47,
+  BGNEXTN = 48,
+  ENDEXTN = 49,
+  TAPENUM = 50,
+  TAPECODE = 51,
+  STRCLASS = 52,
+  RESERVED = 53,
+  FORMAT = 54,
+  MASK = 55,
+  ENDMASKS = 56,
+  LIBDIRSIZE = 57,
+  SRFNAME = 58,
+  LIBSECUR = 59,
   INVALID_RT = 60
 };
 
@@ -36,11 +82,19 @@ uint8_t fromRecordType(RecordType recordType);
 
 extern const char* recordNames[];
 
-enum DataType {
-  NO_DATA = 0, BIT_ARRAY, INT_2, INT_4, REAL_4, REAL_8, ASCII_STRING, INVALID_DT
+enum DataType
+{
+  NO_DATA = 0,
+  BIT_ARRAY,
+  INT_2,
+  INT_4,
+  REAL_4,
+  REAL_8,
+  ASCII_STRING,
+  INVALID_DT
 };
 
-static const size_t dataTypeSize[DataType::INVALID_DT] = { 1, 1, 2, 4, 4, 8, 1 };
+static const size_t dataTypeSize[DataType::INVALID_DT] = {1, 1, 2, 4, 4, 8, 1};
 
 double real8_to_double(uint64_t real);
 
@@ -61,7 +115,7 @@ typedef struct
   std::vector<double> data64;
 } record_t;
 
-std::map<std::pair<int16_t, int16_t>, std::string> getLayerMap(std::string filename);
+std::map<std::pair<int16_t, int16_t>, std::string> getLayerMap(
+    std::string filename);
 
-
-} // namespace odb
+}  // namespace odb
