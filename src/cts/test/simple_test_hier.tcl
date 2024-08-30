86a4a423036c6e318bfa4d1bf99fc54570c6bf1a
andyfox-rushc <andy@rushc.com>
2024-08-30 09:41:20 -0700

    Remove pad insertion in simple_test_hier. Signed-off-by: Andy Fox <andy@rushc.com>
    Signed-off-by: andyfox-rushc <andy@rushc.com>

--- src/cts/test/simple_test_hier.tcl
+++ src/cts/test/simple_test_hier.tcl
@@ -1,17 +1,17 @@
 source "helpers.tcl"
 read_lef Nangate45/Nangate45.lef
-read_lef dummy_pads.lef
+#read_lef dummy_pads.lef
 read_liberty Nangate45/Nangate45_typ.lib
 read_verilog simple_test_hier.v
 link test_16_sinks -hier
 
 initialize_floorplan -die_area "0 0 40 1200"   -core_area "0 0 40 1200" -site FreePDK45_38x28_10R_NP_162NW_34O
-make_io_sites -horizontal_site IOSITE -vertical_site IOSITE -corner_site IOSITE -offset 15
+#make_io_sites -horizontal_site IOSITE -vertical_site IOSITE -corner_site IOSITE -offset 15
 
 source Nangate45/Nangate45.vars
 source Nangate45/Nangate45.rc
 
-place_pad -master PADCELL_SIG_V -row IO_EAST -location 500 "clk"
+#place_pad -master PADCELL_SIG_V -row IO_EAST -location 500 "clk"
 
 global_placement -skip_nesterov_place 
 detailed_placement
