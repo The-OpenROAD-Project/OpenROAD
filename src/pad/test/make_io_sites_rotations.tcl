0a8d33aa21a553e51d4747e0966aeb249e0f76f3
Peter Gadfort <gadfort@zeroasic.com>
2024-09-25 09:57:59 -0400

    pad: fix test and update README to reflect the new base rotations
    Signed-off-by: Peter Gadfort <gadfort@zeroasic.com>

--- src/pad/test/make_io_sites_rotations.tcl
+++ src/pad/test/make_io_sites_rotations.tcl
@@ -9,7 +9,7 @@ read_def Nangate45_blackparrot/floorplan.def
 
 # Test make_io_sites
 make_io_sites -horizontal_site IOSITE -vertical_site IOSITE2 -corner_site IOSITE -offset 15 \
-    -rotation_horizontal R90 \
+    -rotation_horizontal MXR90 \
     -rotation_vertical MY
 
 set def_file [make_result_file "make_io_sites_rotations.def"]
