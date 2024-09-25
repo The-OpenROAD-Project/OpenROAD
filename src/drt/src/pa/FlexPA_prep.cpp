2cddbd64cd896b0f56a1573b7460b86bd75d2f3d
bernardo <bernardoborgessandoval@gmail.com>
2024-09-25 19:51:17 +0000

    drt: inst_term nullptr error raising
    Signed-off-by: bernardo <bernardoborgessandoval@gmail.com>

--- src/drt/src/pa/FlexPA_prep.cpp
+++ src/drt/src/pa/FlexPA_prep.cpp
@@ -1499,7 +1499,10 @@ int FlexPA::initPinAccess(T* pin, frInstTerm* inst_term)
     if (is_macro_cell_pin) {
       macro_cell_pin_no_ap_cnt_++;
     }
-  } else if (inst_term) {
+  } else {
+    if (inst_term == nullptr) {
+      logger_->error(DRT, 254, "inst_term can not be nullptr");
+    }
     // write to pa
     const int pin_access_idx = unique_insts_.getPAIndex(inst_term->getInst());
     for (auto& ap : aps) {
