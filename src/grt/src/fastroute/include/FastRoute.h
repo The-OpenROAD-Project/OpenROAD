ef980eb1578acfc9062e481c21cff2e5fb87be83
João Mai <joaomai2001@gmail.com>
2024-09-29 03:02:33 -0300

    grt: better description for unused functions that are useful for testing during development on FastRoute
    Signed-off-by: João Mai <joaomai2001@gmail.com>

--- src/grt/src/fastroute/include/FastRoute.h
+++ src/grt/src/fastroute/include/FastRoute.h
@@ -496,6 +496,17 @@ class FastRouteCore
   void checkRoute3D();
   void StNetOrder();
   float CalculatePartialSlack();
+  /**
+   * @brief Validates the routing of edges for a specified net.
+   *
+   * Checks various aspects of the routing, including edge lengths,
+   * grid positions, and the distances between routing points. Warnings are logged
+   * if verbose_ is set.
+   *
+   * @param netID The ID of the net to be validated.
+   *
+   * @returns true if any issues are detected in the routing. Else, false.
+   */
   bool checkRoute2DTree(int netID);
   void removeLoops();
   void netedgeOrderDec(int netID);
@@ -504,8 +515,22 @@ class FastRouteCore
   void printEdge3D(int netID, int edgeID);
   void printTree3D(int netID);
   void check2DEdgesUsage();
+  /**
+   * @brief Compares edges used by routing and usage in h/v edges of the routing graph.
+   *
+   * Performs a full comparison between edges used by routing and
+   * usage in the h/v edges of the routing graph. 
+   * Somewhat expensive but helpful for finding usage errors.
+   */
   void verify2DEdgesUsage();
-  void verifyEdgeUsage();
+    /**
+   * @brief Compares edges used by routing and usage in h/v edges of the routing graph.
+   *
+   * Perfmorms a full comparison between edges used by routing and
+   * usage in the h/v edges of the routing graph. 
+   * Somewhat expensive but helpful for finding usage errors.
+   */
+  void verify3DEdgesUsage();
   void layerAssignment();
   void copyBR(void);
   void copyRS(void);
