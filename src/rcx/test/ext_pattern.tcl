33933e06e7de2ad6470948528e86432997b7a969
dimitris-ic <dimitris@fotakis.org>
2024-10-10 16:51:27 -0700

    add all args on a single line for extract_parasitics

--- src/rcx/test/ext_pattern.tcl
+++ src/rcx/test/ext_pattern.tcl
@@ -6,9 +6,7 @@ read_lef sky130hs/sky130hs.tlef
 read_def generate_pattern.defok
 
 define_process_corner -ext_model_index 0 X
-extract_parasitics -ext_model_file ext_pattern.rules \
-      -cc_model 12 -max_res 0 -context_depth 10 \
-      -coupling_threshold 0.1
+extract_parasitics -ext_model_file ext_pattern.rules -cc_model 12 -max_res 0 -context_depth 10 -coupling_threshold 0.1
 
 set spef_file [make_result_file ext_pattern.spef]
 write_spef $spef_file -nets $test_nets
