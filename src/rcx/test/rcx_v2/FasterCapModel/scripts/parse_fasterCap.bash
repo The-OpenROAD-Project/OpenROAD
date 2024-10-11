9e8be7aeed0d2445df9c6fe16158a6361c179321
dimitris-ic <dimitris@fotakis.org>
2024-10-11 12:31:38 -0700

    v2_merge: complete reg test for SolverGen

--- src/rcx/test/rcx_v2/FasterCapModel/scripts/parse_fasterCap.bash
+++ src/rcx/test/rcx_v2/FasterCapModel/scripts/parse_fasterCap.bash
@@ -21,8 +21,9 @@ mkdir $dir
 cd $dir
 pwd
 
-echo "find $data_dir/$data_file_name -name "wires.log" -print > wire_log_file_list "
-find $data_dir/$data_file_name -name "wires.log" -print > wire_log_file_list 
+find_start_dir="$data_dir/$data_file_name"
+echo "find $find_start_dir -name "wires.log" -print > wire_log_file_list "
+find $find_start_dir  -name "wires.log" -print > wire_log_file_list 
 sort wire_log_file_list > sorted.input.list 
 
 python3 $python_script -in_list_file  sorted.input.list -wire $wire -out_file $data_file_name.caps > OUT 
