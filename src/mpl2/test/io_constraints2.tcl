e1451069649549f713b4981fbb1d23d253d15343
Arthur Koucher <arthurkoucher@precisioninno.com>
2024-10-11 15:09:21 -0300

    mpl2: add test for blocked boundaries' pin access
    Signed-off-by: Arthur Koucher <arthurkoucher@precisioninno.com>

--- src/mpl2/test/io_constraints2.tcl
+++ src/mpl2/test/io_constraints2.tcl
@@ -1,5 +1,5 @@
 # Test if pin access blockage is generated correctly for a case
-# with pins in a single boundary.
+# with all boundaries blocked except one.
 source "helpers.tcl"
 
 # We're not interested in the connections, so don't include the lib.
@@ -12,14 +12,14 @@ read_verilog "./testcases/io_constraints1.v"
 link_design "io_constraints1"
 read_def "./testcases/io_constraints1.def" -floorplan_initialize
 
-# Run random PPL to incorporate the constraints into ODB
-set_io_pin_constraint -direction INPUT -region left:*
-place_pins -annealing -random -hor_layers metal5 -ver_layer metal6
+# Run random PPL to incorporate the -exclude constraints into ODB
+place_pins -annealing -random -hor_layers metal5 -ver_layer metal6 \
+           -exclude left:* -exclude right:* -exclude top:*
 
 set_thread_count 0
-rtl_macro_placer -report_directory results/io_constraints1 -halo_width 4.0
+rtl_macro_placer -report_directory results/io_constraints2 -halo_width 4.0
 
-set def_file [make_result_file io_constraints1.def]
+set def_file [make_result_file io_constraints2.def]
 write_def $def_file
 
-diff_files io_constraints1.defok $def_file
+diff_files io_constraints2.defok $def_file
--- src/mpl2/test/regression_tests.tcl
+++ src/mpl2/test/regression_tests.tcl
@@ -2,6 +2,7 @@ record_tests {
     macro_only
     no_unfixed_macros
     io_constraints1
+    io_constraints2
     #mpl2_man_tcl_check
     #mpl2_readme_msgs_check
 }
