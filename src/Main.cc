e2da153501f22edad963b82d9b4585b4816b948c
Matt Liberty <mliberty@precisioninno.com>
2024-10-29 04:54:07 +0000

    ord: fix last commit
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/Main.cc
+++ src/Main.cc
@@ -204,6 +204,12 @@ static void initPython()
 
 static volatile sig_atomic_t fatal_error_in_progress = 0;
 
+// When we enter through main() we have a single tech and design.
+// Custom applications using OR as a library might define multiple.
+// Such applications won't allocate or use these objects.
+static std::unique_ptr<ord::Tech> the_tech;
+static std::unique_ptr<ord::Design> the_design;
+
 static void handler(int sig)
 {
   if (fatal_error_in_progress) {
@@ -265,11 +271,8 @@ int main(int argc, char* argv[])
   cmd_argc = argc;
   cmd_argv = argv;
 
-  // When we enter through main() we have a single tech and design.
-  // Custom applications using OR as a library might define multiple.
-  // Such applications won't allocate or use these objects.
-  auto the_tech = std::make_unique<ord::Tech>();
-  auto the_design = std::make_unique<ord::Design>(the_tech.get());
+  the_tech = std::make_unique<ord::Tech>();
+  the_design = std::make_unique<ord::Design>(the_tech.get());
   ord::OpenRoad::setOpenRoad(the_design->getOpenRoad());
 #ifdef ENABLE_PYTHON3
   if (findCmdLineFlag(cmd_argc, cmd_argv, "-python")) {
