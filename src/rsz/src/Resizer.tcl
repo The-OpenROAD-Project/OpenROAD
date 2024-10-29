2affff2a7928bea92417f8c7964192ab57b0cd17
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-10-29 13:45:01 +0100

    report_design_area: allow redirection to file
    same as all other commands, don't know why this one slipped
    between the cracks
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- src/rsz/src/Resizer.tcl
+++ src/rsz/src/Resizer.tcl
@@ -637,7 +637,7 @@ proc repair_timing { args } {
 
 sta::define_cmd_args "report_design_area" {}
 
-proc report_design_area { args } {
+sta::proc_redirect report_design_area { args } {
   sta::parse_key_args "report_design_area" args keys {} flags {}
   set util [format %.0f [expr [rsz::utilization] * 100]]
   set area [sta::format_area [rsz::design_area] 0]
