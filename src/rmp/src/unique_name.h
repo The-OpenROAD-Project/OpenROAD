bf9d3eb4b54aa85ac495d8369a5fb08e4db803aa
Ethan Mahintorabi <ethanmoon@google.com>
2024-11-04 04:22:07 +0000

    rmp: Adds backedge from ABC to OR
    This PR adds a new function called LogicCut::InsertMappedAbcNetwork,
    this function takes an Abc_Ntk_t* and reinserts it into the STA
    netlist.
    
    This function also removes the old logic cut and replaces it with
    new instances derived from the Abc network.
    
    Only thing left to do is optimize the abc network in some positive
    way.
    
    😎
    
    Signed-off-by: Ethan Mahintorabi <ethanmoon@google.com>

---
+++ src/rmp/src/unique_name.h
@@ -0,0 +1,27 @@
+// Copyright 2024 Google LLC
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+#pragma once
+
+#include <atomic>
+#include <string>
+
+namespace rmp {
+class UniqueName
+{
+ public:
+  UniqueName() = default;
+
+  std::string GetUniqueName(std::string prefix = "rmp_")
+  {
+    int64_t id = counter++;
+    return prefix + std::to_string(id);
+  }
+
+ private:
+  std::atomic<int64_t> counter{0};
+};
+}  // namespace rmp
