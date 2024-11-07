064cfbca572075edc21ead0ce64c3f7eb82597b8
Vitor Bandeira <vvbandeira@precisioninno.com>
2024-11-07 17:18:11 +0000

    etc: add docker fromImage checker
    There can be a case where we want to build a "builder" or "binary" docker
    image, but the "dev" image needed is not available yet. With this change
    the script should generate the dependency images automatically.
    
    Signed-off-by: Vitor Bandeira <vvbandeira@precisioninno.com>

--- etc/DockerHelper.sh
+++ etc/DockerHelper.sh
@@ -8,6 +8,7 @@ baseDir="$(pwd)"
 # docker hub organization/user from where to pull/push images
 org=openroad
 depsPrefixesFile="/etc/openroad_deps_prefixes.txt"
+args=("${@}")
 
 _help() {
     cat <<EOF
@@ -139,7 +140,54 @@ _test() {
     docker run --rm "${imagePath}" "./docker/test_wrapper.sh" "${compiler}" "ctest --test-dir build -j ${numThreads}"
 }
 
+_checkFromImage() {
+    set +e
+    # Check if the image exists locally
+    if docker image inspect "${fromImage}" > /dev/null 2>&1; then
+        echo "Image '${fromImage}' exists locally."
+    else
+        echo "Image '${fromImage}' does not exist locally. Attempting to pull..."
+        # Try to pull the image
+        if docker pull "${fromImage}"; then
+            echo "Successfully pulled '${fromImage}'."
+        else
+            echo "Unable to pull '${fromImage}'. Attempting to build..."
+            # Build the image using the createImage command
+            newArgs=""
+            newTarget=""
+            for arg in "${args[@]}"; do
+                # Check if the argument matches -target=builder
+                if [[ "${arg}" == "-target=builder" ]]; then
+                    newTarget="dev"
+                elif [[ "${arg}" == "-target=binary" ]]; then
+                    newTarget="builder"
+                else
+                    newArgs+=" ${arg}"
+                fi
+            done
+            if [[ "${newTarget}" == "" ]]; then
+                echo "Error"
+                exit 1
+            fi
+            newArgs+=" -target=${newTarget}"
+            createImage="$0 ${newArgs}"
+            echo "Running: ${createImage}"
+            if ${createImage}; then
+                echo "Successfully built '${newTarget}' image."
+            else
+                echo "Failed to build '${newTarget}' needed for '${target}' target."
+                return 1
+            fi
+        fi
+    fi
+    set -e
+    exit 42
+}
+
 _create() {
+    if [[ "${target}" != "dev" ]]; then
+        _checkFromImage
+    fi
     echo "Create docker image ${imagePath} using ${file}"
     eval docker buildx build \
         --file "${file}" \
