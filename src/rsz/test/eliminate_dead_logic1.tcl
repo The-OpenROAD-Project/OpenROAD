a7704c0cfa4b2289a3d601ca1caf30321d394f15
Martin Povišer <povik@cutebit.org>
2024-11-13 09:48:49 +0100

    rsz: Use .vok instead of pasting verilog into test log
    Signed-off-by: Martin Povišer <povik@cutebit.org>

--- src/rsz/test/eliminate_dead_logic1.tcl
+++ src/rsz/test/eliminate_dead_logic1.tcl
@@ -1,3 +1,6 @@
+# make_result_file, diff_files
+source "helpers.tcl"
+
 read_lef sky130hd/sky130hd.tlef
 read_lef sky130hd/sky130hd_std_cell.lef
 read_liberty sky130hd/sky130hd_tt.lib
@@ -7,4 +10,7 @@ link_design top
 
 set_dont_touch \q2[0]
 eliminate_dead_logic
-write_verilog /dev/stdout
+
+set verilog_file [make_result_file eliminate_dead_logic1.v]
+write_verilog $verilog_file
+diff_files $verilog_file eliminate_dead_logic1.vok
