9a230683817fa45a1377c8d352f29228dc9229b3
Matt Liberty <mliberty@precisioninno.com>
2024-11-27 18:24:51 +0000

    odb: rm dbMasterType::NONE
    This is useless as LEF states:
    
    If you do not specify CLASS, the macro is considered a CORE macro...
    
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/drt/src/serialization.h
+++ src/drt/src/serialization.h
@@ -361,7 +361,7 @@ void serialize(Archive& ar,
 template <class Archive>
 void serialize(Archive& ar, odb::dbMasterType& type, const unsigned int version)
 {
-  odb::dbMasterType::Value v = odb::dbMasterType::NONE;
+  odb::dbMasterType::Value v = odb::dbMasterType::CORE;
   if (drt::is_loading(ar)) {
     (ar) & v;
     type = odb::dbMasterType(v);
