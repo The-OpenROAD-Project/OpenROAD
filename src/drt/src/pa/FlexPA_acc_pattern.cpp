61d8e6a446cbeafab832f991e666ef5bda083f5a
bernardo <bernardoborgessandoval@gmail.com>
2024-12-15 00:20:14 +0000

    move prepPattern
    Signed-off-by: bernardo <bernardoborgessandoval@gmail.com>

--- src/drt/src/pa/FlexPA_acc_pattern.cpp
+++ src/drt/src/pa/FlexPA_acc_pattern.cpp
@@ -49,6 +49,133 @@ namespace drt {
 
 using utl::ThreadException;
 
+static inline void serializePatterns(
+    const std::vector<std::vector<std::unique_ptr<FlexPinAccessPattern>>>&
+        patterns,
+    const std::string& file_name)
+{
+  std::ofstream file(file_name.c_str());
+  frOArchive ar(file);
+  registerTypes(ar);
+  ar << patterns;
+  file.close();
+}
+
+void FlexPA::prepPattern()
+{
+  ProfileTask profile("PA:pattern");
+
+  const auto& unique = unique_insts_.getUnique();
+
+  // revert access points to origin
+  unique_inst_patterns_.resize(unique.size());
+
+  int cnt = 0;
+
+  omp_set_num_threads(router_cfg_->MAX_THREADS);
+  ThreadException exception;
+#pragma omp parallel for schedule(dynamic)
+  for (int curr_unique_inst_idx = 0; curr_unique_inst_idx < (int) unique.size();
+       curr_unique_inst_idx++) {
+    try {
+      auto& inst = unique[curr_unique_inst_idx];
+      // only do for core and block cells
+      // TODO the above comment says "block cells" but that's not what the code
+      // does?
+      if (!isStdCell(inst)) {
+        continue;
+      }
+
+      int num_valid_pattern = prepPatternInst(inst, curr_unique_inst_idx, 1.0);
+
+      if (num_valid_pattern == 0) {
+        // In FAx1_ASAP7_75t_R (in asap7) the pins are mostly horizontal
+        // and sorting in X works poorly.  So we try again sorting in Y.
+        num_valid_pattern = prepPatternInst(inst, curr_unique_inst_idx, 0.0);
+        if (num_valid_pattern == 0) {
+          logger_->warn(
+              DRT,
+              87,
+              "No valid pattern for unique instance {}, master is {}.",
+              inst->getName(),
+              inst->getMaster()->getName());
+        }
+      }
+#pragma omp critical
+      {
+        cnt++;
+        if (router_cfg_->VERBOSE > 0) {
+          if (cnt % (cnt > 1000 ? 1000 : 100) == 0) {
+            logger_->info(DRT, 79, "  Complete {} unique inst patterns.", cnt);
+          }
+        }
+      }
+    } catch (...) {
+      exception.capture();
+    }
+  }
+  exception.rethrow();
+  if (router_cfg_->VERBOSE > 0) {
+    logger_->info(DRT, 81, "  Complete {} unique inst patterns.", cnt);
+  }
+  if (isDistributed()) {
+    dst::JobMessage msg(dst::JobMessage::PIN_ACCESS,
+                        dst::JobMessage::BROADCAST),
+        result;
+    std::unique_ptr<PinAccessJobDescription> uDesc
+        = std::make_unique<PinAccessJobDescription>();
+    std::string patterns_file = fmt::format("{}/patterns.bin", shared_vol_);
+    serializePatterns(unique_inst_patterns_, patterns_file);
+    uDesc->setPath(patterns_file);
+    uDesc->setType(PinAccessJobDescription::UPDATE_PATTERNS);
+    msg.setJobDescription(std::move(uDesc));
+    const bool ok
+        = dist_->sendJob(msg, remote_host_.c_str(), remote_port_, result);
+    if (!ok) {
+      logger_->error(
+          utl::DRT, 330, "Error sending UPDATE_PATTERNS Job to cloud");
+    }
+  }
+
+  // prep pattern for each row
+  std::vector<frInst*> insts;
+  std::vector<std::vector<frInst*>> inst_rows;
+  std::vector<frInst*> row_insts;
+
+  auto instLocComp = [](frInst* const& a, frInst* const& b) {
+    const Point originA = a->getOrigin();
+    const Point originB = b->getOrigin();
+    if (originA.y() == originB.y()) {
+      return (originA.x() < originB.x());
+    }
+    return (originA.y() < originB.y());
+  };
+
+  getInsts(insts);
+  std::sort(insts.begin(), insts.end(), instLocComp);
+
+  // gen rows of insts
+  int prev_y_coord = INT_MIN;
+  int prev_x_end_coord = INT_MIN;
+  for (auto inst : insts) {
+    Point origin = inst->getOrigin();
+    if (origin.y() != prev_y_coord || origin.x() > prev_x_end_coord) {
+      if (!row_insts.empty()) {
+        inst_rows.push_back(row_insts);
+        row_insts.clear();
+      }
+    }
+    row_insts.push_back(inst);
+    prev_y_coord = origin.y();
+    Rect inst_boundary_box = inst->getBoundaryBBox();
+    prev_x_end_coord = inst_boundary_box.xMax();
+  }
+  if (!row_insts.empty()) {
+    inst_rows.push_back(row_insts);
+  }
+  prepPatternInstRows(std::move(inst_rows));
+}
+
 // the input inst must be unique instance
 int FlexPA::prepPatternInst(frInst* inst,
                             const int curr_unique_inst_idx,
