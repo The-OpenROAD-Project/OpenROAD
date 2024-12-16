deb360cb8fd25bd97107f678c35fa141721ba5bd
João Mai <joaomai2001@gmail.com>
2024-12-16 17:31:17 -0300

    mpl2: guidance region regression tests
    Signed-off-by: João Mai <joaomai2001@gmail.com>

---
+++ src/mpl2/test/guides2.tcl
@@ -0,0 +1,22 @@
+# Test if pin access blockage is generated correctly for a case
+# with pins in a single boundary.
+source "helpers.tcl"
+
+# We're not interested in the connections, so don't include the lib.
+read_lef "./Nangate45/Nangate45.lef"
+
+read_lef "./testcases/macro_only.lef"
+read_liberty "./testcases/macro_only.lib"
+
+read_verilog "./testcases/io_constraints1.v"
+link_design "io_constraints1"
+read_def "./testcases/io_constraints1.def" -floorplan_initialize
+
+set_macro_guidance_region -macro_name MACRO_1 -region {40 20 140 120}
+set_thread_count 0
+rtl_macro_placer -report_directory results/guides2 -halo_width 4.0
+
+set def_file [make_result_file guides2.def]
+write_def $def_file
+
+diff_files guides2.defok $def_file
--- src/mpl2/test/regression_tests.tcl
+++ src/mpl2/test/regression_tests.tcl
@@ -1,6 +1,8 @@
 record_tests {
     macro_only
     no_unfixed_macros
+    guides1
+    guides2
     #mpl2_man_tcl_check
     #mpl2_readme_msgs_check
 }
