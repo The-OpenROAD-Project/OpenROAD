89f7a56843dae0419009045298c664e88e71bd06
Matt Liberty <mliberty@precisioninno.com>
2024-12-31 20:40:26 +0000

    dft: preserve region/group/source_type in ReplaceCell
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/dft/test/one_cell_sky130.tcl
+++ src/dft/test/one_cell_sky130.tcl
@@ -4,8 +4,7 @@ read_lef sky130hd/sky130hd.tlef
 read_lef sky130hd/sky130_fd_sc_hd_merged.lef
 read_liberty sky130hd/sky130_fd_sc_hd__tt_025C_1v80.lib
 
-read_verilog one_cell_sky130.v
-link_design one_cell
+read_def one_cell_sky130.def
 
 create_clock -name main_clock -period 2.0000 -waveform {0.0000 1.0000} [get_ports {clock}]
 
@@ -22,3 +21,7 @@ report_instance ff1
 set verilog_file [make_result_file one_cell_sky130.v]
 write_verilog $verilog_file
 diff_files $verilog_file one_cell_sky130.vok
+
+set def_file [make_result_file one_cell_sky130.def]
+write_def $def_file
+diff_files $def_file one_cell_sky130.defok
