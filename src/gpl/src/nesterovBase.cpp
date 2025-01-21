3cdc9b3892f29a40c94498f6ab9593d57d4f9432
Augusto Berndt <augusto.berndt@precisioninno.com>
2025-01-21 19:29:47 +0000

    gpl: keep divergence check this looks like a mistake
    Signed-off-by: Augusto Berndt <augusto.berndt@precisioninno.com>

--- src/gpl/src/nesterovBase.cpp
+++ src/gpl/src/nesterovBase.cpp
@@ -2661,7 +2661,7 @@ bool NesterovBase::checkDivergence()
 {
   if (sumOverflowUnscaled_ < 0.2f
       && sumOverflowUnscaled_ - minSumOverflow_ >= 0.02f
-      && hpwlWithMinSumOverflow_ * 1.2f < prevHpwl_ && false) {
+      && hpwlWithMinSumOverflow_ * 1.2f < prevHpwl_) {
     divergeMsg_ = "RePlAce divergence detected. ";
     divergeMsg_ += "Re-run with a smaller max_phi_cof value.";
     divergeCode_ = 307;
