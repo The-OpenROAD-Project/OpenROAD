f3ed6054ec39d02256171934ecbf9b0f69640635
João Mai <jmai@precisioninno.com>
2025-02-05 15:23:27 -0300

    mpl: basic multi pattern unit test structure
    Signed-off-by: João Mai <jmai@precisioninno.com>

--- src/mpl/test/cpp/TestSnapper.cpp
+++ src/mpl/test/cpp/TestSnapper.cpp
@@ -160,5 +160,98 @@ TEST_F(TestSnapper, DoubleLayer)
   EXPECT_EQ(macro->getOrigin().y(), 1559);
 }
 
+TEST_F(TestSnapper, MultiPattern)
+{
+  tech_->setManufacturingGrid(1);
+
+  odb::dbTechLayer* h1 = odb::dbTechLayer::create(
+      tech_.get(), "H1", odb::dbTechLayerType::DEFAULT);
+  h1->setDirection(odb::dbTechLayerDir::HORIZONTAL);
+  odb::dbTrackGrid* track1 = odb::dbTrackGrid::create(block_.get(), h1);
+  track1->addGridPatternY(0, 1000 / 3, 3);
+  track1->addGridPatternY(0, 1000 / 5, 5);
+
+  odb::dbMaster* master1 = odb::dbMaster::create(lib_.get(), "master1");
+  master1->setHeight(100);
+  master1->setWidth(100);
+  master1->setType(odb::dbMasterType::BLOCK);
+
+  odb::dbMTerm* mterm1 = odb::dbMTerm::create(
+      master1, "pin1", odb::dbIoType::INPUT, odb::dbSigType::SIGNAL);
+  odb::dbMPin* mpin1 = odb::dbMPin::create(mterm1);
+  odb::dbBox::create(mpin1, h1, 0, 0, 10, 10);
+
+  odb::dbMTerm* mterm2 = odb::dbMTerm::create(
+      master1, "pin2", odb::dbIoType::INPUT, odb::dbSigType::SIGNAL);
+  odb::dbMPin* mpin2 = odb::dbMPin::create(mterm2);
+  odb::dbBox::create(mpin2, h1, 0, 10, 10, 20);
+
+  master1->setFrozen();
+
+  odb::dbInst* inst = odb::dbInst::create(block_.get(), master1, "macro1");
+  inst->setOrigin(10, 119);
+
+  snapper_->setMacro(inst);
+  snapper_->snapMacro();
+
+  EXPECT_EQ(inst->getOrigin().x(), 10);
+  EXPECT_EQ(inst->getOrigin().y(), 115);
+}
+
+TEST_F(TestSnapper, MultiLayerMultiPattern)
+{
+  tech_->setManufacturingGrid(1);
+
+  odb::dbTechLayer* h1 = odb::dbTechLayer::create(
+      tech_.get(), "H1", odb::dbTechLayerType::DEFAULT);
+  h1->setDirection(odb::dbTechLayerDir::HORIZONTAL);
+  odb::dbTrackGrid* track1 = odb::dbTrackGrid::create(block_.get(), h1);
+  track1->addGridPatternY(0, 1000 / 3, 3);
+  track1->addGridPatternY(0, 1000 / 11, 11);
+
+  odb::dbTechLayer* h2 = odb::dbTechLayer::create(
+      tech_.get(), "H2", odb::dbTechLayerType::DEFAULT);
+  h2->setDirection(odb::dbTechLayerDir::HORIZONTAL);
+  odb::dbTrackGrid* track2 = odb::dbTrackGrid::create(block_.get(), h2);
+  track2->addGridPatternY(0, 1000 / 7, 7);
+  track2->addGridPatternY(0, 1000 / 13, 13);
+
+  odb::dbMaster* master1 = odb::dbMaster::create(lib_.get(), "master1");
+  master1->setHeight(100);
+  master1->setWidth(100);
+  master1->setType(odb::dbMasterType::BLOCK);
+
+  odb::dbMTerm* mterm1 = odb::dbMTerm::create(
+      master1, "pin1", odb::dbIoType::INPUT, odb::dbSigType::SIGNAL);
+  odb::dbMPin* mpin1 = odb::dbMPin::create(mterm1);
+  odb::dbBox::create(mpin1, h1, 0, 0, 10, 10);
+
+  odb::dbMTerm* mterm2 = odb::dbMTerm::create(
+      master1, "pin2", odb::dbIoType::INPUT, odb::dbSigType::SIGNAL);
+  odb::dbMPin* mpin2 = odb::dbMPin::create(mterm2);
+  odb::dbBox::create(mpin2, h1, 0, 10, 10, 20);
+
+  odb::dbMTerm* mterm3 = odb::dbMTerm::create(
+      master1, "pin3", odb::dbIoType::INPUT, odb::dbSigType::SIGNAL);
+  odb::dbMPin* mpin3 = odb::dbMPin::create(mterm3);
+  odb::dbBox::create(mpin3, h2, 0, 0, 10, 10);
+
+  odb::dbMTerm* mterm4 = odb::dbMTerm::create(
+      master1, "pin4", odb::dbIoType::INPUT, odb::dbSigType::SIGNAL);
+  odb::dbMPin* mpin4 = odb::dbMPin::create(mterm4);
+  odb::dbBox::create(mpin4, h2, 0, 10, 10, 20);
+
+  master1->setFrozen();
+
+  odb::dbInst* inst = odb::dbInst::create(block_.get(), master1, "macro1");
+  inst->setOrigin(10, 119);
+
+  snapper_->setMacro(inst);
+  snapper_->snapMacro();
+
+  EXPECT_EQ(inst->getOrigin().x(), 10);
+  EXPECT_EQ(inst->getOrigin().y(), 121);
+}
+
 }  // namespace
 }  // namespace mpl
