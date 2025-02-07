5c756ab6f1a821c3f2f20717c2b66b75653d5d3d
Eder Monteiro <emrmonteiro@precisioninno.com>
2025-02-07 11:23:17 -0300

    ppl: add location information when there are not enought slots
    Signed-off-by: Eder Monteiro <emrmonteiro@precisioninno.com>

--- src/ppl/include/ppl/IOPlacer.h
+++ src/ppl/include/ppl/IOPlacer.h
@@ -192,6 +192,7 @@ class IOPlacer
   void randomPlacement();
   void randomPlacement(std::vector<int> pin_indices,
                        std::vector<int> slot_indices,
+                       const Edge edge,
                        bool top_layer,
                        bool is_group);
   int placeFallbackPins(bool random);
