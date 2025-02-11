00cff18f77c57653d821b68c4ac76b38c6533ac3
Peter Gadfort <peter.gadfort@gmail.com>
2025-02-11 08:11:40 -0700

    tap: update tapcell tests for one_site_gaps
    Signed-off-by: Peter Gadfort <peter.gadfort@gmail.com>

--- src/tap/test/disallow_one_site_gaps2.tcl
+++ src/tap/test/disallow_one_site_gaps2.tcl
@@ -1,14 +1,13 @@
 source "helpers.tcl"
 read_lef Nangate45/Nangate45_tech.lef
-read_lef Nangate45/Nangate45_stdcell.lef
+read_lef Nangate45_data/Nangate45_stdcell.lef
 read_def disallow_one_site_gaps.def
 
 set def_file [make_result_file disallow_one_site_gaps2.def]
 
-place_endcaps -endcap_vertical "TAPCELL_X1"
+place_endcaps -endcap_vertical "TAPCELL_X2"
 place_tapcells -distance 20 \
-    -master "TAPCELL_X1" \
-    -disallow_one_site_gaps
+    -master "TAPCELL_X2"
 
 write_def $def_file
 
