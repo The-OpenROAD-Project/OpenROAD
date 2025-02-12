1ffcb66504d911f5f2db3264eb94f9ea94c5104b
Eder Monteiro <emrmonteiro@precisioninno.com>
2025-02-12 15:07:05 -0300

    ppl: clang-format
    Signed-off-by: Eder Monteiro <emrmonteiro@precisioninno.com>

--- src/ppl/src/IOPlacer.cpp
+++ src/ppl/src/IOPlacer.cpp
@@ -1916,8 +1916,8 @@ Edge IOPlacer::getRegionEdge(const odb::Rect& region)
       return Edge::left;
     }
     return Edge::right;
-  } 
-  
+  }
+
   if (region.yMin() == region.yMax()) {
     if (region.yMin() == die_boundary.yMin()) {
       return Edge::bottom;
