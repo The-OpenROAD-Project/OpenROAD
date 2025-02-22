832ada1653a033981734aa6ad1458a7f732994bb
Matt Liberty <mliberty@precisioninno.com>
2025-02-22 04:10:49 +0000

    dbSta: rm use of boost::json namespace
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/dbSta/src/dbSta.cc
+++ src/dbSta/src/dbSta.cc
@@ -77,8 +77,19 @@
 
 ////////////////////////////////////////////////////////////////
 
-namespace {
+namespace ord {
 
+using sta::dbSta;
+
+dbSta* makeDbSta()
+{
+  return new dbSta;
+}
+}  // namespace ord
+
+namespace sta {
+
+namespace {
 // Holds the usage information of a specific cell which includes (i) name of
 // the cell, (ii) number of instances of the cell, and (iii) area of the cell
 // in microns^2.
@@ -96,21 +107,8 @@ struct CellUsageSnapshot
   std::vector<CellUsageInfo> cells_usage_info;
 };
 
-}  // namespace
-
-namespace ord {
-
-using sta::dbSta;
-
-dbSta* makeDbSta()
-{
-  return new dbSta;
-}
-}  // namespace ord
-
-namespace boost::json {
-void tag_invoke(json::value_from_tag,
-                json::value& json_value,
+void tag_invoke(boost::json::value_from_tag,
+                boost::json::value& json_value,
                 CellUsageInfo const& cell_usage_info)
 {
   json_value = {{"name", cell_usage_info.name},
@@ -118,8 +116,8 @@ void tag_invoke(json::value_from_tag,
                 {"area", cell_usage_info.area}};
 }
 
-void tag_invoke(json::value_from_tag,
-                json::value& json_value,
+void tag_invoke(boost::json::value_from_tag,
+                boost::json::value& json_value,
                 CellUsageSnapshot const& cell_usage_snapshot)
 {
   json_value
@@ -128,9 +126,7 @@ void tag_invoke(json::value_from_tag,
           boost::json::value_from(cell_usage_snapshot.cells_usage_info)}};
 }
 
-}  // namespace boost::json
-
-namespace sta {
+}  // namespace
 
 using utl::Logger;
 using utl::STA;
