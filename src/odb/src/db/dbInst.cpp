802373770c6c0a93de299f70409e17556a67d492
Matt Liberty <mliberty@precisioninno.com>
2025-03-24 04:18:15 +0000

    odb: fix bugprone-sizeof-expression by removing useless ZASSERTs
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/odb/src/db/dbInst.cpp
+++ src/odb/src/db/dbInst.cpp
@@ -1207,7 +1207,6 @@ bool dbInst::swapMaster(dbMaster* new_master_)
   if (new_inst_hdr == nullptr) {
     new_inst_hdr = (_dbInstHdr*) dbInstHdr::create((dbBlock*) block,
                                                    (dbMaster*) new_master_);
-    ZASSERT(new_inst_hdr);
   }
 
   new_inst_hdr->_inst_cnt++;
@@ -1276,7 +1275,6 @@ dbInst* dbInst::create(dbBlock* block_,
   if (inst_hdr == nullptr) {
     inst_hdr
         = (_dbInstHdr*) dbInstHdr::create((dbBlock*) block, (dbMaster*) master);
-    ZASSERT(inst_hdr);
   }
 
   if (block->_inst_hash.hasMember(name_)) {
