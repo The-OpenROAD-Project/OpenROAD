9e74f00eae9a6b23adca8465adb494fee420b30e
Matt Liberty <mliberty@precisioninno.com>
2025-03-24 09:08:21 +0000

    odb/rcx: fix readability-delete-null-pointer
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/odb/include/odb/dbMap.hpp
+++ src/odb/include/odb/dbMap.hpp
@@ -66,11 +66,8 @@ inline dbMap<T, D>::dbMap(const dbSet<T>& set)
 template <class T, class D>
 inline dbMap<T, D>::~dbMap()
 {
-  if (_map)
-    delete _map;
-
-  if (_vector)
-    delete _vector;
+  delete _map;
+  delete _vector;
 }
 
 template <class T, class D>
--- src/rcx/src/extFlow_v2.cpp
+++ src/rcx/src/extFlow_v2.cpp
@@ -1388,8 +1388,7 @@ uint extRCModel::readRules_v2(Ath__parser* parser,
   if (ignore)
     delete dummy;
 
-  if (wTable != nullptr)
-    delete wTable;
+  delete wTable;
 
   return cnt;
 }
