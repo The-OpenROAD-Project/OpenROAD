af72f761d9b6fa2784c80277eb43f02259737c6f
arthurjolo <arthurjolo@gmail.com>
2025-03-24 09:53:35 -0300

    change merge blockages to get the polygons bounding box not instead of the rectangles
    Signed-off-by: arthurjolo <arthurjolo@gmail.com>

--- src/cts/src/HTreeBuilder.cpp
+++ src/cts/src/HTreeBuilder.cpp
@@ -1107,6 +1107,10 @@ void HTreeBuilder::legalize()
 			"leng:{:0.3f}, sinks:{}", levelIdx, bufferIdx,
 			parentPoint, branchPoint, leng, sinks.size());
       }
+      if(levelIdx + 1 == 2 && bufferIdx == 1) {
+        logger_->report("bp: {}, parent: {}", branchPoint,
+          parentPoint);
+      }
       // clang-format on
       int scalingFactor = wireSegmentUnit_;
       double x1, y1, x2, y2;
@@ -1131,8 +1135,9 @@ void HTreeBuilder::legalize()
                                                  branch_point_dir);
         // clang-format off
 	debugPrint(logger_, CTS, "legalizer", 1,
-		   "findBestLegalLocation branchPt:{}=>{} parentPt:{} new "
-		   "branchPt is {} blockage", branchPoint, legalBranchPoint,
+		   "findBestLegalLocation branchPt {}_{}:{}=>{} parentPt:{} new "
+		   "branchPt is {} blockage", levelIdx + 1, bufferIdx,
+       branchPoint, legalBranchPoint,
 		   parentPoint,
 		   isInsideBbox(legalBranchPoint.getX(), legalBranchPoint.getY(),
 				x1, y1, x2, y2)? "inside" : "outside");
@@ -1152,9 +1157,10 @@ void HTreeBuilder::legalize()
                                            scalingFactor,
                                            branch_point_dir);
         // clang-format off
-	debugPrint(logger_, CTS, "legalizer", 3,
-		   "adjustBeyondBlockage applied to legal branchPt:{}=>{} "
-		   "parentPt:{} newDist={:0.3f}", branchPoint, newLocation,
+	debugPrint(logger_, CTS, "legalizer", 1,
+		   "adjustBeyondBlockage applied to legal branchPt {}_{} :"
+		   "{}=>{} parentPt:{} newDist={:0.3f}", levelIdx + 1, bufferIdx,
+       branchPoint, newLocation,
 		   parentPoint, computeDist(newLocation, parentPoint));
         // clang-format on
         commitMoveLoc(branchPoint, newLocation);
