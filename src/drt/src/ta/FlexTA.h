d376d62bdcad5de7d7cea039fec334672f1c5c19
osamahammad21 <osama21@aucegypt.edu>
2025-03-30 20:22:10 +0000

    drt: clang-tidy sug
    Signed-off-by: osamahammad21 <osama21@aucegypt.edu>

--- src/drt/src/ta/FlexTA.h
+++ src/drt/src/ta/FlexTA.h
@@ -268,7 +268,9 @@ class FlexTAWorker
                              frCoord& pinCoord);
   void initCosts();
   void sortIroutes();
-  bool outOfDieVia(frLayerNum lNum, const Point& pt, const Rect& dieBox) const;
+  bool outOfDieVia(frLayerNum layer_num,
+                   const Point& pt,
+                   const Rect& die_box) const;
 
   // quick drc
   frSquaredDistance box2boxDistSquare(const Rect& box1,
