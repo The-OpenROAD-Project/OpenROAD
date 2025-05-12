f194fb740a7e27557730bfc46c6d099d9f42ad20
Faholan <62927863+Faholan@users.noreply.github.com>
2025-05-12 15:50:13 -0700

    Revert "Update last wrong places"
    This reverts commit 0a880a895c21e520255b83642a1fc2b2133cf4f7.
    
    Signed-off-by: Faholan <62927863+Faholan@users.noreply.github.com>

--- src/pdn/src/grid_component.cpp
+++ src/pdn/src/grid_component.cpp
@@ -177,10 +177,7 @@ ShapePtr GridComponent::addShape(Shape* shape)
 void GridComponent::removeShape(Shape* shape)
 {
   for (const auto& via : shape->getVias()) {
-    const auto via_strong = via.lock();
-    if (via_strong) {
-      via_strong->removeShape(shape);
-    }
+    via->removeShape(shape);
   }
 
   auto& shapes = shapes_[shape->getLayer()];
