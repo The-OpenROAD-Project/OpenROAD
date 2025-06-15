b5a5691aebf7c3adf91b12e4ecd20e786be34b48
Peter Gadfort <peter.gadfort@gmail.com>
2025-06-15 10:27:13 -0400

    move gzip file handling to utl
    Signed-off-by: Peter Gadfort <peter.gadfort@gmail.com>

--- src/odb/src/defout/defout_impl.cpp
+++ src/odb/src/defout/defout_impl.cpp
@@ -6,9 +6,6 @@
 #include <sys/stat.h>
 
 #include <algorithm>
-#include <boost/algorithm/string/predicate.hpp>
-#include <boost/iostreams/filter/gzip.hpp>
-#include <boost/iostreams/filtering_streambuf.hpp>
 #include <cstdint>
 #include <cstdio>
 #include <limits>
@@ -258,15 +255,6 @@ bool defout_impl::writeBlock(dbBlock* block, std::ostream& stream)
 bool defout_impl::writeBlock(dbBlock* block, const char* def_file)
 {
   utl::StreamHandler stream_handler(def_file, false);
-
-  if (boost::ends_with(std::string(def_file), ".gz")) {
-    boost::iostreams::filtering_streambuf<boost::iostreams::output> outbuf;
-    outbuf.push(boost::iostreams::gzip_compressor());
-    outbuf.push(stream_handler.getStream());
-    std::ostream zstd_compressed(&outbuf);
-
-    return writeBlock(block, zstd_compressed);
-  }
   return writeBlock(block, stream_handler.getStream());
 }
 
