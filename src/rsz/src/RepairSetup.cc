bd2607abcdca948982077c90372787bd1ebac82d
Matthew Guthaus <mrg@ucsc.edu>
2025-06-17 15:13:50 -0700

    Rewrite sizedown.
    Consider multiple fanouts.
    Do DRV checks.
    Look at all fanout arcs.
    Update golden models.
    Refactor some functions into BaseMove
    Add sequential fanout slack and worst fanout input slack.
    
    Signed-off-by: Matthew Guthaus <mrg@ucsc.edu>

--- src/rsz/src/RepairSetup.cc
+++ src/rsz/src/RepairSetup.cc
@@ -162,6 +162,10 @@ bool RepairSetup::repairSetup(const float setup_slack_margin,
     // Always  have sizing
     move_sequence.push_back(resizer_->size_down_move_.get());
     move_sequence.push_back(resizer_->size_up_move_.get());
+    // Disabled by default for now
+    // if (!skip_size_down) {
+    //   move_sequence.push_back(resizer_->size_down_move_.get());
+    // }
     if (!skip_pin_swap) {
       move_sequence.push_back(resizer_->swap_pins_move_.get());
     }
@@ -178,6 +182,7 @@ bool RepairSetup::repairSetup(const float setup_slack_margin,
 
   string repair_moves = "Repair move sequence: ";
   for (auto move : move_sequence) {
+    move->init();
     repair_moves += move->name() + string(" ");
   }
   logger_->info(RSZ, 100, repair_moves);
