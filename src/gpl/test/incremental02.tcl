19160faf4ea97af1f8e11812714be6fd057d4ce7
Augusto Berndt <augusto.berndt@precisioninno.com>
2025-06-30 13:58:53 +0000

    gpl: revert to original test results after fix error check logic
    Signed-off-by: Augusto Berndt <augusto.berndt@precisioninno.com>

--- src/gpl/test/incremental02.tcl
+++ src/gpl/test/incremental02.tcl
@@ -6,7 +6,6 @@ set test_name incremental02
 read_lef ./nangate45.lef
 read_def ./$test_name.def
 
-#global_placement_debug -pause 10 -update 10 -initial
 global_placement -incremental -density 0.3 -pad_left 2 -pad_right 2
 set def_file [make_result_file $test_name.def]
 write_def $def_file
