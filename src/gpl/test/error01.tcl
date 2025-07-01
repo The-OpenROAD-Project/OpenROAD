6fd9b14b680e99ffe6d47d2614313a46ddbd91db
Augusto Berndt <augusto.berndt@precisioninno.com>
2025-07-01 16:20:42 +0000

    gpl: test doesn't fail anymore with new behavior, automatically change density instead of throwing error
    Signed-off-by: Augusto Berndt <augusto.berndt@precisioninno.com>

--- src/gpl/test/error01.tcl
+++ src/gpl/test/error01.tcl
@@ -4,5 +4,4 @@ read_lef ./nangate45.lef
 read_def ./$test_name.def
 
 catch {global_placement -init_density_penalty 0.01 \
-         -skip_initial_place -density 0.001} error
-puts $error
+         -skip_initial_place -density 0.001}
