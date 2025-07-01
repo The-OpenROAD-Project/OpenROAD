a0b28db5c8ecfa697db366c980adb083a88978fe
Augusto Berndt <augusto.berndt@precisioninno.com>
2025-07-01 21:19:23 +0000

    gpl: remove comments
    Signed-off-by: Augusto Berndt <augusto.berndt@precisioninno.com>

--- src/gpl/src/graphics.h
+++ src/gpl/src/graphics.h
@@ -122,7 +122,6 @@ class Graphics : public gui::Renderer, public gui::HeatMapDataSource
   std::vector<std::shared_ptr<PlacerBase>> pbVec_;
   std::vector<std::shared_ptr<NesterovBase>> nbVec_;
   NesterovPlace* np_ = nullptr;
-  // GCell* selected_ = nullptr;
   static constexpr size_t kInvalidIndex = std::numeric_limits<size_t>::max();
   size_t selected_ = kInvalidIndex;
   bool draw_bins_ = false;
