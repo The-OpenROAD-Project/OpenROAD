8454c101d6f2f36434572ff4dee13a8d0a9cb1c3
Matt Liberty <mliberty@precisioninno.com>
2025-07-01 05:45:51 +0000

    drt: tclint/fmt test
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/drt/test/ispd18_sample_distributed.tcl
+++ src/drt/test/ispd18_sample_distributed.tcl
@@ -9,13 +9,13 @@ read_lef testcase/ispd18_sample/ispd18_sample.input.lef
 read_def testcase/ispd18_sample/ispd18_sample.input.def
 read_guides testcase/ispd18_sample/ispd18_sample.input.guide
 detailed_route -output_drc results/ispd18_sample.output.drc.rpt \
-               -output_maze results/ispd18_sample.output.maze.log \
-               -verbose 1 \
-	             -distributed \
-	             -remote_host 127.0.0.1 \
-	             -remote_port 1234 \
-               -cloud_size 2 \
-               -shared_volume results
+  -output_maze results/ispd18_sample.output.maze.log \
+  -verbose 1 \
+  -distributed \
+  -remote_host 127.0.0.1 \
+  -remote_port 1234 \
+  -cloud_size 2 \
+  -shared_volume results
 exec kill $server1
 exec kill $server2
 exec kill $balancer
