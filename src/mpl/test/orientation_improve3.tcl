1022ff5728685123ab7b65dc7be362d42c395aac
Arthur Koucher <arthurkoucher@precisioninno.com>
2025-07-03 13:51:30 -0300

    mpl: fix argument of place_pin call inside new test
    Signed-off-by: Arthur Koucher <arthurkoucher@precisioninno.com>

--- src/mpl/test/orientation_improve3.tcl
+++ src/mpl/test/orientation_improve3.tcl
@@ -6,7 +6,7 @@ read_lef "./testcases/orientation_improve1.lef"
 
 read_def "./testcases/orientation_improve1.def"
 
-place_pin -pin_name io_1 -layer metal3 -force_to_die_boundary -location "150 20"
+place_pin -pin_name io_1 -layer metal3 -force_to_die_boundary -location {150 20}
 
 set_thread_count 0
 rtl_macro_placer -report_directory results/orientation_improve3 -halo_width 0.3
