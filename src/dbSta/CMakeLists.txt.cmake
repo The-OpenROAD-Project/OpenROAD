7ed413e4630c679572f2192c773da71e553b1ee0
Maruf Hossain <mdmarufhossaintasin@gmail.com>
2025-07-15 04:21:20 +0600

    Updated CMakeLists.cmake
    Signed-off-by: Maruf Hossain <mdmarufhossaintasin@gmail.com>

--- src/dbSta/CMakeLists.txt.cmake
+++ src/dbSta/CMakeLists.txt.cmake
@@ -1,13 +1,67 @@
+# Core LEF Checker Library
 add_library(lef_checker
-    LefChecker.cc
+  LefChecker.cc
 )
 
 target_include_directories(lef_checker
-    PUBLIC
-    ${CMAKE_CURRENT_SOURCE_DIR}
+  PUBLIC
+  ${CMAKE_CURRENT_SOURCE_DIR}
+  ${OPENROAD_INCLUDE_DIRS}
 )
 
 target_link_libraries(lef_checker
-    PUBLIC
-    odb
+  PUBLIC
+  odb
 )
+
+# Tcl Interface
+configure_file(
+  LefCheckerTcl.i
+  ${CMAKE_CURRENT_BINARY_DIR}/LefCheckerTcl.i
+  COPYONLY
+)
+
+install(
+  FILES ${CMAKE_CURRENT_BINARY_DIR}/LefCheckerTcl.i
+  DESTINATION ${TCL_INSTALL_DIR}
+)
+
+# Python Bindings (Optional)
+if(SWIG_FOUND AND PYTHON3_FOUND)
+  swig_add_library(lef_checker_python
+    LANGUAGE python
+    SOURCES ${CMAKE_SOURCE_DIR}/src/odb/python/lef_checker.i
+  )
+  
+  target_include_directories(lef_checker_python
+    PRIVATE
+    ${PYTHON3_INCLUDE_DIRS}
+  )
+  
+  target_link_libraries(lef_checker_python
+    lef_checker
+    ${PYTHON3_LIBRARIES}
+  )
+  
+  install(
+    TARGETS lef_checker_python
+    DESTINATION ${PYTHON3_SITE_PACKAGES}
+  )
+endif()
+
+# Testing
+if(BUILD_TESTING)
+  add_executable(test_lef_checker
+    test/LefCheckerTest.cc
+  )
+  
+  target_link_libraries(test_lef_checker
+    lef_checker
+    gtest
+  )
+  
+  add_test(
+    NAME test_lef_checker
+    COMMAND test_lef_checker
+  )
+endif()
