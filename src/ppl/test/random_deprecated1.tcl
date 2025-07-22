ba93acd4bbd5b7c8882ba69da54821ba9310b03e
Eder Monteiro <emrmonteiro@precisioninno.com>
2025-07-22 12:22:12 -0300

    ppl: tclfmt again
    Signed-off-by: Eder Monteiro <emrmonteiro@precisioninno.com>

--- src/ppl/test/random_deprecated1.tcl
+++ src/ppl/test/random_deprecated1.tcl
@@ -4,4 +4,4 @@ read_lef Nangate45/Nangate45.lef
 read_def gcd.def
 
 place_pins -random -hor_layers metal3 -ver_layers metal2 \
-           -corner_avoidance 0 -min_distance 0.12
+    -corner_avoidance 0 -min_distance 0.12
