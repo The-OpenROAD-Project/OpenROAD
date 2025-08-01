24e998ea917cef6d83262dbbc745083c0880cc69
Matt Liberty <mliberty@precisioninno.com>
2025-08-01 04:13:03 +0000

    dbSta: add safety asserts to legacy dbNetwork::staToDb methods
    The ones that assume a particular type of mapping rather than the newer
    ones that take multiple possible mappings.
    
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/dbSta/src/dbNetwork.cc
+++ src/dbSta/src/dbNetwork.cc
@@ -2635,7 +2635,9 @@ void dbNetwork::staToDb(const Instance* instance,
 
 dbNet* dbNetwork::staToDb(const Net* net) const
 {
-  return reinterpret_cast<dbNet*>(const_cast<Net*>(net));
+  dbNet* db_net = reinterpret_cast<dbNet*>(const_cast<Net*>(net));
+  assert(!db_net || db_net->getObjectType() == odb::dbNetObj);
+  return db_net;
 }
 
 dbNet* dbNetwork::flatNet(const Net* net) const
@@ -2732,7 +2734,9 @@ void dbNetwork::staToDb(const Pin* pin,
 
 dbBTerm* dbNetwork::staToDb(const Term* term) const
 {
-  return reinterpret_cast<dbBTerm*>(const_cast<Term*>(term));
+  dbBTerm* bterm = reinterpret_cast<dbBTerm*>(const_cast<Term*>(term));
+  assert(!bterm || bterm->getObjectType() == odb::dbBTermObj);
+  return bterm;
 }
 
 void dbNetwork::staToDb(const Term* term,
@@ -2780,20 +2784,26 @@ void dbNetwork::staToDb(const Cell* cell,
 dbMaster* dbNetwork::staToDb(const Cell* cell) const
 {
   const ConcreteCell* ccell = reinterpret_cast<const ConcreteCell*>(cell);
-  return reinterpret_cast<dbMaster*>(ccell->extCell());
+  auto master = reinterpret_cast<dbMaster*>(ccell->extCell());
+  assert(!master || master->getObjectType() == odb::dbMasterObj);
+  return master;
 }
 
 // called only on db cells.
 dbMaster* dbNetwork::staToDb(const LibertyCell* cell) const
 {
   const ConcreteCell* ccell = cell;
-  return reinterpret_cast<dbMaster*>(ccell->extCell());
+  auto master = reinterpret_cast<dbMaster*>(ccell->extCell());
+  assert(!master || master->getObjectType() == odb::dbMasterObj);
+  return master;
 }
 
 dbMTerm* dbNetwork::staToDb(const Port* port) const
 {
   const ConcretePort* cport = reinterpret_cast<const ConcretePort*>(port);
-  return reinterpret_cast<dbMTerm*>(cport->extPort());
+  auto mterm = reinterpret_cast<dbMTerm*>(cport->extPort());
+  assert(!mterm || mterm->getObjectType() == odb::dbMTermObj);
+  return mterm;
 }
 
 dbBTerm* dbNetwork::isTopPort(const Port* port) const
@@ -2841,7 +2851,9 @@ void dbNetwork::staToDb(const Port* port,
 
 dbMTerm* dbNetwork::staToDb(const LibertyPort* port) const
 {
-  return reinterpret_cast<dbMTerm*>(port->extPort());
+  auto mterm = reinterpret_cast<dbMTerm*>(port->extPort());
+  assert(!mterm || mterm->getObjectType() == odb::dbMTermObj);
+  return mterm;
 }
 
 void dbNetwork::staToDb(PortDirection* dir,
