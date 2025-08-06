fc304ffa7851a309e3bc27489ff0ecebe38f31a1
Arthur Koucher <arthurkoucher@precisioninno.com>
2025-08-06 15:48:14 -0300

    mpl: tcl format
    Signed-off-by: Arthur Koucher <arthurkoucher@precisioninno.com>

--- src/mpl/test/keep_clustering_data.tcl
+++ src/mpl/test/keep_clustering_data.tcl
@@ -10,8 +10,8 @@ read_def testcases/io_constraints6.def
 
 set_thread_count 0
 rtl_macro_placer -keep_clustering_data \
-                 -report_directory results/keep_clustering_data \
-                 -halo_width 4.0
+    -report_directory results/keep_clustering_data \
+    -halo_width 4.0
 
 set def_file [make_result_file "keep_clustering_data.def"]
 write_def $def_file
