31dfcdde3e12e796c21d16b7da75a3a8ac50c5de
Matt Liberty <mliberty@precisioninno.com>
2025-08-11 14:37:52 +0000

    grt: fix Coverity warnings about uninit fields in Segment's ctor
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/grt/src/fastroute/include/DataType.h
+++ src/grt/src/fastroute/include/DataType.h
@@ -49,7 +49,7 @@ enum class EdgeDirection
 struct Segment  // A Segment is a 2-pin connection
 {
   Segment(int16_t x1, int16_t y1, int16_t x2, int16_t y2, int8_t cost)
-      : x1(x1), y1(y1), x2(x2), y2(y2), cost(cost)
+      : x1(x1), y1(y1), x2(x2), y2(y2), cost(cost), xFirst(false), HVH(false)
   {
   }
   const int16_t x1, y1, x2, y2;  // coordinates of two endpoints (x1 <= x2)
