a2357eecb29df535d77ce658dda0ea2468f70bfd
Matt Liberty <mliberty@precisioninno.com>
2025-08-19 16:13:45 +0000

    utl/drt: mv drt's frTime_helper to utl mem_stats.cpp for sharing
    Make these reuseable:
    
      size_t getPeakRSS();
      size_t getCurrentRSS();
    
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

---
+++ src/utl/include/utl/mem_stats.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2019-2025, The OpenROAD Authors
+
+#pragma once
+
+namespace utl {
+
+size_t getPeakRSS();
+size_t getCurrentRSS();
+
+}  // namespace utl
