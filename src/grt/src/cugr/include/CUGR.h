7f5c9796a778fd1e3f3ccff2400954218800c9e5
Eder Monteiro <emrmonteiro@precisioninno.com>
2025-08-21 16:43:41 -0300

    grt: rename main file of cugr
    Signed-off-by: Eder Monteiro <emrmonteiro@precisioninno.com>

---
+++ src/grt/src/cugr/include/CUGR.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include "enum.h"
+#include "geo.h"
+
+// STL libraries
+#include <csignal>
+#include <iostream>
+#include <string>
+#include <vector>
+// #include <unordered_map>
+// #include <unordered_set>
+#include <bitset>
+#include <fstream>
+#include <mutex>
+#include <set>
+#include <sstream>
+#include <thread>
+#include <tuple>
+
+// Boost libraries
+#include <boost/foreach.hpp>
+#include <boost/functional/hash.hpp>
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/box.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/index/rtree.hpp>
+#include <boost/icl/split_interval_map.hpp>
+
+#include "odb/geom.h"
+
+namespace odb {
+class dbDatabase;
+}  // namespace odb
+
+namespace stt {
+class SteinerTreeBuilder;
+}  // namespace stt
+
+namespace utl {
+class Logger;
+}  // namespace utl
+
+namespace grt {
+
+class Design;
+class GridGraph;
+class GRNet;
+
+namespace bg = boost::geometry;
+namespace bgi = boost::geometry::index;
+
+using boostPoint = bg::model::point<int, 2, bg::cs::cartesian>;
+using boostBox = bg::model::box<boostPoint>;
+using RTree = bgi::rtree<std::pair<boostBox, int>, bgi::rstar<32>>;
+
+class CUGR
+{
+ public:
+  CUGR(odb::dbDatabase* db,
+        utl::Logger* log,
+        stt::SteinerTreeBuilder* stt_builder);
+  void route();
+  void write(std::string guide_file = "");
+
+ private:
+  void sortNetIndices(std::vector<int>& netIndices) const;
+  void getGuides(const GRNet* net,
+                 std::vector<std::pair<int, grt::BoxT<int>>>& guides);
+  void printStatistics() const;
+
+  Design* design_;
+  GridGraph* gridGraph;
+  std::vector<GRNet*> nets;
+
+  odb::dbDatabase* db_;
+  stt::SteinerTreeBuilder* stt_builder_;
+  utl::Logger* logger_;
+
+
+  int areaOfPinPatches;
+  int areaOfWirePatches;
+
+  // constants
+  const double weight_wire_length = 0.5;
+  const double weight_via_number = 4.0;
+  const double weight_short_area = 500.0;
+
+  const int min_routing_layer = 1;
+  const double cost_logistic_slope = 1.0;
+  const double max_detour_ratio
+      = 0.25;  // allowed stem length increase to trunk length ratio
+  const int target_detour_count = 20;
+  const double via_multiplier = 2.0;
+
+  const double maze_logistic_slope = 0.5;
+
+  const double pin_patch_threshold = 20.0;
+  const int pin_patch_padding = 1;
+  const double wire_patch_threshold = 2.0;
+  const double wire_patch_inflation_rate = 1.2;
+
+  const bool write_heatmap = false;
+};
+
+}  // namespace grt
