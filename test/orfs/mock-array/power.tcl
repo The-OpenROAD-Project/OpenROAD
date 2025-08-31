e59d9934ddc7ace5d3d6c4f74a78c7e5404fb564
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-08-31 13:18:05 +0200

    power: test power per instance reporting
    The power per module(instance in OpenSTA speak) based
    on .vcd is manual for now pending fixes in -hier
    support:
    
        bazelisk test //test/orfs/mock-array:MockArray_power_instances_test
    
    Once this test case is working with -hier, remove tags=["manual"]
    flag.
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- test/orfs/mock-array/power.tcl
+++ test/orfs/mock-array/power.tcl
@@ -1,25 +1,4 @@
-source $::env(SCRIPTS_DIR)/util.tcl
-
-foreach libFile $::env(LIB_FILES) {
-  if { [lsearch -exact $::env(ADDITIONAL_LIBS) $libFile] == -1 } {
-    read_liberty $libFile
-  }
-}
-
-log_cmd read_verilog $::env(RESULTS_DIR)/$::env(POWER_STAGE_STEM).v
-log_cmd read_verilog $::env(RESULTS_DIR)/../../Element/base/$::env(POWER_STAGE_STEM).v
-log_cmd read_verilog $::env(PLATFORM_DIR)/verilog/stdcell/empty.v
-log_cmd link_design MockArray
-
-log_cmd read_sdc $::env(RESULTS_DIR)/$::env(POWER_STAGE_STEM).sdc
-log_cmd read_spef $::env(RESULTS_DIR)/$::env(POWER_STAGE_STEM).spef
-puts "read_spef for ces_*_* macros"
-for { set x 0 } { $x < 8 } { incr x } {
-  for { set y 0 } { $y < 8 } { incr y } {
-    log_cmd read_spef -path ces_${x}_${y} \
-      $::env(RESULTS_DIR)/../../Element/base/$::env(POWER_STAGE_STEM).spef
-  }
-}
+source $::env(LOAD_POWER_TCL)
 
 # OpenSTA reports reg2reg paths inside macros,
 # whereas these paths are hidden to OpenROAD that
@@ -27,9 +6,6 @@ for { set x 0 } { $x < 8 } { incr x } {
 log_cmd report_checks
 
 log_cmd report_power
-
-set vcd_file $::env(VCD_STIMULI)
-log_cmd read_vcd -scope TOP/MockArray $vcd_file
 
 set fp [open $::env(RESULTS_DIR)/activity.tcl w]
 set pins [get_pins -hierarchical *]
