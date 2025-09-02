d5ac7efb03a5ca2837db7dfa52ac2e739d0610db
Vitor Bandeira <vvbandeira@precisioninno.com>
2025-09-02 20:40:17 +0000

    ci: dockerfile refactor
    Signed-off-by: Vitor Bandeira <vvbandeira@precisioninno.com>

--- etc/DockerTag.sh
+++ etc/DockerTag.sh
@@ -4,12 +4,8 @@ cd $(dirname $(realpath $0))/../
 
 if [[ "$@" == "-dev" ]]; then
     file_list=(
-        "./docker/Dockerfile.binary"
-        "./docker/Dockerfile.builder"
-        "./docker/Dockerfile.dev"
-        "./etc/Build.sh"
+        "./Dockerfile"
         "./etc/DependencyInstaller.sh"
-        "./etc/DockerHelper.sh"
         "./etc/DockerTag.sh"
     )
     cat "${file_list[@]}" | sha256sum | awk '{print substr($1, 1, 6)}'
