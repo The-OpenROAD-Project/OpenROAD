f97b61bb3972a143a5a85d87fc280c61284bf389
Vitor Bandeira <vvbandeira@precisioninno.com>
2025-09-03 14:12:52 +0000

    etc: DockerTag.sh usage
    Signed-off-by: Vitor Bandeira <vvbandeira@precisioninno.com>

--- etc/DockerTag.sh
+++ etc/DockerTag.sh
@@ -19,6 +19,12 @@ elif [[ "$@" == "-master" ]]; then
     git fetch --tags >&2
     git describe
 else
-    echo "Usage: $0 {-dev|-bazel|-master}"
+    echo "Usage:"
+    echo " To generate a tag for images that only have dev dependencies:"
+    echo "       $0 -dev"
+    echo " To generate a tag for images that use bazel (mainly for CI):"
+    echo "       $0 -bazel"
+    echo " To generate a tag for full releases based on master branch"
+    echo "       $0 -master"
     exit 1
 fi
