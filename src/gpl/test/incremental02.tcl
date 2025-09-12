4c74414f2f6946069aa2b73dbd912bd6b31681b2
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-09-12 11:40:39 +0200

    test: diff_file elsewhere takes OK as first parameter
    Small nit I saw when I examined the test.
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- src/gpl/test/incremental02.tcl
+++ src/gpl/test/incremental02.tcl
@@ -9,4 +9,4 @@ read_def ./$test_name.def
 global_placement -incremental -density 0.3 -pad_left 2 -pad_right 2
 set def_file [make_result_file $test_name.def]
 write_def $def_file
-diff_file $def_file $test_name.defok
+diff_file $test_name.defok $def_file
