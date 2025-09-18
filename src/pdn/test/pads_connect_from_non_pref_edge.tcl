27629600f062f099470c75e96e32e4c83a438136
Peter Gadfort <peter.gadfort@gmail.com>
2025-09-18 13:08:17 -0400

    pdn: fix indent formatting in tcl
    Signed-off-by: Peter Gadfort <peter.gadfort@gmail.com>

--- src/pdn/test/pads_connect_from_non_pref_edge.tcl
+++ src/pdn/test/pads_connect_from_non_pref_edge.tcl
@@ -12,43 +12,43 @@ set_voltage_domain -name {CORE} -power {VDD} -ground {VSS}
 
 # stdcell grid
 define_pdn_grid \
-    -name stdcell_grid \
-    -starts_with POWER \
-    -voltage_domains {CORE}
+  -name stdcell_grid \
+  -starts_with POWER \
+  -voltage_domains {CORE}
 
 add_pdn_ring \
-    -grid stdcell_grid \
-    -layers "Metal4 Metal3" \
-    -widths "15 15" \
-    -spacings "1.7 1.7" \
-    -core_offsets "6 6" \
-    -connect_to_pads
+  -grid stdcell_grid \
+  -layers "Metal4 Metal3" \
+  -widths "15 15" \
+  -spacings "1.7 1.7" \
+  -core_offsets "6 6" \
+  -connect_to_pads
 
 add_pdn_stripe \
-    -grid stdcell_grid \
-    -layer Metal1 \
-    -width 0.6 \
-    -followpins
+  -grid stdcell_grid \
+  -layer Metal1 \
+  -width 0.6 \
+  -followpins
 
 add_pdn_stripe \
-    -grid stdcell_grid \
-    -layer Metal4 \
-    -width 1.6 \
-    -pitch 153.6 \
-    -offset 16.32 \
-    -spacing 1.7 \
-    -starts_with POWER \
-    -extend_to_core_ring
+  -grid stdcell_grid \
+  -layer Metal4 \
+  -width 1.6 \
+  -pitch 153.6 \
+  -offset 16.32 \
+  -spacing 1.7 \
+  -starts_with POWER \
+  -extend_to_core_ring
 
 add_pdn_stripe \
-    -grid stdcell_grid \
-    -layer Metal5 \
-    -width 1.6 \
-    -pitch 153.18 \
-    -offset 16.65 \
-    -spacing 1.7 \
-    -starts_with POWER \
-    -extend_to_core_ring
+  -grid stdcell_grid \
+  -layer Metal5 \
+  -width 1.6 \
+  -pitch 153.18 \
+  -offset 16.65 \
+  -spacing 1.7 \
+  -starts_with POWER \
+  -extend_to_core_ring
 
 add_pdn_connect -grid stdcell_grid -layers "Metal1 Metal4"
 add_pdn_connect -grid stdcell_grid -layers "Metal2 Metal3"
