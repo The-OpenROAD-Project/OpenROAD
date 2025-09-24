0a902d13636b55a74820761b98862d88f26db9fd
Eder Monteiro <emrmonteiro@precisioninno.com>
2025-09-24 14:43:41 -0300

    est: add function to compute max index from SteinerTree
    Signed-off-by: Eder Monteiro <emrmonteiro@precisioninno.com>

--- src/est/include/est/SteinerTree.h
+++ src/est/include/est/SteinerTree.h
@@ -115,6 +115,7 @@ class SteinerTree
   stt::Tree& fluteTree() { return tree_; }
   void createSteinerPtToPinMap();
   void locAddPin(const Point& loc, const Pin* pin);
+  int getMaxIndex() const;
 
   static constexpr SteinerPt null_pt = -1;
 
