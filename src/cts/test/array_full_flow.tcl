e1d811e088c40ad7f10c24fecd78c223b8f17b75
arthurjolo <arthurjl@precisioninno.com>
2025-09-26 16:22:19 -0300

    cts: tcl format
    Signed-off-by: arthurjolo <arthurjl@precisioninno.com>

--- src/cts/test/array_full_flow.tcl
+++ src/cts/test/array_full_flow.tcl
@@ -136,7 +136,7 @@ set_cts_config -root_buf $cts_buffer \
   -buf_list $cts_buffer \
   -sink_clustering_max_diameter $cts_cluster_diameter
 
-clock_tree_synthesis -sink_clustering_enable \
+clock_tree_synthesis -sink_clustering_enable
 
 set_propagated_clock [all_clocks]
 estimate_parasitics -placement
