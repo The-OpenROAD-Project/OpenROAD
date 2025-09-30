294e863d02a3e297e2572f2c29439a427b3535bd
Rafael Moresco <rafaelmorescovieira@gmail.com>
2025-09-30 13:36:10 -0300

    misc: remove unecessary text from test
    Signed-off-by: Rafael Moresco <rafaelmorescovieira@gmail.com>

--- test/open_db_invalid.tcl
+++ test/open_db_invalid.tcl
@@ -2,11 +2,6 @@
 # This test verifies proper error handling when an invalid database file is specified
 # Expected behavior: OpenROAD should exit with error message and not reach this script
 
-source "helpers.tcl"
-
 puts "TEST: Testing -db flag with invalid database file"
 
-# This test should be run with: openroad -db /nonexistent/file.odb -exit open_db_invalid.tcl
-# The -db flag should be processed before this script runs
-# Expected behavior: OpenROAD should exit with error before reaching this script
 
