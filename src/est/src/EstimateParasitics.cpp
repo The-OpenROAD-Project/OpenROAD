3d4a2ffc6b0e0f0baa5f390c35da038691d1e1ac
Eder Monteiro <emrmonteiro@precisioninno.com>
2025-09-30 15:13:49 -0300

    est: add small resistor between pin and tree node when they are in the same layer
    Signed-off-by: Eder Monteiro <emrmonteiro@precisioninno.com>

--- src/est/src/EstimateParasitics.cpp
+++ src/est/src/EstimateParasitics.cpp
@@ -938,17 +938,15 @@ void EstimateParasitics::parasiticNodeConnectPins(
         if (tree_layer != nullptr && !layer_res_.empty()) {
           odb::dbTechLayer* pin_layer = getPinLayer(pin);
 
-          if (pin_layer != tree_layer) {
-            insertViaResistances(pin_layer,
-                                 tree_layer,
-                                 parasitic,
-                                 pin_node,
-                                 node,
-                                 resistor_id,
-                                 corner,
-                                 net,
-                                 max_node_index);
-          }
+          insertViaResistances(pin_layer,
+                                tree_layer,
+                                parasitic,
+                                pin_node,
+                                node,
+                                resistor_id,
+                                corner,
+                                net,
+                                max_node_index);
         } else {
           double cut_res
               = std::max(computeAverageCutResistance(corner), 1.0e-3);
