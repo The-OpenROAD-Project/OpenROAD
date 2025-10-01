e1d5f87c7da1e9ff29790a05ea90fa695ebad12c
Matt Liberty <mliberty@precisioninno.com>
2025-10-01 18:49:56 +0000

    ram: tclfmt
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/ram/src/ram.tcl
+++ src/ram/src/ram.tcl
@@ -7,46 +7,44 @@ sta::define_cmd_args "generate_ram_netlist" {-bytes_per_word bits
                                              [-tristate_cell name]
                                              [-inv_cell name]
                                              [-read_ports count]}
-  
-proc generate_ram_netlist { args } {
-  sta::parse_key_args "generate_ram_netlist" args \
-      keys {-bytes_per_word -word_count -storage_cell -tristate_cell -inv_cell
+
+proc generate_ram_netlist {args} {
+    sta::parse_key_args "generate_ram_netlist" args \
+        keys {-bytes_per_word -word_count -storage_cell -tristate_cell -inv_cell
       -read_ports } flags {}
 
-  if { [info exists keys(-bytes_per_word)] } {
-    set bytes_per_word $keys(-bytes_per_word)
-  } else {
-    utl::error RAM 1 "The -bytes_per_word argument must be specified."
-  }
+    if {[info exists keys(-bytes_per_word)]} {
+        set bytes_per_word $keys(-bytes_per_word)
+    } else {
+        utl::error RAM 1 "The -bytes_per_word argument must be specified."
+    }
 
-  if { [info exists keys(-word_count)] } {
-    set word_count $keys(-word_count)
-  } else {
-    utl::error RAM 2 "The -word_count argument must be specified."
-  }
+    if {[info exists keys(-word_count)]} {
+        set word_count $keys(-word_count)
+    } else {
+        utl::error RAM 2 "The -word_count argument must be specified."
+    }
 
-  set storage_cell ""
-  if { [info exists keys(-storage_cell)] } {
-    set storage_cell $keys(-storage_cell)
-  }
+    set storage_cell ""
+    if {[info exists keys(-storage_cell)]} {
+        set storage_cell $keys(-storage_cell)
+    }
 
-  set tristate_cell ""
-  if { [info exists keys(-tristate_cell)] } {
-    set tristate_cell $keys(-tristate_cell)
-  }
+    set tristate_cell ""
+    if {[info exists keys(-tristate_cell)]} {
+        set tristate_cell $keys(-tristate_cell)
+    }
 
-  set inv_cell ""
-  if { [info exists keys(-inv_cell)] } {
-    set inv_cell $keys(-inv_cell)
-  }
+    set inv_cell ""
+    if {[info exists keys(-inv_cell)]} {
+        set inv_cell $keys(-inv_cell)
+    }
 
-  set read_ports 1
-  if { [info exists keys(-read_ports)] } {
-    set read_ports $keys(-read_ports)
-  }
-  
-  ram::generate_ram_netlist_cmd $bytes_per_word $word_count $storage_cell \
-      $tristate_cell $inv_cell $read_ports
+    set read_ports 1
+    if {[info exists keys(-read_ports)]} {
+        set read_ports $keys(-read_ports)
+    }
+
+    ram::generate_ram_netlist_cmd $bytes_per_word $word_count $storage_cell \
+        $tristate_cell $inv_cell $read_ports
 }
-
-
