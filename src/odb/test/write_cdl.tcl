9677807244539f1a8543f447c03c934a77a2bc29
Christian COSTA <christian.costa@st.com>
2025-10-09 17:39:08 +0200

    odb: Test again reference cdl file.
    Signed-off-by: Christian COSTA <christian.costa@st.com>

--- src/odb/test/write_cdl.tcl
+++ src/odb/test/write_cdl.tcl
@@ -1,3 +1,5 @@
+source "helpers.tcl"
+
 read_lef write_cdl.lef
 read_liberty write_cdl.lib
 
@@ -5,9 +7,12 @@ read_verilog write_cdl.v
 
 link_design top
 
-write_cdl -masters {write_cdl.cdl} results/write_cdl_out.cdl
+set cdl_file [make_result_file write_cdl_out.cdl]
+write_cdl -masters {write_cdl.cdl} $cdl_file
 
-write_cdl -masters {write_cdl_escaped.cdl} results/write_cdl_escaped_out.cdl
+set cdl_escaped_file [make_result_file write_cdl_escaped_out.cdl]
+write_cdl -masters {write_cdl_escaped.cdl} $cdl_escaped_file
 
-puts "pass"
-exit 0
+diff_files write_cdl.cdlok $cdl_file
+
+diff_files write_cdl.cdlok $cdl_escaped_file
