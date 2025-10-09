60e719789e5ea34067bb23188e90f1e715f45189
Jonas Gava <jfgava@precisioninno.com>
2025-10-09 22:12:42 +0000

    cts: disable double NDR width
    Signed-off-by: Jonas Gava <jfgava@precisioninno.com>

--- src/cts/src/TritonCTS.cpp
+++ src/cts/src/TritonCTS.cpp
@@ -1770,7 +1770,7 @@ void TritonCTS::writeClockNDRsToDb(TreeBuilder* builder)
     }
 
     // Set NDR settings
-    int ndr_width = 2*defaultWidth;
+    int ndr_width = defaultWidth;
     layerRule->setWidth(ndr_width);
     int ndr_space = 2 * getNetSpacing(layer, ndr_width, ndr_width);
     layerRule->setSpacing(ndr_space);
