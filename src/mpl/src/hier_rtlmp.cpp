b4b4029c58278cd6ec97f12455d8e9be43039582
Arthur Koucher <arthurkoucher@precisioninno.com>
2025-10-09 09:43:31 -0300

    mpl: increase even more number of perturbations to ensure      mock-array convergence
    Signed-off-by: Arthur Koucher <arthurkoucher@precisioninno.com>

--- src/mpl/src/hier_rtlmp.cpp
+++ src/mpl/src/hier_rtlmp.cpp
@@ -2102,7 +2102,7 @@ void HierRTLMP::placeMacros(Cluster* cluster)
 
     // Large arrays need more steps to properly converge.
     if (large_macro_cluster) {
-      perturbations_per_step *= 4;
+      perturbations_per_step *= num_perturb_per_step_;
     }
   }
 
