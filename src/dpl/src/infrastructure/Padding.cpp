ad69c6f9bad164f595a054a19f6fa427ea9abf74
Henner Zeller <h.zeller@acm.org>
2025-10-14 14:15:30 -0700

    Add a function-local `using odb::dbMasterType` for readability.
    Signed-off-by: Henner Zeller <h.zeller@acm.org>

--- src/dpl/src/infrastructure/Padding.cpp
+++ src/dpl/src/infrastructure/Padding.cpp
@@ -37,50 +37,52 @@ bool Padding::isPaddedType(odb::dbInst* inst) const
   if (inst == nullptr) {
     return false;
   }
-  odb::dbMasterType type = inst->getMaster()->getType();
+
+  using odb::dbMasterType;
+  dbMasterType type = inst->getMaster()->getType();
   // Use switch so if new types are added we get a compiler warning.
   switch (type.getValue()) {
-    case odb::dbMasterType::CORE:
-    case odb::dbMasterType::CORE_ANTENNACELL:
-    case odb::dbMasterType::CORE_FEEDTHRU:
-    case odb::dbMasterType::CORE_TIEHIGH:
-    case odb::dbMasterType::CORE_TIELOW:
-    case odb::dbMasterType::CORE_WELLTAP:
+    case dbMasterType::CORE:
+    case dbMasterType::CORE_ANTENNACELL:
+    case dbMasterType::CORE_FEEDTHRU:
+    case dbMasterType::CORE_TIEHIGH:
+    case dbMasterType::CORE_TIELOW:
+    case dbMasterType::CORE_WELLTAP:
       return true;
-    case odb::dbMasterType::ENDCAP:
-    case odb::dbMasterType::ENDCAP_PRE:
-    case odb::dbMasterType::ENDCAP_POST:
-    case odb::dbMasterType::ENDCAP_LEF58_RIGHTEDGE:
-    case odb::dbMasterType::ENDCAP_LEF58_LEFTEDGE:
-    case odb::dbMasterType::CORE_SPACER:
-    case odb::dbMasterType::BLOCK:
-    case odb::dbMasterType::BLOCK_BLACKBOX:
-    case odb::dbMasterType::BLOCK_SOFT:
-    case odb::dbMasterType::ENDCAP_TOPLEFT:
-    case odb::dbMasterType::ENDCAP_TOPRIGHT:
-    case odb::dbMasterType::ENDCAP_BOTTOMLEFT:
-    case odb::dbMasterType::ENDCAP_BOTTOMRIGHT:
-    case odb::dbMasterType::ENDCAP_LEF58_BOTTOMEDGE:
-    case odb::dbMasterType::ENDCAP_LEF58_TOPEDGE:
-    case odb::dbMasterType::ENDCAP_LEF58_RIGHTBOTTOMEDGE:
-    case odb::dbMasterType::ENDCAP_LEF58_LEFTBOTTOMEDGE:
-    case odb::dbMasterType::ENDCAP_LEF58_RIGHTTOPEDGE:
-    case odb::dbMasterType::ENDCAP_LEF58_LEFTTOPEDGE:
-    case odb::dbMasterType::ENDCAP_LEF58_RIGHTBOTTOMCORNER:
-    case odb::dbMasterType::ENDCAP_LEF58_LEFTBOTTOMCORNER:
-    case odb::dbMasterType::ENDCAP_LEF58_RIGHTTOPCORNER:
-    case odb::dbMasterType::ENDCAP_LEF58_LEFTTOPCORNER:
+    case dbMasterType::ENDCAP:
+    case dbMasterType::ENDCAP_PRE:
+    case dbMasterType::ENDCAP_POST:
+    case dbMasterType::ENDCAP_LEF58_RIGHTEDGE:
+    case dbMasterType::ENDCAP_LEF58_LEFTEDGE:
+    case dbMasterType::CORE_SPACER:
+    case dbMasterType::BLOCK:
+    case dbMasterType::BLOCK_BLACKBOX:
+    case dbMasterType::BLOCK_SOFT:
+    case dbMasterType::ENDCAP_TOPLEFT:
+    case dbMasterType::ENDCAP_TOPRIGHT:
+    case dbMasterType::ENDCAP_BOTTOMLEFT:
+    case dbMasterType::ENDCAP_BOTTOMRIGHT:
+    case dbMasterType::ENDCAP_LEF58_BOTTOMEDGE:
+    case dbMasterType::ENDCAP_LEF58_TOPEDGE:
+    case dbMasterType::ENDCAP_LEF58_RIGHTBOTTOMEDGE:
+    case dbMasterType::ENDCAP_LEF58_LEFTBOTTOMEDGE:
+    case dbMasterType::ENDCAP_LEF58_RIGHTTOPEDGE:
+    case dbMasterType::ENDCAP_LEF58_LEFTTOPEDGE:
+    case dbMasterType::ENDCAP_LEF58_RIGHTBOTTOMCORNER:
+    case dbMasterType::ENDCAP_LEF58_LEFTBOTTOMCORNER:
+    case dbMasterType::ENDCAP_LEF58_RIGHTTOPCORNER:
+    case dbMasterType::ENDCAP_LEF58_LEFTTOPCORNER:
       // These classes are completely ignored by the placer.
-    case odb::dbMasterType::COVER:
-    case odb::dbMasterType::COVER_BUMP:
-    case odb::dbMasterType::RING:
-    case odb::dbMasterType::PAD:
-    case odb::dbMasterType::PAD_AREAIO:
-    case odb::dbMasterType::PAD_INPUT:
-    case odb::dbMasterType::PAD_OUTPUT:
-    case odb::dbMasterType::PAD_INOUT:
-    case odb::dbMasterType::PAD_POWER:
-    case odb::dbMasterType::PAD_SPACER:
+    case dbMasterType::COVER:
+    case dbMasterType::COVER_BUMP:
+    case dbMasterType::RING:
+    case dbMasterType::PAD:
+    case dbMasterType::PAD_AREAIO:
+    case dbMasterType::PAD_INPUT:
+    case dbMasterType::PAD_OUTPUT:
+    case dbMasterType::PAD_INOUT:
+    case dbMasterType::PAD_POWER:
+    case dbMasterType::PAD_SPACER:
       return false;
   }
   // gcc warniing
