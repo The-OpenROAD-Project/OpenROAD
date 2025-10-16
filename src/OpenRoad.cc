6d1b6d694b0487faa86a702070a283fbab85b0c4
tedhong <tedhong@google.com>
2025-10-16 23:15:45 +0000

    gpl: Factor out graphics use in gpl
    Signed-off-by: Ted Hong <tedhong@google.com>

--- src/OpenRoad.cc
+++ src/OpenRoad.cc
@@ -3,6 +3,8 @@
 
 #include "ord/OpenRoad.hh"
 
+#include <tcl.h>
+
 #include <cstdlib>
 #include <cstring>
 #include <filesystem>
@@ -267,6 +269,7 @@ void OpenRoad::init(Tcl_Interp* tcl_interp,
   rsz::initResizer(tcl_interp);
   ppl::initIoplacer(tcl_interp);
   gpl::initReplace(tcl_interp);
+  gpl::initReplaceGraphics(replace_, logger_);
   dpl::initOpendp(tcl_interp);
   fin::initFinale(tcl_interp);
   ram::initRamGen(tcl_interp);
