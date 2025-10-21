cc7d500346c8607b2300d748b92e318587b3e8fc
Vitor Bandeira <vvbandeira@precisioninno.com>
2025-10-21 19:10:11 +0000

    etc: add bazel option for DockerTag
    Signed-off-by: Vitor Bandeira <vvbandeira@precisioninno.com>

--- etc/DockerTag.sh
+++ etc/DockerTag.sh
@@ -13,6 +13,11 @@ if [[ "$@" == "-dev" ]]; then
         "./etc/DockerTag.sh"
     )
     cat "${file_list[@]}" | sha256sum | awk '{print substr($1, 1, 6)}'
+elif [[ "$@" == "-bazel" ]]; then
+    file_list=(
+        "./bazel/Dockerfile"
+    )
+    cat "${file_list[@]}" | sha256sum | awk '{print substr($1, 1, 6)}'
 elif [[ "$@" == "-master" ]]; then
     git describe
 else
