487646b308e1eaeef16cd8ee74744a04a6c2a303
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-10-25 09:47:29 +0200

    mock-array: more OpenSTA testing
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- test/orfs/mock-array/power.tcl
+++ test/orfs/mock-array/power.tcl
@@ -104,6 +104,22 @@ if { abs($total_power_vcd - $total_power_user_activity) > 1e-3 } {
 log_cmd report_parasitic_annotation
 log_cmd report_activity_annotation -report_unannotated
 
+# some drive-by tests for OpenSTA path groups
+set first_path [lindex [find_timing_paths] 0]
+set from [get_property [get_property $first_path startpoint] full_name]
+set to [get_property [get_property $first_path endpoint] full_name]
+puts "Checking if we can find timing paths from $from to $to"
+if { [llength [find_timing_paths -from $from -to $to]] == 0 } {
+  puts "Error: find_timing_paths -from $from -to $to failed"
+  exit 1
+}
+puts "Checking if we can create a group_path from $from to $to"
+group_path -name test_group -from $from -to $to
+if { [llength [find_timing_paths -path_group test_group]] == 0 } {
+  puts "Error: find_timing_paths -path_group test_group failed"
+  exit 1
+}
+
 set f [open $::env(OUTPUT) w]
 puts $f OK
 close $f
