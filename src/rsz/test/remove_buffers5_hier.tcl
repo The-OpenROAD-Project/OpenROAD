a6744212735bda09f2f85121768f5d9dd47d06ae
Jaehyun Kim <jhkim@precisioninno.com>
2025-11-07 19:43:15 +0900

    Added new remove_buffers regressions
    Signed-off-by: Jaehyun Kim <jhkim@precisioninno.com>

--- src/rsz/test/remove_buffers5_hier.tcl
+++ src/rsz/test/remove_buffers5_hier.tcl
@@ -1,42 +1,53 @@
-# remove_buffers in1 -> b1 -> b2 -> b3 -> out1
+# remove_buffers test w/ asap7/riscv32i design in hierarchical flow
 source "helpers.tcl"
 
-set test_name remove_buffers5
+set test_name remove_buffers5_hier
 
 read_liberty asap7/asap7sc7p5t_INVBUF_RVT_FF_nldm_220122.lib.gz
 read_liberty asap7/asap7sc7p5t_SIMPLE_RVT_FF_nldm_211120.lib.gz
 read_liberty asap7/asap7sc7p5t_AO_RVT_FF_nldm_211120.lib.gz
 read_liberty asap7/asap7sc7p5t_SEQ_RVT_FF_nldm_220123.lib
 read_liberty asap7/asap7sc7p5t_OA_RVT_FF_nldm_211120.lib.gz
+read_liberty asap7/fakeram7_256x32.lib
 read_lef asap7/asap7_tech_1x_201209.lef
 read_lef asap7/asap7sc7p5t_28_R_1x_220121a.lef
 
 read_verilog asap7_riscv32i.v
-link_design riscv_top -hier
+link_design riscv_top
 
-create_clock [get_ports clk] -name core_clock -period 3000
+set clk_name clk
+set clk_port_name clk
+set clk_period 3000
+set clk_port [get_ports $clk_port_name]
+set non_clock_inputs [lsearch -inline -all -not -exact [all_inputs] $clk_port]
 
-#source Nangate45/Nangate45.rc
-#set_wire_rc -layer metal3
+create_clock -name $clk_name -period $clk_period $clk_port
+set_input_delay  0 -clock $clk_name $non_clock_inputs
+set_output_delay 0 -clock $clk_name [all_outputs]
 
 # make sure sta works before/after removal
-set sp {riscv.dp.pcreg.q[31]$_DFF_PP0_/CLK}
-set ep {riscv.dp.rf.rf[13][17]$_DFFE_PP_/D}
+set report_checks_cmd [list report_checks \
+  -from {riscv.dp.pcreg.q[31]$_DFF_PP0_/CLK} \
+  -through _10996_/Y \
+  -through _14612_/Y \
+  -through dmem/_156_/Y \
+  -to {riscv.dp.rf.rf[13][17]$_DFFE_PP_/D}]
 
 estimate_parasitics -placement
-report_checks -from $sp -through _10996_/Y -through _14612_/Y -through dmem/_156_/Y -to $ep -digits 5
+{*}$report_checks_cmd
 
 remove_buffers
 
-report_checks -from $sp -through _10996_/Y -through _14612_/Y -through dmem/_156_/Y -to $ep -digits 5
+# 3 report_checks results should be the same.
+{*}$report_checks_cmd
 
 estimate_parasitics -placement
-report_checks -from $sp -through _10996_/Y -through _14612_/Y -through dmem/_156_/Y -to $ep -digits 5
+{*}$report_checks_cmd
 
 sta::network_changed
 estimate_parasitics -placement
-report_checks -from $sp -through _10996_/Y -through _14612_/Y -through dmem/_156_/Y -to $ep -digits 5
+{*}$report_checks_cmd
 
-#set repaired_filename [make_result_file $test_name.def]
-#write_def $repaired_filename
-#diff_file $test_name.defok $repaired_filename
+set verilog_out [make_result_file $test_name.v]
+write_verilog $verilog_out
+diff_file $test_name.vok $verilog_out
