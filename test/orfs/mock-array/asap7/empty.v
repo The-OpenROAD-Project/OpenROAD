816854e1f2f0ff449b3bab539559b43e0d7c9618
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-11-27 01:04:14 +0100

    test/orfs: silence eqy warnings with dummy content in empty stdcells
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

---
+++ test/orfs/mock-array/asap7/empty.v
@@ -0,0 +1,41 @@
+// Used to silence warnings.
+module TAPCELL_ASAP7_75t_R ();
+  // silence eqy
+  wire dummy_wire;
+  assign dummy_wire = 1'b0;
+endmodule
+module FILLERxp5_ASAP7_75t_R;
+  // silence eqy
+  wire dummy_wire;
+  assign dummy_wire = 1'b0;
+endmodule
+module FILLER_ASAP7_75t_R ();
+  // silence eqy
+  wire dummy_wire;
+  assign dummy_wire = 1'b0;
+endmodule
+module DECAPx1_ASAP7_75t_R ();
+  // silence eqy
+  wire dummy_wire;
+  assign dummy_wire = 1'b0;
+endmodule
+module DECAPx2_ASAP7_75t_R ();
+  // silence eqy
+  wire dummy_wire;
+  assign dummy_wire = 1'b0;
+endmodule
+module DECAPx4_ASAP7_75t_R ();
+  // silence eqy
+  wire dummy_wire;
+  assign dummy_wire = 1'b0;
+endmodule
+module DECAPx6_ASAP7_75t_R ();
+  // silence eqy
+  wire dummy_wire;
+  assign dummy_wire = 1'b0;
+endmodule
+module DECAPx10_ASAP7_75t_R ();
+  // silence eqy
+  wire dummy_wire;
+  assign dummy_wire = 1'b0;
+endmodule
