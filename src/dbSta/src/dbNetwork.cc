11b3f006742ea56efc66a020074427aa9fd1dc40
Peter Gadfort <peter.gadfort@gmail.com>
2025-12-02 15:28:02 -0500

    dbsta: skip supply nets when iterating over pins
    Signed-off-by: Peter Gadfort <peter.gadfort@gmail.com>

--- src/dbSta/src/dbNetwork.cc
+++ src/dbSta/src/dbNetwork.cc
@@ -547,18 +547,21 @@ DbInstancePinIterator::DbInstancePinIterator(const Instance* inst,
 bool DbInstancePinIterator::hasNext()
 {
   if (top_) {
-    if (bitr_ == bitr_end_) {
-      return false;
+    while (bitr_ != bitr_end_) {
+      dbBTerm* bterm = *bitr_;
+      if (!network_->isPGSupply(bterm)) {
+        next_ = network_->dbToSta(bterm);
+        bitr_++;
+        return true;
+      }
+      bitr_++;
     }
-    dbBTerm* bterm = *bitr_;
-    next_ = network_->dbToSta(bterm);
-    bitr_++;
-    return true;
+    return false;
   }
 
   while (iitr_ != iitr_end_) {
     dbITerm* iterm = *iitr_;
-    if (!iterm->getSigType().isSupply()) {
+    if (!network_->isPGSupply(iterm)) {
       next_ = network_->dbToSta(*iitr_);
       ++iitr_;
       return true;
@@ -621,7 +624,7 @@ bool DbNetPinIterator::hasNext()
 {
   while (iitr_ != iitr_end_) {
     dbITerm* iterm = *iitr_;
-    if (!iterm->getSigType().isSupply()) {
+    if (!network_->isPGSupply(iterm)) {
       next_ = network_->dbToSta(*iitr_);
       ++iitr_;
       return true;
@@ -685,11 +688,14 @@ bool DbNetTermIterator::hasNext()
 
 Term* DbNetTermIterator::next()
 {
-  if (iter_ != end_) {
+  while (iter_ != end_) {
     dbBTerm* bterm = *iter_;
     iter_++;
-    return network_->dbToStaTerm(bterm);
+    if (!network_->isPGSupply(bterm)) {
+      return network_->dbToStaTerm(bterm);
+    }
   }
+
   if (mod_iter_ != mod_end_ && (network_->hasHierarchy())) {
     dbModBTerm* modbterm = *mod_iter_;
     mod_iter_++;
@@ -4989,6 +4995,33 @@ void dbNetwork::removeDriverFromCache(const Net* net, const Pin* drvr)
   if (drvrs) {
     drvrs->erase(drvr);
   }
+}
+
+bool dbNetwork::isPGSupply(dbITerm* iterm) const
+{
+  if (iterm->getSigType().isSupply()) {
+    return true;
+  }
+
+  return isPGSupply(iterm->getNet());
+}
+
+bool dbNetwork::isPGSupply(dbBTerm* bterm) const
+{
+  if (bterm->getSigType().isSupply()) {
+    return true;
+  }
+
+  return isPGSupply(bterm->getNet());
+}
+
+bool dbNetwork::isPGSupply(dbNet* net) const
+{
+  if (net == nullptr) {
+    return false;
+  }
+
+  return net->isSpecial() && net->getSigType().isSupply();
 }
 
 }  // namespace sta
