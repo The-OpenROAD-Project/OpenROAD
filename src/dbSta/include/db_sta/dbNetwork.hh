5389ace1e5d5cb93c0272edfb29b5a3e5237b8c0
Peter Gadfort <peter.gadfort@gmail.com>
2025-12-04 13:16:36 -0500

    dbsta: fix function argument name
    Signed-off-by: Peter Gadfort <peter.gadfort@gmail.com>

--- src/dbSta/include/db_sta/dbNetwork.hh
+++ src/dbSta/include/db_sta/dbNetwork.hh
@@ -163,7 +163,7 @@ class dbNetwork : public ConcreteNetwork
                          const dbIoType& io_type) const;
 
   bool isPGSupply(dbITerm* iterm) const;
-  bool isPGSupply(dbBTerm* iterm) const;
+  bool isPGSupply(dbBTerm* bterm) const;
   bool isPGSupply(dbNet* net) const;
 
   // dbStaCbk::inDbBTermCreate
