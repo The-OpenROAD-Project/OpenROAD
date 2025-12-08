2163ecf0c1bde8e52d2cdffba7c088c17285f691
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-12-08 13:57:54 +0100

    test/orfs/ram_8x7: simulation
    ```
    $ bazelisk test //test/orfs/ram_8x7:ram_8x7_floorplan_sim_test --test_output=streamed
    [deleted]
    [ RUN      ] Ram_8x7Test.SimpleTest
    test/orfs/ram_8x7/ram_8x7_sim.cpp:77: Failure
    Expected equality of these values:
      harness.top.R0_data
        Which is: '\0'
      (i - 1) + 100
        Which is: 128
    ```
    
    ```
    $ bazelisk test //test/orfs/ram_8x7:ram_8x7_source_sim_test --test_output=streamed --cache_test_results=no
    [deleted]
    Running main() from gmock_main.cc
    [==========] Running 1 test from 1 test suite.
    [----------] Global test environment set-up.
    [----------] 1 test from Ram_8x7Test
    [ RUN      ] Ram_8x7Test.SimpleTest
    [       OK ] Ram_8x7Test.SimpleTest (4 ms)
    [----------] 1 test from Ram_8x7Test (4 ms total)
    
    [----------] Global test environment tear-down
    [==========] 1 test from 1 test suite ran. (4 ms total)
    [  PASSED  ] 1 test.
    ```
    
    ```
    $ git diff
    diff --git a/test/orfs/ram_8x7/ram_8x7_sim.cpp b/test/orfs/ram_8x7/ram_8x7_sim.cpp
    index 1bede1f518..6a87f5babf 100644
    --- a/test/orfs/ram_8x7/ram_8x7_sim.cpp
    +++ b/test/orfs/ram_8x7/ram_8x7_sim.cpp
    @@ -74,7 +74,7 @@ TEST(Ram_8x7Test, SimpleTest)
    
         if (i > 0) {
           // Check read data from previous write
    -      EXPECT_EQ(harness.top.R0_data, (i - 1) + 100);
    +      EXPECT_EQ(harness.top.R0_data, ((i - 1) + 100) % 128);
         }
       }
     }
    $ bazelisk test //test/orfs/ram_8x7:ram_8x7_floorplan_sim_test --test_output=streamed
    INFO: Invocation ID: 9383fe03-da25-467b-9ed8-a2872a6a1d00
    WARNING: Streamed test output requested. All tests will be run locally, without sharding, one at a time
    INFO: Analyzed target //test/orfs/ram_8x7:ram_8x7_floorplan_sim_test (0 packages loaded, 0 targets configured).
    INFO: Found 1 test target...
    Target //test/orfs/ram_8x7:ram_8x7_floorplan_sim_test up-to-date:
      bazel-bin/test/orfs/ram_8x7/ram_8x7_floorplan_sim_test
    INFO: Elapsed time: 0.353s, Critical Path: 0.00s
    INFO: 1 process: 4 action cache hit, 1 internal.
    INFO: Build completed successfully, 1 total action
    //test/orfs/ram_8x7:ram_8x7_floorplan_sim_test                  (cached) PASSED in 0.0s
    
    Executed 0 out of 1 test: 1 test passes.
    ```
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

---
+++ test/orfs/ram_8x7/ram_8x7_sim.cpp
@@ -0,0 +1,80 @@
+#include "Vram_8x7.h"
+// #include <gmock/gmock-matchers.h>
+#include <gmock/gmock-matchers.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+
+#include <string>
+
+class Ram_8x7TestHarness
+{
+ public:
+  VerilatedContext ctx;
+  Vram_8x7 top;
+  std::unique_ptr<VerilatedVcdC> trace;
+
+  Ram_8x7TestHarness(const std::string& vcd_file)
+  {
+    ctx.traceEverOn(true);
+    trace = std::make_unique<VerilatedVcdC>();
+    top.trace(trace.get(), 100);
+    trace->open(vcd_file.c_str());
+  }
+
+  ~Ram_8x7TestHarness() { trace->close(); }
+
+  // Simulate one clock cycle
+  void step()
+  {
+    // evaluate to pick up changes on inputs
+    top.eval();
+    // show registers changing on rising clock edge as well as inputs changing
+    // on rising clock edge
+    ctx.timeInc(1);
+    trace->dump(ctx.time());
+
+    top.R0_clk = 0;
+    top.W0_clk = 0;
+    top.eval();
+    ctx.timeInc(1);
+    trace->dump(ctx.time());
+
+    top.R0_clk = 1;
+    top.W0_clk = 1;
+    top.eval();
+    // Now we're ready to pick up poked inputs on rising edge
+  }
+};
+
+TEST(Ram_8x7Test, SimpleTest)
+{
+  const char* output_dir = std::getenv("TEST_UNDECLARED_OUTPUTS_DIR");
+  std::string vcd_path
+      = output_dir ? std::string(output_dir) + "/trace.vcd" : "trace.vcd";
+  Ram_8x7TestHarness harness(vcd_path);
+
+  harness.step();
+  harness.step();
+  harness.step();
+
+  // unit-test for combinational read ram_8x7 in a loop
+  for (int i = 0; i < 32; ++i) {
+    harness.top.W0_en = 1;
+    harness.top.W0_addr = (i + 1) % 8;
+    harness.top.W0_data = i + 100;  // Arbitrary data
+
+    // Set up combinational read
+    harness.top.R0_en = 1;
+    harness.top.R0_addr = i % 8;
+    harness.step();
+    harness.top.W0_en = 0;
+    harness.top.R0_en = 0;
+
+    if (i > 0) {
+      // Check read data from previous write
+      EXPECT_EQ(harness.top.R0_data, (i - 1) + 100);
+    }
+  }
+}
