fd820446895b28d1a1bf06e8b76c069f188b8a97
PrecisEDAnon <PrecisEDAnon@proton.me>
2025-12-14 22:18:15 +0000

    Add extra DPL toggle
    Signed-off-by: PrecisEDAnon <PrecisEDAnon@proton.me>

--- src/dpl/src/optimization/detailed.cxx
+++ src/dpl/src/optimization/detailed.cxx
@@ -17,6 +17,7 @@
 #include "boost/tokenizer.hpp"
 #include "detailed.h"
 #include "optimization/detailed_global.h"
+#include "optimization/detailed_global_legacy.h"
 #include "optimization/detailed_mis.h"
 #include "optimization/detailed_orient.h"
 #include "optimization/detailed_random.h"
@@ -143,8 +144,13 @@ void Detailed::doDetailedCommand(std::vector<std::string>& args)
     DetailedMis mis(arch_, network_);
     mis.run(mgr_, args);
   } else if (strcmp(args[0].c_str(), "gs") == 0) {
-    DetailedGlobalSwap gs(arch_, network_);
-    gs.run(mgr_, args);
+    if (mgr_->isExtraDplEnabled()) {
+      DetailedGlobalSwap gs(arch_, network_);
+      gs.run(mgr_, args);
+    } else {
+      legacy::DetailedGlobalSwap gs(arch_, network_);
+      gs.run(mgr_, args);
+    }
   } else if (strcmp(args[0].c_str(), "vs") == 0) {
     DetailedVerticalSwap vs(arch_, network_);
     vs.run(mgr_, args);
