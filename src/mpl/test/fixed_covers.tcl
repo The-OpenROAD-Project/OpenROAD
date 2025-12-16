afd4d35c4e710611013f3b031d0be3d3d5d10e6f
Arthur Koucher <arthurkoucher@precisioninno.com>
2025-12-16 11:30:26 -0300

    mpl: ignore COVERs when searching for fixed insts in macro placement area
    Signed-off-by: Arthur Koucher <arthurkoucher@precisioninno.com>

--- src/mpl/test/fixed_covers.tcl
+++ src/mpl/test/fixed_covers.tcl
@@ -14,7 +14,7 @@ place_inst -cell DUMMY_BUMP -name "test_pad1" -orient R0 -status FIRM -loc "100 
 set_thread_count 0
 rtl_macro_placer -report_directory [make_result_dir] -halo_width 0.3
 
-set def_file [make_result_file fixed_macros.def]
+set def_file [make_result_file fixed_covers.def]
 write_def $def_file
 
-diff_files fixed_macros.defok $def_file
+diff_files fixed_covers.defok $def_file
