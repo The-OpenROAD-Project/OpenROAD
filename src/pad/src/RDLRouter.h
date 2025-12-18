8029077b55da87de9ea32e475b81e260905d94d5
Peter Gadfort <peter.gadfort@gmail.com>
2025-12-18 12:32:57 -0500

    pad: add DB property to enable/disable RDL routing to specific iterms
    Signed-off-by: Peter Gadfort <peter.gadfort@gmail.com>

--- src/pad/src/RDLRouter.h
+++ src/pad/src/RDLRouter.h
@@ -246,6 +246,9 @@ class RDLRouter
   // Debugging
   RDLGui* gui_;
   odb::dbNet* debug_net_{nullptr};
+
+  // Consts
+  static constexpr const char* kRouteProperty_ = "RDL_ROUTE";
 };
 
 }  // namespace pad
