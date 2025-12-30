fe55901aaa7788f841fa56e44e58eb3a77fdcc05
Ahmed R. Mohamed <ahmed@precisioninno.com>
2025-12-30 22:27:35 +0200

    odb: fix 3dblox declaration mismatches
    Some structs in src/odb/include/odb/3dblox.h where forward declared
    as classes. Which errored with "-Wmismatched-tags" compiler flag.
    Also, found another mismatch in PartitionMgr.h
    Note that C++ does not distinguish between class and struct in a
    forward declaration. Only MSVC can have issues with it.
    
    Signed-off-by: Ahmed R. Mohamed <ahmed@precisioninno.com>

--- src/odb/include/odb/3dblox.h
+++ src/odb/include/odb/3dblox.h
@@ -16,12 +16,7 @@ class Sta;
 }
 namespace odb {
 class dbDatabase;
-class ChipletDef;
-class ChipletRegion;
 class dbChip;
-class ChipletInst;
-class Connection;
-class DesignDef;
 class dbChipRegionInst;
 class dbChipInst;
 class dbChipRegion;
@@ -31,6 +26,11 @@ class dbInst;
 class dbTech;
 class dbLib;
 
+struct ChipletDef;
+struct ChipletRegion;
+struct ChipletInst;
+struct Connection;
+struct DesignDef;
 struct BumpMapEntry;
 
 class ThreeDBlox
