32ce4f9e9c4a63fa950c0620f21468fd692661cf
Damon Anderson <damon@h3dgamma.com>
2026-01-08 12:13:48 -0500

    Updated getViaDef(s) to check which cuts are available, and return the first valid via
    Signed-off-by: Damon Anderson <damon@h3dgamma.com>

--- src/drt/src/db/obj/frAccess.h
+++ src/drt/src/db/obj/frAccess.h
@@ -103,11 +103,31 @@ class frAccessPoint : public frBlockObject
   // e.g., getViaDefs(2)    --> get all two-cut viadefs
   const std::vector<const frViaDef*>& getViaDefs(int numCut = 1) const
   {
-    return viaDefs_[numCut - 1];
+    // Prioritize requested numCut:
+    if (hasViaDef(numCut, 0)) return viaDefs_[numCut - 1];
+
+	// Otherwise, look for a valid one:
+	for (int vi = 0; vi < 2; vi++) {
+		if (hasViaDef(vi+1, 0)) return viaDefs_[(vi+1) - 1];
+	}
+
+	// No valid vias exist
+	static std::vector<const frViaDef*> empty;
+	return empty;
   }
   std::vector<const frViaDef*>& getViaDefs(int numCut = 1)
   {
-    return viaDefs_[numCut - 1];
+	// Prioritize requested numCut:
+    if (hasViaDef(numCut, 0)) return viaDefs_[numCut - 1];
+
+	// Otherwise, look for a valid one:
+	for (int vi = 0; vi < 2; vi++) {
+		if (hasViaDef(vi+1, 0)) return viaDefs_[(vi+1) - 1];
+	}
+
+	// No valid vias exist
+	static std::vector<const frViaDef*> empty;
+	return empty;
   }
   const std::vector<std::vector<const frViaDef*>>& getAllViaDefs() const
   {
@@ -129,7 +149,16 @@ class frAccessPoint : public frBlockObject
   // e.g., getViaDef(1, 1) --> get 1st alternative one-cut viadef
   const frViaDef* getViaDef(int numCut = 1, int idx = 0) const
   {
-    return viaDefs_[numCut - 1][idx];
+    // Prioritize requested numCut:
+	if (hasViaDef(numCut, idx)) return viaDefs_[numCut - 1][idx];
+
+	// Otherwise, look for a valid one:
+	for (int vi = 0; vi < 2; vi++) {
+		if (hasViaDef(vi+1, idx)) return viaDefs_[(vi+1) - 1][idx];
+	}
+
+	// No valid vias exist
+    return nullptr;
   }
   frPinAccess* getPinAccess() const { return aps_; }
   frCost getCost() const { return (int) typeL_ + 4 * (int) typeH_; }
