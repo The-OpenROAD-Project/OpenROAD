291aa47722325099699ea87f5fec9904f6f40b42
Mike Inouye <mikeinouye@google.com>
2026-01-09 14:46:40 -0800

    Gemini lint
    Signed-off-by: Mike Inouye <mikeinouye@google.com>

--- src/utl/src/ScopedTemporaryFile.cpp
+++ src/utl/src/ScopedTemporaryFile.cpp
@@ -67,7 +67,7 @@ ScopedTemporaryFile::~ScopedTemporaryFile()
 OutStreamHandler::OutStreamHandler(const char* filename, bool binary)
     : filename_(filename)
 {
-  if (!strcmp(filename, "")) {
+  if (filename_.empty()) {
     std::throw_with_nested(std::runtime_error("filename is empty"));
   }
   tmp_filename_ = generate_unused_filename(filename_);
