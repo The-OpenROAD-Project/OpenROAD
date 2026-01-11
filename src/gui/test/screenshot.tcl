f811edf2887bfb87171fd1b42982544dd761a647
Peter Gadfort <gadfort@zeroasic.com>
2026-01-11 12:29:11 -0500

    gui: add screenshot tests to GUI tests to provide basic (very basic) testing
    Signed-off-by: Peter Gadfort <gadfort@zeroasic.com>

---
+++ src/gui/test/screenshot.tcl
@@ -0,0 +1,16 @@
+# test for removing non-fixed bterms
+source "helpers.tcl"
+
+read_lef Nangate45/Nangate45_tech.lef
+read_lef Nangate45/Nangate45_stdcell.lef
+read_def nangate_gcd/floorplan.def
+
+set img [make_result_file screenshot.png]
+
+save_image $img
+if {![file exists $img]} {
+    puts "Error: screenshot.png was not created"
+    exit 1
+} else {
+    puts "screenshot.png created successfully"
+}
