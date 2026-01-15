20726b692b7463e89635e211c06331be587e21ac
Peter Gadfort <gadfort@zeroasic.com>
2026-01-15 10:54:20 -0500

    gui: ensure the gui can detertime shape of image
    Signed-off-by: Peter Gadfort <gadfort@zeroasic.com>

--- src/gui/test/screenshot.tcl
+++ src/gui/test/screenshot.tcl
@@ -7,10 +7,9 @@ read_def nangate_gcd/floorplan.def
 
 set img [make_result_file screenshot.png]
 
-save_image $img
+save_image -resolution 0.100 -area [ord::get_die_area] $img
 if { ![file exists $img] } {
   puts "Error: screenshot.png was not created"
-  exit 1
 } else {
   puts "screenshot.png created successfully"
 }
