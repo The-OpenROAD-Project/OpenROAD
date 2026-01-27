e45a5a1fa44f9b3fce8b6dfa729317380e0bbd20
Jaehyun Kim <jhkim@precisioninno.com>
2026-01-27 20:11:24 +0900

    dbSta: Avoid multiple creations of dbNet related to feed-through net
    Signed-off-by: Jaehyun Kim <jhkim@precisioninno.com>

--- src/dbSta/src/dbReadVerilog.cc
+++ src/dbSta/src/dbReadVerilog.cc
@@ -70,6 +70,7 @@ using sta::Network;
 using sta::Pin;
 using sta::PinPathNameLess;
 using sta::PinSeq;
+using sta::PinSet;
 using sta::Port;
 using sta::PortDirection;
 using sta::Term;
@@ -161,7 +162,7 @@ class Verilog2db
                dbModBTerm*& mod_bterm,
                dbModITerm*& mod_iterm);
   void recordBusPortsOrder();
-  void makeDbNets(const Instance* inst);
+  void makeDbNets(const Instance* inst, PinSet& visited_pins);
 
   void makeVModNets(const Instance* inst, dbModInst* mod_inst);
   void makeVModNets(InstPairs& inst_pairs);
@@ -271,7 +272,8 @@ void Verilog2db::makeDbNetlist()
   // As a side effect we accumulate the instance <-> modinst pairs
   InstPairs inst_pairs;
   makeDbModule(network_->topInstance(), /* parent */ nullptr, inst_pairs);
-  makeDbNets(network_->topInstance());
+  PinSet visited_pins(network_);
+  makeDbNets(network_->topInstance(), visited_pins);
   if (hierarchy_) {
     makeVModNets(inst_pairs);
   }
@@ -695,7 +697,7 @@ dbIoType Verilog2db::staToDb(PortDirection* dir)
   return dbIoType::INOUT;
 }
 
-void Verilog2db::makeDbNets(const Instance* inst)
+void Verilog2db::makeDbNets(const Instance* inst, PinSet& visited_pins)
 {
   bool is_top = (inst == network_->topInstance());
   std::unique_ptr<NetIterator> net_iter{network_->netIterator(inst)};
@@ -707,6 +709,26 @@ void Verilog2db::makeDbNets(const Instance* inst)
       continue;
     }
 
+    // Collect connected pins
+    PinSeq net_pins;
+    bool already_visited = false;
+    std::unique_ptr<NetConnectedPinIterator> pin_iter{
+        network_->connectedPinIterator(net)};
+    while (pin_iter->hasNext()) {
+      const Pin* pin = pin_iter->next();
+      if (visited_pins.contains(pin)) {
+        already_visited = true;
+        break;
+      }
+      net_pins.push_back(pin);
+      visited_pins.insert(pin);
+    }
+
+    if (already_visited) {
+      continue;
+    }
+
+    // Create a new flat net
     const char* net_name = network_->pathName(net);
     dbNet* db_net = dbNet::create(block_, net_name);
     debugPrint(logger_,
@@ -723,16 +745,10 @@ void Verilog2db::makeDbNets(const Instance* inst)
       db_net->setSigType(odb::dbSigType::GROUND);
     }
 
-    // Sort connected pins for regression stability.
-    PinSeq net_pins;
-    std::unique_ptr<NetConnectedPinIterator> pin_iter{
-        network_->connectedPinIterator(net)};
-    while (pin_iter->hasNext()) {
-      const Pin* pin = pin_iter->next();
-      net_pins.push_back(pin);
-    }
+    // Sort connected pins for regression stability
     sort(net_pins, PinPathNameLess(network_));
 
+    // Connect pins to the new flat net
     for (const Pin* pin : net_pins) {
       if (network_->isTopLevelPort(pin)) {
         const char* port_name = network_->portName(pin);
@@ -774,11 +790,12 @@ void Verilog2db::makeDbNets(const Instance* inst)
     }
   }
 
+  // Recursion into child module instances
   std::unique_ptr<InstanceChildIterator> child_iter{
       network_->childIterator(inst)};
   while (child_iter->hasNext()) {
     const Instance* child = child_iter->next();
-    makeDbNets(child);
+    makeDbNets(child, visited_pins);
   }
 }
 
@@ -1189,8 +1206,9 @@ void Verilog2db::makeUnusedDbNetlist()
   makeModBTerms(cell, module);
   InstPairs inst_pairs;
   makeChildInsts(inst, module, inst_pairs);
-  makeDbNets(inst);
-  // Create top-level mod nets
+  // Create top-level
+  PinSet visited_pins(network_);
+  makeDbNets(inst, visited_pins);
   makeModNets(inst);
   if (hierarchy_) {
     makeVModNets(inst_pairs);
