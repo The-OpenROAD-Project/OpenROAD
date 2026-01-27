8d6e07044ab59c098e3f9eb10964a7511f099fd9
Arthur Koucher <arthurkoucher@precisioninno.com>
2026-01-27 19:01:37 +0000

    mpl: error early when macro placement is unfeasible
    Signed-off-by: Arthur Koucher <arthurkoucher@precisioninno.com>

--- src/mpl/src/hier_rtlmp.h
+++ src/mpl/src/hier_rtlmp.h
@@ -126,6 +126,7 @@ class HierRTLMP
   using SoftSAVector = std::vector<std::unique_ptr<SACoreSoftMacro>>;
   using HardSAVector = std::vector<std::unique_ptr<SACoreHardMacro>>;
 
+  bool macroPlacementIsFeasible() const;
   void runMultilevelAutoclustering();
   void runHierarchicalMacroPlacement();
 
