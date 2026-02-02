c482861e93e510fc6f262b7e2423294992ef5b2f
Eder Monteiro <emrmonteiro@precisioninno.com>
2026-02-02 17:14:22 -0300

    odb: properly restore dbGuide attributes
    Signed-off-by: Eder Monteiro <emrmonteiro@precisioninno.com>

--- src/odb/src/db/dbGuide.cpp
+++ src/odb/src/db/dbGuide.cpp
@@ -213,6 +213,9 @@ void dbGuide::destroy(dbGuide* guide)
     block->journal_->pushParam(_guide->layer_);
     block->journal_->pushParam(_guide->via_layer_);
     block->journal_->pushParam(_guide->is_congested_);
+    block->journal_->pushParam(_guide->is_jumper_);
+    block->journal_->pushParam(_guide->ignore_usage_);
+    block->journal_->pushParam(_guide->is_connect_to_term_);
     block->journal_->endAction();
   }
 
--- src/odb/src/db/dbJournal.cpp
+++ src/odb/src/db/dbJournal.cpp
@@ -1946,6 +1946,9 @@ void dbJournal::undo_deleteObject()
       uint32_t layer_id;
       uint32_t via_layer_id;
       bool is_congested;
+      bool is_jumper_;
+      bool ignore_usage_;
+      bool is_connect_to_term_;
       log_.pop(net_id);
       log_.pop(x_min);
       log_.pop(y_min);
@@ -1954,12 +1957,18 @@ void dbJournal::undo_deleteObject()
       log_.pop(layer_id);
       log_.pop(via_layer_id);
       log_.pop(is_congested);
+      log_.pop(is_jumper_);
+      log_.pop(ignore_usage_);
+      log_.pop(is_connect_to_term_);
       auto net = dbNet::getNet(block_, net_id);
       auto layer = dbTechLayer::getTechLayer(block_->getTech(), layer_id);
       auto via_layer
           = dbTechLayer::getTechLayer(block_->getTech(), via_layer_id);
       auto guide = dbGuide::create(
           net, layer, via_layer, {x_min, y_min, x_max, y_max}, is_congested);
+      guide->setIsJumper(is_jumper_);
+      guide->setIgnoreUsage(ignore_usage_);
+      guide->setIsConnectedToTerm(is_connect_to_term_);
       debugPrint(logger_,
                  utl::ODB,
                  "DB_ECO",
