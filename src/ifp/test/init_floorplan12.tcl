e4c5a72440c3ca42e8fefa012bb0ed4bba804d49
Peter Gadfort <gadfort@zeroasic.com>
2026-02-03 14:01:11 -0500

    ifp: format tcl test
    Signed-off-by: Peter Gadfort <gadfort@zeroasic.com>

--- src/ifp/test/init_floorplan12.tcl
+++ src/ifp/test/init_floorplan12.tcl
@@ -5,8 +5,10 @@ read_liberty Nangate45/Nangate45_typ.lib
 read_verilog reg1.v
 link_design top
 
-catch {initialize_floorplan -utilization 99 \
-  -aspect_ratio 0.2 \
-  -core_space 1 \
-  -site FreePDK45_38x28_10R_NP_162NW_34O} err
+catch {
+    initialize_floorplan -utilization 99 \
+        -aspect_ratio 0.2 \
+        -core_space 1 \
+        -site FreePDK45_38x28_10R_NP_162NW_34O
+} err
 puts $err
