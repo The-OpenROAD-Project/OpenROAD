9490ae058f9a994489fc1152908963b77c221000
Henner Zeller <h.zeller@acm.org>
2026-02-20 21:26:42 +0100

    Run `nixfmt` on default.nix, flake.nix
    Signed-off-by: Henner Zeller <h.zeller@acm.org>

--- default.nix
+++ default.nix
@@ -40,38 +40,37 @@
   overrideSDK,
   git,
   yaml-cpp,
-}: let
+}:
+let
   or-tools' =
     (or-tools.override {
       ## Alligned alloc not available on the default SDK for x86_64-darwin (10.12!!)
-      stdenv =
-        if stdenv.isDarwin
-        then (overrideSDK stdenv "11.0")
-        else stdenv;
-    })
-    .overrideAttrs (finalAttrs: previousAttrs: {
-      # Based on https://github.com/google/or-tools/commit/af44f98dbeb905656b5a9fc664b5fdcffcbe1f60
-      # Stops CMake going haywire on reconfigures
-      postPatch =
-        previousAttrs.postPatch
-        + ''
-          sed -Ei.bak 's/(NOT\s+\w+_FOUND\s+AND\s+)+//' cmake/ortoolsConfig.cmake.in
-          sed -Ei.bak 's/NOT absl_FOUND/NOT TARGET absl::base/' cmake/ortoolsConfig.cmake.in
-        '';
-    });
+      stdenv = if stdenv.isDarwin then (overrideSDK stdenv "11.0") else stdenv;
+    }).overrideAttrs
+      (
+        finalAttrs: previousAttrs: {
+          # Based on https://github.com/google/or-tools/commit/af44f98dbeb905656b5a9fc664b5fdcffcbe1f60
+          # Stops CMake going haywire on reconfigures
+          postPatch = previousAttrs.postPatch + ''
+            sed -Ei.bak 's/(NOT\s+\w+_FOUND\s+AND\s+)+//' cmake/ortoolsConfig.cmake.in
+            sed -Ei.bak 's/NOT absl_FOUND/NOT TARGET absl::base/' cmake/ortoolsConfig.cmake.in
+          '';
+        }
+      );
   fetchedGtest = fetchzip {
     url = "https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip";
     sha256 = "sha256-t0RchAHTJbuI5YW4uyBPykTvcjy90JW9AOPNjIhwh6U=";
   };
-  lemon-graph' =
-    lemon-graph.overrideAttrs (finalAttrs: previousAttrs: {
+  lemon-graph' = lemon-graph.overrideAttrs (
+    finalAttrs: previousAttrs: {
       src = fetchFromGitHub {
         owner = "The-OpenROAD-Project";
         repo = "lemon-graph";
         rev = "f871b10396270cfd09ffddc4b6ead07722e9c232";
         sha256 = "snqjc82vtgKC5uGu7V6Hhcf7YzRk0xHJDEOCN91iywI=";
       };
-  });
+    }
+  );
   self = clangStdenv.mkDerivation (finalAttrs: {
     name = "openroad";
 
@@ -84,23 +83,25 @@
       "-DVERBOSE=1"
       "-DFETCHCONTENT_SOURCE_DIR_GOOGLETEST=${fetchedGtest}"
     ];
-    
+
     postPatch = ''
       patchShebangs .
     '';
-    
+
     shellHook = ''
       alias ord-format-changed="${git}/bin/git diff --name-only | grep -E '\.(cpp|cc|c|h|hh)$' | xargs clang-format -i -style=file:.clang-format"; 
     '';
-    
+
     qt5Libs = [
       libsForQt5.qt5.qtbase
       libsForQt5.qt5.qtcharts
       libsForQt5.qt5.qtsvg
       libsForQt5.qt5.qtdeclarative
     ];
-    
-    QT_PLUGIN_PATH = lib.makeSearchPathOutput "bin" "lib/qt-${libsForQt5.qt5.qtbase.version}/plugins" self.qt5Libs;
+
+    QT_PLUGIN_PATH =
+      lib.makeSearchPathOutput "bin" "lib/qt-${libsForQt5.qt5.qtbase.version}/plugins"
+        self.qt5Libs;
 
     buildInputs = self.qt5Libs ++ [
       abseil-cpp
@@ -139,4 +140,4 @@
     ];
   });
 in
-  self
+self
