bc320931092c314ab09b5253f863571314225dc9
João Mai <jmai@precisioninno.com>
2026-02-26 16:29:21 +0000

    mpl: halos regression tests
    Signed-off-by: João Mai <jmai@precisioninno.com>

--- src/mpl/test/halos2.tcl
+++ src/mpl/test/halos2.tcl
@@ -9,9 +9,11 @@ read_lef "./testcases/orientation_improve1.lef"
 read_def "./testcases/halos1.def"
 
 set_thread_count 0
+set_macro_halo -macro MACRO_1 -halo {8 4 2 1}
+set_macro_halo -macro MACRO_2 -halo {2 4}
 rtl_macro_placer -report_directory [make_result_dir]
 
-set def_file [make_result_file halos1.def]
+set def_file [make_result_file halos2.def]
 write_def $def_file
 
-diff_files halos1.defok $def_file
+diff_files halos2.defok $def_file
