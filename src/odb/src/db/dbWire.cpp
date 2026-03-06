d5b0417007677c8811cb30b90a732e67ba07f687
Arthur Koucher <arthurkoucher@precisioninno.com>
2026-03-06 15:51:45 -0300

    odb: improve naming for future bug fix review
    Signed-off-by: Arthur Koucher <arthurkoucher@precisioninno.com>

--- src/odb/src/db/dbWire.cpp
+++ src/odb/src/db/dbWire.cpp
@@ -124,32 +124,47 @@ void dbWire::addOneSeg(unsigned char op, int value)
   wire->opcodes_.push_back(op);
 }
 
-uint32_t dbWire::getTermJid(const int termid) const
+uint32_t dbWire::getTermJid(const int term_id) const
 {
   _dbWire* wire = (_dbWire*) this;
-  int topcd = kIterm;
-  int ttid = termid;
-  if (termid < 0) {
-    topcd = kBterm;
-    ttid = -termid;
+  int target_op_code = kIterm;
+  int target_term_id = term_id;
+
+  if (term_id < 0) {
+    target_op_code = kBterm;
+    target_term_id = -term_id;
   }
-  const uint32_t wlen = wire->length();
-  uint32_t jj;
-  for (jj = 0; jj < wlen; jj++) {
-    if ((wire->opcodes_[jj] & WOP_OPCODE_MASK) == topcd) {
-      if (wire->data_[jj] == ttid) {
+
+  const uint32_t number_of_junctions = wire->length();
+
+  uint32_t junction_id;
+  for (junction_id = 0; junction_id < number_of_junctions; junction_id++) {
+    const WireOp junction_op_code
+        = static_cast<WireOp>(wire->opcodes_[junction_id] & WOP_OPCODE_MASK);
+
+    if (junction_op_code == target_op_code) {
+      const int term_id = wire->data_[junction_id];
+
+      if (term_id == target_term_id) {
         break;
       }
     }
   }
-  if (jj == wlen) {
+
+  if (junction_id == number_of_junctions) {
     return 0;
   }
-  jj--;
-  if ((wire->opcodes_[jj] & WOP_OPCODE_MASK) == kProperty) {
-    jj--;
+
+  junction_id--;
+
+  const WireOp junction_op_code
+      = static_cast<WireOp>(wire->opcodes_[junction_id] & WOP_OPCODE_MASK);
+
+  if (junction_op_code == kProperty) {
+    junction_id--;
   }
-  return jj;
+
+  return junction_id;
 }
 
 std::optional<Rect> dbWire::getBBox()
