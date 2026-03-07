e21feae9ab37d9c6fccd7cb33c7bf897359665bf
Eder Monteiro <emrmonteiro@precisioninno.com>
2026-03-07 13:28:45 -0300

    grt: merge segments when loading guides
    Signed-off-by: Eder Monteiro <emrmonteiro@precisioninno.com>

--- src/grt/src/GlobalRouter.cpp
+++ src/grt/src/GlobalRouter.cpp
@@ -3287,24 +3287,30 @@ void GlobalRouter::boxToGlobalRouting(const odb::Rect& route_bds,
                                       GRoute& route)
 {
   const int tile_size = grid_->getTileSize();
-  int x0 = (tile_size * (route_bds.xMin() / tile_size)) + (tile_size / 2);
-  int y0 = (tile_size * (route_bds.yMin() / tile_size)) + (tile_size / 2);
+
+  const int x0 = (tile_size * (route_bds.xMin() / tile_size)) + (tile_size / 2);
+  const int y0 = (tile_size * (route_bds.yMin() / tile_size)) + (tile_size / 2);
 
   const int x1 = (tile_size * (route_bds.xMax() / tile_size)) - (tile_size / 2);
   const int y1 = (tile_size * (route_bds.yMax() / tile_size)) - (tile_size / 2);
 
+  if (x0 != x1 && y0 != y1) {
+    logger_->error(
+        GRT,
+        305,
+        "Segment ({}, {}, {}, {}, {}, {}) is not horizontal/vertical.",
+        x0,
+        y0,
+        layer,
+        x1,
+        y1,
+        layer);
+  }
+
   if (x0 == x1 && y0 == y1) {
     route.emplace_back(x0, y0, layer, x1, y1, via_layer);
-  }
-
-  while (y0 == y1 && (x0 + tile_size) <= x1) {
-    route.emplace_back(x0, y0, layer, x0 + tile_size, y0, layer);
-    x0 += tile_size;
-  }
-
-  while (x0 == x1 && (y0 + tile_size) <= y1) {
-    route.emplace_back(x0, y0, layer, x0, y0 + tile_size, layer);
-    y0 += tile_size;
+  } else {
+    route.emplace_back(x0, y0, layer, x1, y1, layer);
   }
 }
 
