bfb68c6934728ca998e44f3831338e4822fcabbf
osamahammad21 <osama21@aucegypt.edu>
2026-03-11 20:03:01 +0200

    odb: codegenerate
    Signed-off-by: osamahammad21 <osama21@aucegypt.edu>

--- src/odb/src/db/dbDatabase.h
+++ src/odb/src/db/dbDatabase.h
@@ -337,6 +337,7 @@ class _dbDatabase : public _dbObject
   utl::Logger* logger_;
   std::set<dbDatabaseObserver*> observers_;
   UnfoldedModel* unfolded_model_;  // non-persistent object
+
   // User Code End Fields
 };
 dbIStream& operator>>(dbIStream& stream, _dbDatabase& obj);
