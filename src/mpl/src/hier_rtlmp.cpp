6c9fd47efb71f270eec1322a8985c6fb63c83bc7
João Mai <jmai@precisioninno.com>
2026-03-19 16:59:24 +0000

    mpl: remove blockage creation in MPL
    Signed-off-by: João Mai <jmai@precisioninno.com>

--- src/mpl/src/hier_rtlmp.cpp
+++ src/mpl/src/hier_rtlmp.cpp
@@ -2360,15 +2360,7 @@ void HierRTLMP::commitMacroPlacementToDb()
 
     snapper.setMacro(inst);
     snapper.snapMacro();
-
-    if (inst->getHalo() == nullptr || !inst->getHalo()->isSoft()) {
-      macro->setRealLocation(inst->getLocation());
-      odb::Rect box = macro->getBBox();
-      odb::dbBlockage* blockage = odb::dbBlockage::create(
-          block_, box.xMin(), box.yMin(), box.xMax(), box.yMax(), inst);
-      blockage->setSoft();
-    }
-
+    
     inst->setPlacementStatus(odb::dbPlacementStatus::LOCKED);
   }
 }
