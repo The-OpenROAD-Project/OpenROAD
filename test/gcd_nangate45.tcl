93c2bbc16e464f92e56ee848b8e27bed83d861f6
Waleed <waleedakram059@gmail.com>
2026-03-19 14:07:44 +0500

    Incidentally pushed old files , now modified the one to which I got the report and added in Blog
    Signed-off-by: Waleed <waleedakram059@gmail.com>

--- test/gcd_nangate45.tcl
+++ test/gcd_nangate45.tcl
@@ -10,5 +10,5 @@ set sdc_file "gcd_nangate45.sdc"
 set die_area {0 0 100.13 100.8}
 set core_area {10.07 11.2 90.25 91}
 
-set global_place_density 0.3
+set global_place_density 0.4
 include -echo "flow.tcl"
