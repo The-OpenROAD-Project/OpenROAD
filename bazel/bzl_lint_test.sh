67246b418d4e17ae00a82f58126bc218c3c88ee6
Divinesoumyadip <soumyacode7@gmail.com>
2026-03-22 18:44:02 +0000

    bazel: fix bzl scripts to use readlink-f and git ls-files pattern
    Signed-off-by: Divinesoumyadip <soumyacode7@gmail.com>

--- bazel/bzl_lint_test.sh
+++ bazel/bzl_lint_test.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
-set -e
-BUILDIFIER_BIN=$(realpath "$1")
-if [ -n "${BUILD_WORKSPACE_DIRECTORY:-}" ]; then
-  cd "$BUILD_WORKSPACE_DIRECTORY"
-fi
-"$BUILDIFIER_BIN" -r -mode=check -lint=warn .
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025-2025, The OpenROAD Authors
+#
+# Lint all Starlark files using buildifier.
+set -euo pipefail
+TOOL="$(readlink -f "$1")"
+WORKSPACE="$(dirname "$(readlink -f BUILD.bazel)")"
+cd "$WORKSPACE"
+git ls-files -- '*.bzl' 'BUILD' 'BUILD.bazel' 'MODULE.bazel' 'WORKSPACE' 'WORKSPACE.bazel' -z | xargs -0 "$TOOL" -mode=check -lint=warn
