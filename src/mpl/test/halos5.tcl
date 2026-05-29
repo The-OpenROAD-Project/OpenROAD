dc85178665445162480df9d83547f3ff1bf2e736
João Mai <jmai@precisioninno.com>
2026-05-29 21:03:39 +0000

    mpl: test flag -use_full_halo
    Signed-off-by: João Mai <jmai@precisioninno.com>

---
+++ src/mpl/test/halos5.tcl
@@ -0,0 +1,16 @@
+# Test that -use_full_halo restores uniform halo behavior,
+# ignoring pin-side detection (regression for the use_full_halo flag).
+source "helpers.tcl"
+
+read_lef "./Nangate45/Nangate45.lef"
+read_lef "./testcases/orientation_improve1.lef"
+
+read_def "./testcases/halos1.def"
+
+set_thread_count 0
+rtl_macro_placer -report_directory [make_result_dir] -use_full_halo
+
+set def_file [make_result_file halos5.def]
+write_def $def_file
+
+diff_files halos5.defok $def_file
