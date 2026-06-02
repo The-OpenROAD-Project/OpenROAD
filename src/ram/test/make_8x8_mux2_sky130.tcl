0ce5beed33f149352ec442b9197efa13cac93741
Thinh Nguyen <nguyenthinh19011@gmail.com>
2026-06-02 10:41:26 -0400

    ram: auto-detect power and ground pin names
    Signed-off-by: Thinh Nguyen <nguyenthinh19011@gmail.com>

--- src/ram/test/make_8x8_mux2_sky130.tcl
+++ src/ram/test/make_8x8_mux2_sky130.tcl
@@ -11,8 +11,6 @@ generate_ram \
   -column_mux_ratio 2 \
   -read_ports 1 \
   -storage_cell sky130_fd_sc_hd__dfxtp_1 \
-  -power_pin VPWR \
-  -ground_pin VGND \
   -routing_layer {met1 0.48} \
   -ver_layer {met2 0.48 20} \
   -hor_layer {met3 0.48 10} \
