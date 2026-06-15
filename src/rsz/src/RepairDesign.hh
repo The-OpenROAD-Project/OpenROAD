0883e4a9afcdbbf4610eda114700eddab16e5aeb
James Cherry <cherry@CerezoBook.local>
2026-06-14 19:52:24 -0700

    clang-format
    Signed-off-by: James Cherry <cherry@CerezoBook.local>

--- src/rsz/src/RepairDesign.hh
+++ src/rsz/src/RepairDesign.hh
@@ -88,9 +88,7 @@ class RepairDesign : sta::dbStaState
   bool getCin(const sta::Pin* drvr_pin, float& cin);
   bool getLargestSizeCin(const sta::Pin* drvr_pin, float& cin);
   void findBufferSizes();
-  bool performGainBuffering(sta::Net* net,
-                            sta::Vertex* drvr,
-                            int max_fanout);
+  bool performGainBuffering(sta::Net* net, sta::Vertex* drvr, int max_fanout);
   void performEarlySizingRound(int& repaired_net_count);
 
   void checkDriverArcSlew(const sta::Scene* corner,
