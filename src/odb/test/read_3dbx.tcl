1b7d6a377de49f03d66735c070b43388bd4db9f4
osamahammad21 <osama@precisioninno.com>
2026-06-22 15:09:41 +0300

    odb: test unfolded classes tcl wrap
    Signed-off-by: osamahammad21 <osama@precisioninno.com>

--- src/odb/test/read_3dbx.tcl
+++ src/odb/test/read_3dbx.tcl
@@ -7,5 +7,10 @@ if { [$db getChip] == "NULL" } {
   puts "FAIL: Read 3dbx Failed"
   exit 1
 }
+check "Unfolded Chip Insts Count " { llength [$db getUnfoldedChipInsts] }  2
+check "Unfolded Chip Conns Count " { llength [$db getUnfoldedChipConns] }  2
+check "Unfolded Chip Regions Count " { llength [$db getUnfoldedChipRegionInsts] }  4
+check "Unfolded Chip Bumps Count " { llength [$db getUnfoldedChipBumpInsts] }  4
+check "Unfolded Chip Bumps Count " { llength [$db getUnfoldedChipNets] }  0
 
 puts "pass"
