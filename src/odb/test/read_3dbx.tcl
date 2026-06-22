8fa51ad7aa5184f6a36aae7c77fd5c3eb25fe840
osamahammad21 <osama@precisioninno.com>
2026-06-22 15:12:49 +0300

    tclfmt
    Signed-off-by: osamahammad21 <osama@precisioninno.com>

--- src/odb/test/read_3dbx.tcl
+++ src/odb/test/read_3dbx.tcl
@@ -3,14 +3,14 @@ source "helpers.tcl"
 set db [ord::get_db]
 
 read_3dbx "data/example.3dbx"
-if { [$db getChip] == "NULL" } {
-  puts "FAIL: Read 3dbx Failed"
-  exit 1
+if {[$db getChip] == "NULL"} {
+    puts "FAIL: Read 3dbx Failed"
+    exit 1
 }
-check "Unfolded Chip Insts Count " { llength [$db getUnfoldedChipInsts] }  2
-check "Unfolded Chip Conns Count " { llength [$db getUnfoldedChipConns] }  2
-check "Unfolded Chip Regions Count " { llength [$db getUnfoldedChipRegionInsts] }  4
-check "Unfolded Chip Bumps Count " { llength [$db getUnfoldedChipBumpInsts] }  4
-check "Unfolded Chip Bumps Count " { llength [$db getUnfoldedChipNets] }  0
+check "Unfolded Chip Insts Count " { llength [$db getUnfoldedChipInsts] } 2
+check "Unfolded Chip Conns Count " { llength [$db getUnfoldedChipConns] } 2
+check "Unfolded Chip Regions Count " { llength [$db getUnfoldedChipRegionInsts] } 4
+check "Unfolded Chip Bumps Count " { llength [$db getUnfoldedChipBumpInsts] } 4
+check "Unfolded Chip Bumps Count " { llength [$db getUnfoldedChipNets] } 0
 
 puts "pass"
