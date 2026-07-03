c8ec1664e06d4c48e05afe7e35aa889314dce043
Eder Monteiro <emrmonteiro@precisioninno.com>
2026-07-03 11:26:57 -0300

    grt/cugr: add via_multirect_cugr test for compound-via footprint union
    Signed-off-by: Eder Monteiro <emrmonteiro@precisioninno.com>

---
+++ src/grt/test/via_multirect_cugr.tcl
@@ -0,0 +1,12 @@
+# A via with several rects on one routing layer must have its full footprint
+# (the union of the rects) charged, not just the last box visited. via_multirect
+# defines VIA_C with two metal1 rects that union to 0.15x0.15; the via_geom line
+# must report that unioned extent (encl lower=150x150), not 150x100 or 100x150.
+source "helpers.tcl"
+read_lef "via_multirect.lef"
+read_def "via_priority.def"
+
+set_routing_layers -signal metal1-metal2
+set_debug_level GRT via_geom 1
+
+global_route -use_cugr
