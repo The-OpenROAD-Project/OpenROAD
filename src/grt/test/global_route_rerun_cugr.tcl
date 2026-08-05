f7fa908e4ebee17128874c61f96bb3beccbaa8a8
Eder Monteiro <emrmonteiro@precisioninno.com>
2026-08-05 20:54:47 +0000

    grt: test rerunning global_route in the same CUGR session
    Signed-off-by: Eder Monteiro <emrmonteiro@precisioninno.com>

---
+++ src/grt/test/global_route_rerun_cugr.tcl
@@ -0,0 +1,16 @@
+# Rerunning global_route -use_cugr in the same session: init() must rebuild
+# the CUGR netlist from scratch (stale appended state used to leave the
+# second route guide-less, failing with GRT-0076). The rerun must reproduce
+# the single-route result, so it shares gcd_cugr's golden guides.
+source "helpers.tcl"
+read_lef "Nangate45/Nangate45.lef"
+read_def "gcd.def"
+
+set guide_file [make_result_file global_route_rerun_cugr.guide]
+
+global_route -verbose -use_cugr
+global_route -verbose -use_cugr
+
+write_guides $guide_file
+
+diff_file gcd_cugr.guideok $guide_file
