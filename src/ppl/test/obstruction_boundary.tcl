9eb3cb9a31702d7426b8b732a2f567ef9c2289ff
Eder Monteiro <emrmonteiro@precisioninno.com>
2026-08-07 16:20:27 -0300

    ppl: fix line length in obstruction_boundary test
    Signed-off-by: Eder Monteiro <emrmonteiro@precisioninno.com>

--- src/ppl/test/obstruction_boundary.tcl
+++ src/ppl/test/obstruction_boundary.tcl
@@ -25,4 +25,5 @@ place_pins -hor_layers metal3 -ver_layers metal2 -corner_avoidance 0 \
 set obstruction_rect [list [list 100000 292000 104000 296000]]
 puts "pin to obstruction violations: [count_rect_violations metal2 $obstruction_rect spacing]"
 set min_spacing_rect [list [list 60000 292000 64000 296000]]
-puts "pin to min spacing obstruction violations: [count_rect_violations metal2 $min_spacing_rect 5000]"
+puts "pin to min spacing obstruction violations:\
+  [count_rect_violations metal2 $min_spacing_rect 5000]"
