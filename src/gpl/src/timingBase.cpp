f699094dd55cbc886578db6d23db1c644b3970e9
Øyvind Harboe <oyvind.harboe@zylin.com>
2026-08-10 16:06:23 +0200

    gpl: use boost scoped_exit
    Address review feedback from Matt to use boost scoped_exit
    instead of std::shared_ptr.
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- src/gpl/src/timingBase.cpp
+++ src/gpl/src/timingBase.cpp
@@ -4,6 +4,7 @@
 #include "timingBase.h"
 
 #include <algorithm>
+#include <boost/scope_exit.hpp>
 #include <cmath>
 #include <cstddef>
 #include <functional>
@@ -126,12 +127,12 @@ bool TimingBase::executeTimingDriven(bool run_journal_restore,
                                      bool enable_repair_timing)
 {
   {
-    // std::scope_exit at home, rarely needed, so inlined
-    std::shared_ptr<void> fix_pointers_guard(nullptr, [&](void*) {
+    BOOST_SCOPE_EXIT_ALL(&)
+    {
       if (!run_journal_restore) {
         nbc_->fixPointers();
       }
-    });
+    };
 
     rs_->findResizeSlacks(run_journal_restore,
                           (enable_repair_timing && repair_timing_),
