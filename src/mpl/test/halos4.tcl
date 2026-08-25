2025281b18de8d821851c55bbbd0db38cba87430
João Mai <jmai@precisioninno.com>
2026-08-25 15:34:51 +0000

    mpl: update tests
    Signed-off-by: João Mai <jmai@precisioninno.com>

--- src/mpl/test/halos4.tcl
+++ src/mpl/test/halos4.tcl
@@ -1,14 +1,14 @@
-# Test if base halos are correctly generated using 4 arguments.
+# Test that -use_full_halo restores uniform halo behavior,
+# ignoring pin-aware detection (regression for the use_full_halo flag).
 source "helpers.tcl"
 
 read_lef "./Nangate45/Nangate45.lef"
 read_lef "./testcases/orientation_improve1.lef"
 
-read_def "./testcases/halo3.def"
+read_def "./testcases/halos1.def"
 
 set_thread_count 0
-set_macro_base_halo 16.0 8.0 4.0 2.0
-rtl_macro_placer -report_directory [make_result_dir]
+rtl_macro_placer -report_directory [make_result_dir] -use_full_halo
 
 set def_file [make_result_file halos4.def]
 write_def $def_file
