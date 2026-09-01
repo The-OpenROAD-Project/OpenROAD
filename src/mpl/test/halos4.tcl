8b57ad8a0ec457817ec10bc26f1322389ffc2d78
João Mai <jmai@precisioninno.com>
2026-09-01 13:23:58 +0000

    mpl: update tests to reflect change in buildMacroHalo
    Signed-off-by: João Mai <jmai@precisioninno.com>

--- src/mpl/test/halos4.tcl
+++ src/mpl/test/halos4.tcl
@@ -1,5 +1,6 @@
-# Test that -use_full_channel restores uniform channel behavior,
-# ignoring pin-aware detection (regression for the use_full_channel flag).
+# Test that the default (no -pin_aware_channels) uses uniform channel
+# behavior, ignoring pin-aware detection (regression for the
+# full-channel-by-default behavior).
 source "helpers.tcl"
 
 read_lef "./Nangate45/Nangate45.lef"
@@ -8,7 +9,7 @@ read_lef "./testcases/orientation_improve1.lef"
 read_def "./testcases/halos1.def"
 
 set_thread_count 0
-rtl_macro_placer -report_directory [make_result_dir] -use_full_channel
+rtl_macro_placer -report_directory [make_result_dir]
 
 set def_file [make_result_file halos4.def]
 write_def $def_file
