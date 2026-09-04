9d9bf45308499012b50be6a3838b2cc6a77ae2d7
Naveen Venkat <archgen.guest@nyayanidhi.in>
2026-09-03 23:43:36 -0700

    mpl: ignore the macro being placed in overlap checks
    Signed-off-by: Naveen Venkat <archgen.guest@nyayanidhi.in>

--- src/mpl/test/place_macro_right_angle_rotation.tcl
+++ src/mpl/test/place_macro_right_angle_rotation.tcl
@@ -6,6 +6,12 @@ read_lef "./testcases/place_macro_right_angle_rotation.lef"
 
 read_def "./testcases/place_macro_right_angle_rotation.def"
 
+# Exercise overlap checks when the target macro is already placed.
+set block [ord::get_db_block]
+set macro [$block findInst macro]
+$macro setLocation 40000 40000
+$macro setPlacementStatus PLACED
+
 place_macro -macro_name macro -location {20 20} -orientation R90
 
 set def_file [make_result_file place_macro_right_angle_rotation.def]
