48e217115ac8998f850c65ea8a848319578f2e51
Arthur Koucher <arthurkoucher@precisioninno.com>
2026-09-22 12:42:31 +0000

    odb: default initiliaze all members across tmg_conn_g.h
    Signed-off-by: Arthur Koucher <arthurkoucher@precisioninno.com>

--- src/odb/src/db/tmg_conn_g.h
+++ src/odb/src/db/tmg_conn_g.h
@@ -16,42 +16,44 @@ class ConnectionGraph
  public:
   struct Edge
   {
-    int from;
-    int to;
-    Edge* next;
+    int from{-1};
+    int to{-1};
+    Edge* next{nullptr};
 
     // The same edge in the other direction. Every connection between
     // two points is stored twice in the graph, once per direction, so
     // a walk can leave a point along any of its edges.
-    Edge* reverse;
+    Edge* reverse{nullptr};
 
     // An edge is either a path or a short:
     // If wire_short is nullptr and wire_section_index is != -1,
     // the edge is a path. The opposite for a short.
-    Short* wire_short;
-    int wire_section_index;
+    Short* wire_short{nullptr};
+    int wire_section_index{-1};
 
     // Note that when an edge is marked as visited, it's reverse version is
     // also marked as visited.
-    bool visited;
+    bool visited{false};
 
     // When an edge is removed from the graph, the latter is not rebuilt.
     // Instead, that edge is marked as "skip" and the walkers will ignore
     // it when traversing.
-    bool skip;
+    bool skip{false};
   };
 
   struct Point
   {
-    Edge* first_edge;  // Head of the chain of edges that leave this point.
-    int path_index;
+    int path_index{-1};
+
+    // Head of the chain of edges that leave this point.
+    Edge* first_edge{nullptr};
 
     // Visit state.
     // 0:     This point was not yet reached.
     // 1:     This point was reached, but is no longer on the current descent.
     // 2 + k: This point is on the current descent, and descent_edges_[k] is
     //        the edge that leaves it.
-    int visited;
+    int visited{0};
   };
 
   void init(int ptN, int shortN);
@@ -78,7 +80,7 @@ class ConnectionGraph
 
   std::deque<Edge> edges_;
 
-  Edge* next_edge_;
+  Edge* next_edge_{nullptr};
 };
 
 }  // namespace odb
