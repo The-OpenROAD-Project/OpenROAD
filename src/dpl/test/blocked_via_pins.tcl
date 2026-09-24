7d6fc8f87f8180f6ce1d33963a90413c55a301da
Matt Liberty <mliberty@precisioninno.com>
2026-09-24 05:41:55 +0000

    dpl: offset the core in blocked_via_pins
    Exercise the core relative coordinates of the pin blockage check.
    
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- src/dpl/test/blocked_via_pins.tcl
+++ src/dpl/test/blocked_via_pins.tcl
@@ -1,4 +1,5 @@
-# pins under a power via stack column but clear of its metal
+# pins under a power via stack column but clear of its metal; the core
+# is offset from the origin to exercise the core relative coordinates
 source "helpers.tcl"
 read_lef asap7/asap7_tech_1x_201209.lef
 read_lef asap7/asap7sc7p5t_28_R_1x_220121a.lef
